c3d00061a746607381651eb339125620687854bc
KornelJanczak <korneljanczak10@gmail.com>
2024-11-07 23:49:09 +0100

    refactor: simplify error handling middleware and integrate logging

--- server/src/common/middleware/errorMiddleware.ts
+++ server/src/common/middleware/errorMiddleware.ts
@@ -1,21 +1,12 @@
 import { Request, Response, NextFunction } from "express";
 import ServerError from "../errors/serverError";
-
-interface NotFoundHandler {
-  (req: Request, res: Response, next: NextFunction): void;
-}
-
-export const notFoundHandler: NotFoundHandler = (req, res, next) => {
-  const error = new Error(`Not Found = ${req.originalUrl}`);
-  res.status(404);
-  next(error);
-};
+import logger from "../config/logger";
 
 export const generalErrorHandler = (
   err: Error | ServerError,
-  req: Request,
+  _: Request,
   res: Response,
-  next: NextFunction
+  __: NextFunction
 ) => {
   const statusCode = res.statusCode == 200 ? 500 : res.statusCode;
 
@@ -27,12 +18,12 @@ export const generalErrorHandler = (
       stack: process.env.NODE_ENV === "production" ? null : err.stack,
     };
 
-    const jsonErrorStucture = JSON.stringify(errorResponse, null, 2);
+    logger.error(errorResponse);
 
-    return res.status(err.code).send(errorResponse);
+    res.status(err.code).send(errorResponse);
   }
 
-  return res.status(statusCode).send({
+  res.status(statusCode).send({
     message: err.message || "Internal Server Error",
     stack: process.env.NODE_ENV === "production" ? null : err.stack,
   });
