021b4677563c3f55b612ca170a501ce6c1f6fcd1
KornelJanczak <korneljanczak10@gmail.com>
2024-11-16 20:28:13 +0100

    feat: integrate session management and passport for authentication; configure session settings

--- server/src/app.ts
+++ server/src/app.ts
@@ -3,28 +3,37 @@ import cookieParser from "cookie-parser";
 import express, { Application } from "express";
 import routes from "./router/index";
 import { Express } from "express";
-import { GrantType, setupKinde } from "@kinde-oss/kinde-node-express";
+import { setupKinde } from "@kinde-oss/kinde-node-express";
 import { generalErrorHandler } from "./common/middleware/errorMiddleware";
+import config from "./common/config/kindeConfig";
+import passport from "passport";
+import session from "express-session";
+import "./common/strategies/google-strategy";
 
-const config = {
-  clientId: process.env.KINDE_CLIENT_ID,
-  issuerBaseUrl: process.env.KINDE_ISSUER_URL,
-  siteUrl: "http://localhost:3000",
-  secret: process.env.KINDE_CLIENT_SECRET,
-  redirectUrl: "http://localhost:3000",
-  scope: "openid profile email",
-  grantType: GrantType.AUTHORIZATION_CODE, 
-  unAuthorisedUrl: "http://localhost:3000/unauthorised",
-  postLogoutRedirectUrl: "http://localhost:3000",
-};
+const SESSION_EXPIRY_DATE = 60 * 60000;
 
 export default function createApp(): Application {
   const app: Express = express();
   app.use(express.json());
   app.use(cookieParser());
+  app.use(
+    session({
+      secret: process.env.SESSION_SECRET || "",
+      saveUninitialized: true,
+      resave: false,
+      cookie: {
+        maxAge: SESSION_EXPIRY_DATE,
+        secure: true,
+      },
+    })
+  );
+
+  app.use(passport.initialize());
+  app.use(passport.session());
+
   app.use("/api", routes);
 
-  setupKinde(config, app);
+  // setupKinde(config, app);
 
   app.use(generalErrorHandler);
   return app;
