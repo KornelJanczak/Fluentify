1b4bd016b87a75f2410f1547a5020f881c8587be
KornelJanczak <korneljanczak10@gmail.com>
2024-11-16 20:28:05 +0100

    feat: implement Google authentication routes and user status check; add logout functionality

--- server/src/api/v1/modules/auth/router.ts
+++ server/src/api/v1/modules/auth/router.ts
@@ -1,11 +1,40 @@
-import { Router } from "express";
+import { Router, Request, Response, NextFunction } from "express";
+import passport from "passport";
 
-const router: Router = Router();
+const router = Router();
 
-router.get("/auth/kinde_callback", (req, res) => {
-  console.log(req);
+router.get(
+  "/auth/google",
+  passport.authenticate("google", {
+    scope: [
+      "email",
+      "profile",
+      "openid",
+      "https://www.googleapis.com/auth/cloud-platform",
+    ],
+  })
+);
 
-  res.send("Hello World");
+router.get("/auth/callback/google", passport.authenticate("google"));
+
+router.get("/auth/status", (req: Request, res: Response) => {
+  console.log(req.user, "STATUS USER");
+  req.user;
+
+  req.user ? res.send(req.user) : res.sendStatus(401);
 });
+
+router.get(
+  "/auth/logout",
+  (req: Request, res: Response, next: NextFunction) => {
+    req.logout((err) => {
+      console.log("chuj");
+
+      if (err) return next(err);
+
+      res.send(200);
+    });
+  }
+);
 
 export default router;
