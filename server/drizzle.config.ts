aaba49da8079e29089366dc658911ff270ee580c
KornelJanczak <korneljanczak10@gmail.com>
2024-11-17 19:27:49 +0100

    feat: update drizzle configuration and database schema for improved structure and functionality

--- server/drizzle.config.ts
+++ server/drizzle.config.ts
@@ -1,12 +1,16 @@
+import { table } from "console";
 import "dotenv/config";
 import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
   out: "./drizzle",
   schema: "./src/common/db/schema.ts",
+
   dialect: "postgresql",
   dbCredentials: {
     url: process.env.DATABASE_URL!,
   },
+  verbose: true,
+  strict: true,
 });
-``
+``;
