bea9715451ef8f666f891dc66c7010567a2247ec
KornelJanczak <korneljanczak10@gmail.com>
2024-11-17 19:26:42 +0100

    feat: add logout and authentication status controllers for user session management

--- server/src/api/v1/modules/auth/controller.ts
+++ server/src/api/v1/modules/auth/controller.ts
@@ -1,1 +1,35 @@
-// export const 
+import { Request, Response, NextFunction } from "express";
+import AuthenticationError from "../../../../common/errors/authenticationError";
+import logger from "../../../../common/config/logger";
+export const logOutController = (
+  req: Request,
+  _: Response,
+  next: NextFunction
+) => {
+  req.logout((err) => {
+    if (err)
+      next(
+        new AuthenticationError({
+          message: err.message,
+          stack: err.stack,
+        })
+      );
+  });
+};
+
+export const authStatusController = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const currentUser = req.user;
+
+  console.log(currentUser);
+  console.log(req.session);
+
+  if (!currentUser)
+    next(new AuthenticationError({ message: "User is not authenticated" }));
+
+  logger.info(`User ${currentUser} is authenticated`);
+  res.send(req.user);
+};
