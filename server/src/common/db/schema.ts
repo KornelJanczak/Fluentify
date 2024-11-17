322119d7924c13db1e63660480589b94f42218df
KornelJanczak <korneljanczak10@gmail.com>
2024-11-17 19:26:17 +0100

    feat: update database schema to use UUIDs for primary keys and adjust field lengths for user and chat tables

--- server/src/common/db/schema.ts
+++ server/src/common/db/schema.ts
@@ -1,32 +1,33 @@
 import { integer, pgTable, varchar } from "drizzle-orm/pg-core";
 import { timestamp, uuid } from "drizzle-orm/pg-core";
 import { createInsertSchema, createSelectSchema } from "drizzle-zod";
-import { type InferSelectModel, relations } from "drizzle-orm";
-import { boolean } from "drizzle-orm/mysql-core";
+import { type InferSelectModel } from "drizzle-orm";
 
 // USER TABLE
 export const users = pgTable("users", {
-  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  id: varchar("id", { length: 255 }).primaryKey().notNull(),
   email: varchar({ length: 255 }).notNull().unique(),
-  imagePaths: varchar({ length: 255 }).notNull(),
-  role: varchar({ length: 50 }).notNull(),
-  subscriptionExpiryDate: varchar({ length: 50 }).notNull(),
-  // chats: integer().references(() => chats.id),
+  imagePaths: varchar({ length: 510 }).notNull(),
+  role: varchar({ length: 255 }).notNull(),
+  subscriptionExpiryDate: varchar({ length: 255 }).notNull(),
 });
 
 export const insertUserSchema = createInsertSchema(users);
 export const selectUserSchema = createSelectSchema(users);
 
 export type User = InferSelectModel<typeof users>;
+
 // #################################################################### //
 
 // CHAT TABLE
 export const chats = pgTable("chats", {
-  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  id: uuid("id").primaryKey().notNull().defaultRandom(),
   usedTokens: integer().notNull(),
   startedAt: timestamp().notNull(),
   workspaceId: varchar({ length: 255 }).notNull(),
-  userId: integer("userId"),
+  userId: varchar("userId")
+    .notNull()
+    .references(() => users.id),
 });
 
 export const insertChatSchema = createInsertSchema(chats);
@@ -38,33 +39,16 @@ export type Chat = InferSelectModel<typeof chats>;
 
 // MESSAGE TABLE
 export const messages = pgTable("messages", {
-  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  id: uuid("id").primaryKey().notNull().defaultRandom(),
+  chatId: uuid("chatId")
+    .notNull()
+    .references(() => chats.id),
   content: varchar({ length: 255 }).notNull(),
   usedTokens: integer().notNull(),
-  userId: integer("userId"),
-  chatId: integer("chatId"),
+  createdAt: timestamp("createdAt").notNull(),
 });
 
 export const insertMessageSchema = createInsertSchema(messages);
 export const selectMessageSchema = createSelectSchema(messages);
 
 export type Message = InferSelectModel<typeof messages>;
-
-// #################################################################### //
-
-// Relations
-export const usersRelations = relations(users, ({ many }) => ({
-  chats: many(chats),
-}));
-
-export const chatRelations = relations(chats, ({ one, many }) => ({
-  user: one(users, {
-    fields: [chats.userId],
-    references: [users.id],
-  }),
-  messages: many(messages),
-}));
-
-export const messagesRelations = relations(messages, ({ one }) => ({
-  chat: one(chats),
-}));
