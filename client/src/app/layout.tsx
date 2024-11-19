12aa5d43d150e3c75d95f408e860965cccdb3058
KornelJanczak <korneljanczak10@gmail.com>
2024-11-19 22:48:32 +0100

    feat: update RootLayout to use NextAuth session for authentication context

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,21 +1,23 @@
-// client/src/app/layout.tsx
-
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/navbar";
-import { AuthProvider } from "@/providers/auth-provider";
+import AuthProvider from "@/common/providers/auth-provider";
+import { getServerSession } from "next-auth";
+import { authOptions } from "./api/auth/[...nextauth]/options";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const session = await getServerSession(authOptions);
+
   return (
     <html lang="en">
       <body className={inter.className}>
-        <AuthProvider>
+        <AuthProvider session={session}>
           <Navbar />
           <div className="flex min-h-screen flex-col items-center p-24">
             {children}
