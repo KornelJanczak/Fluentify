483fea550ee9784ace8efc662949a1cbbbadc469
KornelJanczak <korneljanczak10@gmail.com>
2024-11-21 22:32:47 +0100

    feat: comment out API rewrites in next.config.js

--- client/next.config.js
+++ client/next.config.js
@@ -2,14 +2,14 @@
 const nextConfig = {
   async rewrites() {
     return [
-      {
-        source: "/api/auth/:path",
-        destination: "/api/auth/:path*",
-      },
-      {
-        source: "/api/:path*",
-        destination: "http://localhost:5000/api/:path*",
-      },
+      // {
+      //   source: "/api/auth/:path",
+      //   destination: "/api/auth/:path*",
+      // },
+      // {
+      //   source: "/api/:path*",
+      //   destination: "http://localhost:5000/api/:path*",
+      // },
     ];
   },
 };
