78604bcc6080dfc2dd5b258994d069b297bbb5aa
KornelJanczak <korneljanczak10@gmail.com>
2024-11-21 22:34:42 +0100

    feat: improve authentication middleware with structured logging and enhanced error handling

--- server/src/common/middleware/authMiddleware.ts
+++ server/src/common/middleware/authMiddleware.ts
@@ -1,13 +1,19 @@
 import { NextFunction, Request, Response } from "express";
 import AuthenticationError from "../errors/authenticationError";
+import logger from "../config/logger";
 
-const authMiddleware = (req: Request, res: Response, next: NextFunction) => {
-  console.log("auth", req.isAuthenticated());
-
+const authMiddleware = (req: Request, _: Response, next: NextFunction) => {
   if (!req.isAuthenticated()) {
-    next(new AuthenticationError({ message: "User is not authenticated" }));
+    return next(
+      new AuthenticationError({
+        service: "AuthMiddleware",
+        message: "User is not authenticated",
+      })
+    );
   }
-  next();
+
+  logger.info("AuthMiddleware: User is authenticated");
+  return next();
 };
 
 export default authMiddleware;
