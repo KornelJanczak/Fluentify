0353b1a352251fe66fd9a065039819e03c508a06
KornelJanczak <korneljanczak10@gmail.com>
2024-11-21 22:32:42 +0100

    feat: remove Kinde authentication integration and related files

--- client/src/middleware.ts
+++
@@ -1,9 +0,0 @@
-import { withAuth } from "@kinde-oss/kinde-auth-nextjs/server";
-
-export default function middleware(req) {
-  return withAuth(req);
-}
-
-export const config = {
-  matcher: ["/private"],
-};
