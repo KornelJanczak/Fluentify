13f9724b7382bd6e8a57a89baefe4b09cbba909b
KornelJanczak <korneljanczak10@gmail.com>
2024-11-25 22:27:57 +0100

    feat: rename getSessionToken to getSessionCookie and update return value to include cookie format

--- client/src/common/lib/auth.ts
+++ client/src/common/lib/auth.ts
@@ -1,14 +1,19 @@
 "use server";
 import { cookies } from "next/headers";
+import jwt from "jsonwebtoken";
 
-export const getSessionToken = async (): Promise<string> => {
+export const getSessionCookie = async () => {
   const cookieStore = await cookies();
   const sessionId = cookieStore.get("fluentify-server-session").value;
   const sessionSignature = cookieStore.get(
     "fluentify-server-session.sig"
   ).value;
 
-  const sessionToken = sessionId + "." + sessionSignature;
+  const sessionCookie =
+    "fluentify-server-session=" +
+    sessionId +
+    "; fluentify-server-session.sig=" +
+    sessionSignature;
 
-  return sessionToken;
+  return sessionCookie;
 };
