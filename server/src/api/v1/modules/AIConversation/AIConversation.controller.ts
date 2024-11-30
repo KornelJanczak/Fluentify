81a8eaf61050da89bf782234f85c2ac226cb3640
KornelJanczak <korneljanczak10@gmail.com>
2024-11-30 19:36:11 +0100

    refactor: convert AIConversation controller to a class-based structure and streamline conversation handling

--- server/src/api/v1/modules/AIConversation/AIConversation.controller.ts
+++ server/src/api/v1/modules/AIConversation/AIConversation.controller.ts
@@ -1,29 +1,24 @@
 import { Request, Response, NextFunction } from "express";
 import AIConversationService from "./service";
-import { StreamData } from "ai";
-import { openai } from "@ai-sdk/openai";
-import { streamText } from "ai";
-import { customAi } from "../../../../common/AI";
-import { google } from "@ai-sdk/google";
 
-export const startConversationWithAI = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
-  try {
-    const messages = req.body.messages;
+class AIConversationController {
+  async startConversationWithAI(
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ) {
+    try {
+      const messages = req.body.messages;
 
-    console.log(messages);
+      const AIConversation = new AIConversationService(messages);
+      const conversationResult = await AIConversation.startConversation();
 
-    const data = new StreamData();
-
-    const AIConversation = new AIConversationService(messages, data);
-    const conversationResult = await AIConversation.startConversation();
-
-    return conversationResult.pipeTextStreamToResponse(res);
-    // return conversationResult.toDataStreamResponse();
-  } catch (error) {
-    next(error);
+      return conversationResult.pipeDataStreamToResponse(res);
+    } catch (error) {
+      next(error);
+    }
   }
-};
+}
+
+const aiConversationController = new AIConversationController();
+export default aiConversationController;
