091d82518c836ec59ba5d24fe040a90d60115cf1
KornelJanczak <korneljanczak10@gmail.com>
2024-12-01 19:30:40 +0100

    feat: add ChatRepository for user-specific chat retrieval and update tsconfig for path mapping

---
+++ server/src/common/repositories/chatRepository.ts
@@ -0,0 +1,36 @@
+import { db } from "../db";
+import { type Chat, chats } from "../db/schema";
+import { eq } from "drizzle-orm";
+import DatabaseError from "../errors/dbError";
+import BaseRepository from "./baseRepository";
+import { PgColumn, PgTable, TableConfig } from "drizzle-orm/pg-core";
+
+class ChatRepository extends BaseRepository<Chat> {
+  protected table: PgTable<TableConfig>;
+  protected idColumn: PgColumn;
+
+  constructor(table: PgTable<TableConfig>, idColumn: PgColumn) {
+    super();
+    this.table = table;
+    this.idColumn = idColumn;
+  }
+
+  async getByUserId(userId: string): Promise<Chat[]> {
+    try {
+      const chatList: Chat[] = await db
+        .select()
+        .from(chats)
+        .where(eq(chats.userId, userId));
+
+      return chatList;
+    } catch (err) {
+      throw new DatabaseError({
+        message: err.message,
+        stack: err.stack,
+      });
+    }
+  }
+}
+
+const chatRepository = new ChatRepository(chats, chats.id);
+export default chatRepository;
