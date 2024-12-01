f037433e78fcde686a1a36cd28f61e01a3d9f7de
KornelJanczak <korneljanczak10@gmail.com>
2024-12-01 19:30:58 +0100

    feat: add chat interfaces for ChatService and ChatController abstractions

---
+++ server/src/api/v1/modules/chat/chat.interfaces.ts
@@ -0,0 +1,16 @@
+import { StreamTextResult, CoreTool } from "ai";
+import { Request, Response, NextFunction } from "express";
+
+export type ChatResult = Promise<
+  StreamTextResult<Record<string, CoreTool<any, any>>>
+>;
+
+export interface ChatServiceAbstract {
+  execute(): ChatResult;
+}
+
+export interface ChatControllerAbstract {
+  startChat(req: Request, res: Response, next: NextFunction): void;
+  createChat(req: Request, res: Response, next: NextFunction): void;
+  getChat(req: Request, res: Response, next: NextFunction): void;
+}
