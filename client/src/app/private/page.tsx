9644effdcf1cfc38c57e948bed3af83aa672a557
KornelJanczak <korneljanczak10@gmail.com>
2024-12-05 21:56:04 +0100

    feat: update PrivatePage to remove chat creation logic and pass sessionCookie directly to CreateChatButton

--- client/src/app/private/page.tsx
+++ client/src/app/private/page.tsx
@@ -10,30 +10,13 @@ export default async function PrivatePage() {
 
   if (!currentUser) return;
 
-  const response = await fetch("http://localhost:5000/api/v1/create-chat", {
-    method: "POST",
-    body: JSON.stringify({
-      title: "Private Chat with English Teacher",
-      acab: "adasd",
-    }),
-    headers: {
-      Cookie: sessionCookie,
-    },
-  });
-
-  const chatId = await response.json();
-
   return (
     <>
       <h1 className="mt-10 text-4xl font-bold tracking-tight text-white sm:text-6xl">
         Private Page
       </h1>
 
-      <CreateChatButton sessionCookie={chatId} />
-
-      <div className="mt-20">
-        <Chat />
-      </div>
+      <CreateChatButton sessionCookie={sessionCookie} />
     </>
   );
 }
