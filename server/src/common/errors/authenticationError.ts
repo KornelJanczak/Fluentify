cdb8963b6aabe9516d9b3bbf8e404bb9f94e70ca
KornelJanczak <korneljanczak10@gmail.com>
2024-12-08 16:13:13 +0100

    refactor: update AuthenticationError to use HTTP_STATUS for error code

--- server/src/common/errors/authenticationError.ts
+++ server/src/common/errors/authenticationError.ts
@@ -1,12 +1,13 @@
 import ServerError from "./serverError";
 import { ServerErrorProps } from "./serverError";
+import HTTP_STATUS from "http-status-codes";
 
 class AuthenticationError extends ServerError {
   constructor(props: Partial<ServerErrorProps> = {}) {
     super({
       name: "AuthenticationError",
-      code: 401,
-      ...props
+      code: HTTP_STATUS.NETWORK_AUTHENTICATION_REQUIRED,
+      ...props,
     });
   }
 }
