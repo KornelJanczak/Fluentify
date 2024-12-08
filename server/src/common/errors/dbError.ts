908ea427c6f1e587813683e157b9577cb6d0d379
KornelJanczak <korneljanczak10@gmail.com>
2024-12-08 16:13:27 +0100

    refactor: update DatabaseError to use HTTP_STATUS for internal server error code

--- server/src/common/errors/dbError.ts
+++ server/src/common/errors/dbError.ts
@@ -1,11 +1,12 @@
 import ServerError from "./serverError";
+import HTTP_STATUS from "http-status-codes";
 import { ServerErrorProps } from "./serverError";
 
 export default class DatabaseError extends ServerError {
   constructor(props: Partial<ServerErrorProps> = {}) {
     super({
       name: "DatabaseError",
-      code: 500,
+      code: HTTP_STATUS.INTERNAL_SERVER_ERROR,
       ...props,
     });
   }
