11daed6542eead1dd7f5f8a63ed3196a90f4635b
KornelJanczak <korneljanczak10@gmail.com>
2024-12-09 23:43:08 +0100

    feat: add endpoint to retrieve messages by chat ID in chat router

--- server/src/api/v1/modules/chat/chat.router.ts
+++ server/src/api/v1/modules/chat/chat.router.ts
@@ -7,5 +7,10 @@ const router = Router();
 router.post("/chat", authMiddleware, chatController.startChat);
 router.post("/create-chat", authMiddleware, chatController.createChat);
 router.get("/chat/:id", authMiddleware, chatController.getChat);
+router.get(
+  "/chat/:id/messages",
+  authMiddleware,
+  chatController.getMessagesByChatId
+);
 
 export default router;
