f66ae195649e9277d01465b9459a7f791993e9b7
KornelJanczak <korneljanczak10@gmail.com>
2024-12-12 23:42:19 +0100

    fix: refactor DatabaseError constructor to improve parameter handling

--- server/src/shared/errors/dbError.ts
+++ server/src/shared/errors/dbError.ts
@@ -1,13 +1,13 @@
-import ServerError from "./serverError";
+import ServerError, { ServerErrorArguments } from "./serverError";
 import HTTP_STATUS from "http-status-codes";
-import { ServerErrorProps } from "./serverError";
 
 export default class DatabaseError extends ServerError {
-  constructor(props: Partial<ServerErrorProps> = {}) {
-    super({
-      name: "DatabaseError",
-      code: HTTP_STATUS.INTERNAL_SERVER_ERROR,
-      ...props,
+  constructor({ fileName, service, message, stack }: ServerErrorArguments) {
+    super("DatabaseError", HTTP_STATUS.UNAUTHORIZED, {
+      fileName,
+      message,
+      service,
+      stack,
     });
   }
 }
