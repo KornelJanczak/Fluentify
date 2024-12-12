f292f5719c9b3563bf443c72efa8d522bab6f228
KornelJanczak <korneljanczak10@gmail.com>
2024-12-12 23:42:27 +0100

    fix: refactor AuthenticationError constructor to improve parameter handling

--- server/src/shared/errors/authenticationError.ts
+++ server/src/shared/errors/authenticationError.ts
@@ -1,13 +1,13 @@
-import ServerError from "./serverError";
-import { ServerErrorProps } from "./serverError";
+import ServerError, { ServerErrorArguments } from "./serverError";
 import HTTP_STATUS from "http-status-codes";
 
 class AuthenticationError extends ServerError {
-  constructor(props: Partial<ServerErrorProps> = {}) {
-    super({
-      name: "AuthenticationError",
-      code: HTTP_STATUS.NETWORK_AUTHENTICATION_REQUIRED,
-      ...props,
+  constructor({ fileName, service, message, stack }: ServerErrorArguments) {
+    super("AuthorizationError", HTTP_STATUS.UNAUTHORIZED, {
+      fileName,
+      message,
+      service,
+      stack,
     });
   }
 }
