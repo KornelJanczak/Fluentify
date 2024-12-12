12c617ab7e35c44a32b2d49886f2382b2edb94f4
KornelJanczak <korneljanczak10@gmail.com>
2024-12-12 23:42:34 +0100

    fix: refactor ChatController methods to remove error handling and streamline code

--- server/src/features/chat/chat.controller.ts
+++ server/src/features/chat/chat.controller.ts
@@ -1,4 +1,4 @@
-import { Request, Response, NextFunction } from "express";
+import { Request, Response } from "express";
 import ChatService from "./service";
 import { ChatControllerAbstract, ChatServiceAbstract } from "./chat.interfaces";
 import chatRepository from "@shared/repositories/chatRepository";
@@ -6,99 +6,56 @@ import { v4 as uuidv4 } from "uuid";
 import { User } from "src/shared/db/schema";
 import { type Chat } from "src/shared/db/schema";
 import HTTP_STATUS from "http-status-codes";
-import NotFoundError from "@shared/errors/notFoundError";
 import messagesRepository from "@shared/repositories/messagesRepository";
 
 class ChatController implements ChatControllerAbstract {
-  async startChat(req: Request, res: Response, next: NextFunction) {
-    try {
-      const messages = req.body.messages;
-      const chatId = req.body.chatId;
+  async startChat(req: Request, res: Response) {
+    const messages = req.body.messages;
+    const chatId = req.body.chatId;
 
-      const chatService: ChatServiceAbstract = new ChatService(
-        messages,
-        chatId
-      );
+    const chatService: ChatServiceAbstract = new ChatService(messages, chatId);
+    const conversationResult = await chatService.execute();
 
-      const conversationResult = await chatService.execute();
-
-      return conversationResult.pipeDataStreamToResponse(res);
-    } catch (error) {
-      next(
-        new NotFoundError({
-          service: "chat.controller: startChat",
-          ...error,
-        })
-      );
-    }
+    return conversationResult.pipeDataStreamToResponse(res);
   }
 
-  async createChat(req: Request, res: Response, next: NextFunction) {
-    try {
-      const user: User = req.user as User;
+  async createChat(req: Request, res: Response) {
+    const user: User = req.user as User;
 
-      console.log(req.body);
+    console.log(req.body);
 
-      const newItem = {
-        id: uuidv4(),
-        userId: user.id,
-        title: req.body.title,
-        usedTokens: 0,
-        startedAt: new Date(),
-      };
+    const newItem = {
+      id: uuidv4(),
+      userId: user.id,
+      title: req.body.title,
+      usedTokens: 0,
+      startedAt: new Date(),
+    };
 
-      const newChat = (await chatRepository.create({
-        service: "chat.controller: createChat",
-        newItem,
-      })) as Chat;
+    const newChat = (await chatRepository.create({
+      service: "createChat",
+      newItem,
+    })) as Chat;
 
-      return res.status(HTTP_STATUS.OK).json(newChat.id);
-    } catch (error) {
-      next(
-        new NotFoundError({
-          service: "chat.controller: createChat",
-          ...error,
-        })
-      );
-    }
+    return res.status(HTTP_STATUS.OK).json(newChat.id);
   }
 
-  async getChat(req: Request, res: Response, next: NextFunction) {
-    try {
-      const chat = await chatRepository.getById({
-        service: "chat.controller: getChat",
-        id: req.params.id,
-      });
+  async getChat(req: Request, res: Response) {
+    const chat = await chatRepository.getById({
+      service: "getChat",
+      id: req.params.id,
+    });
 
-      console.log(chat);
-
-      return res.status(HTTP_STATUS.OK).json(chat);
-    } catch (error) {
-      next(
-        new NotFoundError({
-          service: "chat.controller: getChat",
-          ...error,
-        })
-      );
-    }
+    return res.status(HTTP_STATUS.OK).json(chat);
   }
 
-  async getMessagesByChatId(req: Request, res: Response, next: NextFunction) {
-    try {
-      const messages = await messagesRepository.getMessagesByChatId({
-        service: "chat.controller: getMessagesByChatId",
-        chatId: req.params.id,
-      });
+  async getMessagesByChatId(req: Request, res: Response) {
+    const messages = await messagesRepository.getMessagesByChatId({
+      service: "getMessagesByChatId",
+      chatId: req.params.id,
+    });
 
-      return res.status(HTTP_STATUS.OK).json(messages);
-    } catch (error) {
-      next(
-        new NotFoundError({
-          service: "chat.controller: getMessagesByChatId",
-          ...error,
-        })
-      );
-    }
+    return res.status(HTTP_STATUS.OK).json(messages);
   }
 }
 
