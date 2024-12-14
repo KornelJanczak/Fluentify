d85a7ce5c6b50ec786630ad126439b0609789d69
KornelJanczak <korneljanczak10@gmail.com>
2024-12-14 14:11:32 +0100

    feat: add database schema for users, chats, and messages with corresponding insert and select schemas

---
+++ server/src/shared/services/db/schema.ts
@@ -0,0 +1,55 @@
+import { integer, pgTable, varchar } from "drizzle-orm/pg-core";
+import { timestamp, uuid, json } from "drizzle-orm/pg-core";
+import { createInsertSchema, createSelectSchema } from "drizzle-zod";
+import { type InferSelectModel } from "drizzle-orm";
+
+// USER TABLE
+export const users = pgTable("users", {
+  id: varchar("id", { length: 255 }).primaryKey().notNull(),
+  email: varchar({ length: 255 }).notNull().unique(),
+  imagePaths: varchar({ length: 510 }).notNull(),
+  role: varchar({ length: 255 }).notNull(),
+  subscriptionExpiryDate: varchar({ length: 255 }).notNull(),
+});
+
+export const insertUserSchema = createInsertSchema(users);
+export const selectUserSchema = createSelectSchema(users);
+
+export type User = InferSelectModel<typeof users>;
+
+// #################################################################### //
+
+// CHAT TABLE
+export const chats = pgTable("chats", {
+  id: uuid("id").primaryKey().notNull().defaultRandom(),
+  title: varchar({ length: 255 }).notNull(),
+  usedTokens: integer().notNull(),
+  startedAt: timestamp().notNull(),
+  userId: varchar("userId")
+    .notNull()
+    .references(() => users.id),
+});
+
+export const insertChatSchema = createInsertSchema(chats);
+export const selectChatSchema = createSelectSchema(chats);
+
+export type Chat = InferSelectModel<typeof chats>;
+
+// #################################################################### //
+
+// MESSAGE TABLE
+export const messages = pgTable("messages", {
+  id: uuid("id").primaryKey().notNull().defaultRandom(),
+  chatId: uuid("chatId")
+    .notNull()
+    .references(() => chats.id),
+  role: varchar({ length: 255 }).notNull(),
+  content: json("content").notNull(),
+  usedTokens: integer().notNull(),
+  createdAt: timestamp("createdAt").notNull(),
+});
+
+export const insertMessageSchema = createInsertSchema(messages);
+export const selectMessageSchema = createSelectSchema(messages);
+
+export type Message = InferSelectModel<typeof messages>;
