76a8a2b8ca51d16f07eaa433c85de5942feaec71
KornelJanczak <korneljanczak10@gmail.com>
2024-12-15 22:05:58 +0100

    refactor: streamline chat creation and retrieval logic in ChatController by removing unnecessary service parameters

--- server/src/features/chat/chat.controller.ts
+++ server/src/features/chat/chat.controller.ts
@@ -4,7 +4,6 @@ import { ChatControllerAbstract, ChatServiceAbstract } from "./chat.interfaces";
 import chatRepository from "@shared/repositories/chatRepository";
 import { v4 as uuidv4 } from "uuid";
 import { User } from "@shared/services/db/schema";
-import { type Chat } from "@shared/services/db/schema";
 import HTTP_STATUS from "http-status-codes";
 import messagesRepository from "@shared/repositories/messagesRepository";
 
@@ -21,40 +20,25 @@ class ChatController implements ChatControllerAbstract {
 
   async createChat(req: Request, res: Response) {
     const user: User = req.user as User;
-
-    console.log(req.body);
-
-    const newItem = {
+    const newChat = await chatRepository.create({
       id: uuidv4(),
       userId: user.id,
       title: req.body.title,
       usedTokens: 0,
       startedAt: new Date(),
-    };
-
-    const newChat = (await chatRepository.create({
-      service: "createChat",
-      newItem,
-    })) as Chat;
-
+    });
     return res.status(HTTP_STATUS.OK).json(newChat.id);
   }
 
   async getChat(req: Request, res: Response) {
-    const chat = await chatRepository.getById({
-      service: "getChat",
-      id: req.params.id,
-    });
-
+    const chat = await chatRepository.getById(req.params.id);
     return res.status(HTTP_STATUS.OK).json(chat);
   }
 
   async getMessagesByChatId(req: Request, res: Response) {
-    const messages = await messagesRepository.getMessagesByChatId({
-      service: "getMessagesByChatId",
-      chatId: req.params.id,
-    });
-
+    const messages = await messagesRepository.getMessagesByChatId(
+      req.params.id
+    );
     return res.status(HTTP_STATUS.OK).json(messages);
   }
 }
