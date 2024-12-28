4eabe61df44100ae4d24450dc905a052c8f9e24e
KornelJanczak <korneljanczak10@gmail.com>
2024-12-28 20:05:20 +0100

    refactor: rename interfaces for chat service and controller, and add audio generator interface

--- server/src/features/chat/chat.interfaces.ts
+++ server/src/features/chat/chat.interfaces.ts
@@ -4,14 +4,19 @@ import { Request, Response, NextFunction } from "express";
 export type ChatResult = StreamTextResult<Record<string, CoreTool<any, any>>>;
 export type executeReturnType = Promise<Response<any, Record<string, any>>>;
 
-export interface ChatServiceAbstract {
+export interface IChatService {
   execute(): executeReturnType;
 }
 
-export interface ChatControllerAbstract {
+export interface IChatController {
   startChat(req: Request, res: Response, next: NextFunction): void;
-  streamAudio(streamWriter: DataStreamWriter): void;
   createChat(req: Request, res: Response, next: NextFunction): void;
   getChat(req: Request, res: Response, next: NextFunction): void;
   getMessagesByChatId(req: Request, res: Response, next: NextFunction): void;
+}
+
+export interface IChatService {}
+
+export interface IAudioGenerator {
+  execute(): void;
 }
