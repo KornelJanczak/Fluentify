ed28c720d8b6783a6f42d7620ef2d9712f597f63
KornelJanczak <korneljanczak10@gmail.com>
2024-12-28 20:04:56 +0100

    refactor: simplify text-to-speech client initialization and export

--- server/src/shared/services/textToSpeech/index.ts
+++ server/src/shared/services/textToSpeech/index.ts
@@ -1,9 +1,3 @@
 import { TextToSpeechClient } from "@google-cloud/text-to-speech";
-import config from "@root/config";
-import dotenv from "dotenv";
 
-dotenv.config();
-
-const client = new TextToSpeechClient();
-
-export default client;
+export const textToSpeechClient = new TextToSpeechClient();
