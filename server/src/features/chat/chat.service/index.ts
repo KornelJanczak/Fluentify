a904207b5978fe6e71486295ea1fc727cd8b53f0
KornelJanczak <korneljanczak10@gmail.com>
2024-12-28 20:05:10 +0100

    refactor: streamline chat service by removing unused TTS methods and updating AI model integration

--- server/src/features/chat/chat.service/index.ts
+++ server/src/features/chat/chat.service/index.ts
@@ -1,25 +1,12 @@
-import {
-  generateId,
-  streamText,
-  CoreMessage,
-  createDataStreamResponse,
-  DataStreamWriter,
-} from "ai";
-import { google } from "@ai-sdk/google";
+import { streamText, CoreMessage, DataStreamWriter } from "ai";
 import aiCharactersInitialPrompts from "@shared/services/ai/prompts";
-import {
-  ChatServiceAbstract,
-  ChatResult,
-  executeReturnType,
-} from "../chat.interfaces";
 import chatRepository from "@shared/repositories/chatRepository";
 import NotFoundError from "@shared/errors/notFoundError";
 import { v4 as uuidv4 } from "uuid";
 import messagesRepository from "@shared/repositories/messagesRepository";
-import textToSpeechClient from "@shared/services/textToSpeech";
-import fs, { WriteStream } from "fs";
-import { Stream } from "node:stream";
+import { textToSpeechClient } from "@shared/services/textToSpeech";
 import config from "@root/config";
+import { customAi } from "@shared/services/ai";
 
 const fileName = "chat.service";
 const logger = config.createLogger(fileName);
@@ -55,22 +42,22 @@ class ChatService {
       },
     ]);
 
-    this.startStreamingText(streamWriter);
+    await this.startStreamingText(streamWriter);
   }
 
-  private startStreamingText(streamWriter: DataStreamWriter) {
-    let ttsStream: DataStreamWriter;
-
+  private async startStreamingText(streamWriter: DataStreamWriter) {
+    logger.info("Starting streaming text");
     const systemPrompt =
       aiCharactersInitialPrompts.johnFromAmerica("my daily routine");
 
     const result = streamText({
-      model: google("gemini-1.5-pro"),
+      model: customAi("gpt-3.5-turbo"),
+
       system: systemPrompt,
       messages: this.messages,
 
       onFinish: async ({ text }) => {
-        this.ttsRealtime("Hello world".split(" "), streamWriter);
+        await this.generateAudio(text, streamWriter);
         await messagesRepository.saveMessages([
           {
             id: uuidv4(),
@@ -82,101 +69,30 @@ class ChatService {
           },
         ]);
       },
-      onChunk: async ({ chunk }) => {
-        console.log("chunk", chunk);
+      onChunk: async (chunk) => {
+        console.log(chunk);
       },
     });
 
-    result.mergeIntoDataStream(streamWriter);
+    return result.mergeIntoDataStream(streamWriter);
   }
 
-  private ttsRealtime(texts: string[], streamWriterr: DataStreamWriter) {
-    const ttsStream = textToSpeechClient.streamingSynthesize();
-    console.log(ttsStream);
-
-    // Write the response to a file, replace with your desired output stream
-
-    // const streamWriter = fs.createWriteStream("output.wav");
-
-    // The audio data is headerless LINEAR16 audio with a sample rate of 24000.
-    const sampleRate = 24000;
-    const numChannels = 1; // Mono audio
-    const byteRate = sampleRate * numChannels * 2;
-    const header = this.createWavHeader(sampleRate, numChannels, byteRate, 0);
-    // streamWriter.write(header);
-
-    // Handle the TTS response stream
-    ttsStream.on("data", (response: any) => {
-      console.log(response);
-
-    });
-
-    ttsStream.on("error", (err: any) => {
-      console.error("Error during Text-to-Speech:", err);
-      // streamWriterr.onError(err);
-    });
-
-    ttsStream.on("end", () => {
-      console.log("Finished streaming Text-to-Speech");
-    });
-
-    // Note: Only Journey voices support streaming for now.
-    ttsStream.write({
-      streamingConfig: {
-        voice: {
-          name: "en-us-Journey-O",
-          languageCode: "en-US",
-          ssmlGender: "NEUTRAL",
-        },
+  private async generateAudio(text: string, streamWriter: DataStreamWriter) {
+    const [response] = await textToSpeechClient.synthesizeSpeech({
+      input: { text: text },
+      voice: {
+        languageCode: "en-US",
+        ssmlGender: "FEMALE",
+        name: "en-US-Journey-F",
       },
+      audioConfig: { audioEncoding: "MP3" },
     });
 
-    // Stream the texts to TTS stream, replace with actual streaming texts
-    for (const text of texts) {
-      ttsStream.write({ input: { text: text } });
-    }
-
-    ttsStream.end();
+    streamWriter.writeMessageAnnotation({
+      type: "audio",
+      data: JSON.stringify(response.audioContent),
+    });
   }
-
-  private writeString(view: DataView, offset: number, str: string) {
-    for (let i = 0; i < str.length; i++) {
-      view.setUint8(offset + i, str.charCodeAt(i));
-    }
-  }
-
-  private createWavHeader(
-    sampleRate: number,
-    numChannels: number,
-    byteRate: number,
-    dataSize: number
-  ): Uint8Array {
-    const header = new ArrayBuffer(44);
-    const view = new DataView(header);
-
-    // RIFF chunk descriptor
-    this.writeString(view, 0, "RIFF");
-    view.setUint32(4, 36 + dataSize, true); // File size - 8
-    this.writeString(view, 8, "WAVE");
-
-    // fmt sub-chunk
-    this.writeString(view, 12, "fmt ");
-    view.setUint32(16, 16, true); // Subchunk1Size (16 for PCM)
-    view.setUint16(20, 1, true); // AudioFormat (1 for PCM)
-    view.setUint16(22, numChannels, true); // NumChannels
-    view.setUint32(24, sampleRate, true); // SampleRate
-    view.setUint32(28, byteRate, true); // ByteRate (SampleRate * NumChannels * BitsPerSample/8)
-    view.setUint16(32, numChannels * 2, true); // BlockAlign (NumChannels * BitsPerSample/8)
-    view.setUint16(34, 16, true); // BitsPerSample
-
-    // data sub-chunk
-    this.writeString(view, 36, "data");
-    view.setUint32(40, dataSize, true); // Subchunk2Size
-
-    return new Uint8Array(header);
-  }
-
-  //
 
   private extractLastUserMessage(): CoreMessage {
     const lastUserMessage = this.messages
