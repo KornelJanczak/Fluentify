8887dcf8c6af0535ca6b46daca5de7c11d1e25cf
KornelJanczak <korneljanczak10@gmail.com>
2024-12-29 22:05:36 +0100

    refactor: update import statement for config to use named exports

--- server/src/shared/services/db/index.ts
+++ server/src/shared/services/db/index.ts
@@ -1,7 +1,7 @@
 import { drizzle } from "drizzle-orm/neon-http";
 import { neon, neonConfig } from "@neondatabase/serverless";
 import type { NeonQueryFunction } from "@neondatabase/serverless";
-import config from "@root/config";
+import { config } from "@root/config";
 // import { config } from "dotenv";
 
 // config({ path: ".env" });
