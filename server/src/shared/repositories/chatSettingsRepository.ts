b00136570dfb22c0e232d226f7086a4540ffdb85
KornelJanczak <korneljanczak10@gmail.com>
2024-12-29 22:05:52 +0100

    refactor: add chatSettingsRepository with initial getChatSettings method

---
+++ server/src/shared/repositories/chatSettingsRepository.ts
@@ -0,0 +1,12 @@
+import { eq } from "drizzle-orm";
+import { db } from "../services/db";
+import { type ChatSettings, chatSettings } from "../services/db/schema";
+import DatabaseError from "../errors/dbError";
+
+const fileName = "chatSettingsRepository";
+
+class ChatSettingsRepository {
+   async getChatSettings(){}
+}
+
+export const chatSettingsRepository = new ChatSettingsRepository();
