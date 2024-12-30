87e9ff77784443d5176e3285bb38a7e881509537
KornelJanczak <korneljanczak10@gmail.com>
2024-12-30 20:11:52 +0100

    feat: add tutorProfile router to main application routes

--- server/src/router.ts
+++ server/src/router.ts
@@ -1,6 +1,7 @@
 import { Application } from "express";
 import authRouter from "@auth/auth.router";
 import chatRouter from "@chat/chat.router";
+import tutorProfileRouter from "@tutorProfile/tutorProfile.router";
 
 const BASE_PATH = "/api/v1";
 
@@ -8,6 +9,7 @@ export default (app: Application) => {
   const routes = () => {
     app.use(BASE_PATH, authRouter);
     app.use(BASE_PATH, chatRouter);
+    app.use(BASE_PATH, tutorProfileRouter);
   };
   routes();
 };
