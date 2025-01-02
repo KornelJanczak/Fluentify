56da386c23dfc139c1345666890544c397c2bf18
KornelJanczak <korneljanczak10@gmail.com>
2025-01-02 22:48:02 +0100

    refactor: integrate auth container into dependency injection setup

--- server/src/container.ts
+++ server/src/container.ts
@@ -1,27 +1,25 @@
-import { createContainer, InjectionMode, asValue } from "awilix";
+import { asValue } from "awilix";
 import { scopePerRequest } from "awilix-express";
 import { Application } from "express";
 import chatContainer from "@chat/chat.container";
+import authContainer from "@auth/auth.container";
 import { User } from "@shared/services/db/schema";
-
-// export const container = createContainer({
-//   injectionMode: InjectionMode.PROXY,
-// });
 
 export default (app: Application) => {
   const loadContainer = () => {
     app.use(scopePerRequest(chatContainer));
+    app.use(scopePerRequest(authContainer));
     // app.use(scopePerRequest(container));
     console.log("container", chatContainer.cradle);
 
-    app.use((req, res, next) => {
-      const user: User = req.user as User;
+    // app.use((req, res, next) => {
+    //   const user: User = req.user as User;
 
-      //@ts-ignore
-      req.container.register({
-        userId: asValue(user.id), // from auth middleware... IMAGINATION!! :D
-      });
-    });
+    //   //@ts-ignore
+    //   req.container.register({
+    //     userId: asValue(user.id), // from auth middleware... IMAGINATION!! :D
+    //   });
+    // });
 
     console.log(chatContainer);
   };
