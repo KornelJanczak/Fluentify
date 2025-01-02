9999555e4fdd79685c277026ab162710e4653a1c
KornelJanczak <korneljanczak10@gmail.com>
2025-01-02 22:48:32 +0100

    refactor: rename AuthControllerAbstract to IAuthController and add logger dependency interface

--- server/src/features/auth/auth.interfaces.ts
+++ server/src/features/auth/auth.interfaces.ts
@@ -1,7 +1,12 @@
 import { Request, Response, NextFunction } from "express";
+import { Logger } from "winston";
 
-export interface AuthControllerAbstract {
+export interface IAuthController {
   logOut(req: Request, res: Response, next: NextFunction): void;
   authStatus(req: Request, res: Response, next: NextFunction): void;
   authSession(req: Request, res: Response, __: NextFunction): void;
+}
+
+export interface IAuthControllerDependencies {
+  logger: Logger;
 }
