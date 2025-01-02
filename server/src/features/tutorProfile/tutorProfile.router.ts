93b57470cfa0e188013f0601c36d85433be9b037
KornelJanczak <korneljanczak10@gmail.com>
2025-01-02 22:49:09 +0100

    refactor: update tutorProfile.router to use dependency injection for controller

--- server/src/features/tutorProfile/tutorProfile.router.ts
+++ server/src/features/tutorProfile/tutorProfile.router.ts
@@ -1,22 +1,27 @@
 import { Router } from "express";
-import { tutorProfileController } from "./tutorProfile.controller";
+import tutorProfileContainer from "./tutorProfile.container";
 import authMiddleware from "@shared/middleware/authMiddleware";
 import { validateZodSchema } from "@shared/middleware/validateZodMiddleware";
 import { updateTutorProfileSchema } from "./tutorProfile.schema";
+import { ITutorProfileController } from "./tutorProfile.interfaces";
 
+const tutorProfileController =
+  tutorProfileContainer.resolve<ITutorProfileController>(
+    "tutorProfileController"
+  );
 const router = Router();
 
 router.get(
   "/tutorProfile",
   authMiddleware,
-  tutorProfileController.getTutorProfile
+  tutorProfileController.getTutorProfile.bind(tutorProfileController)
 );
 
 router.put(
   "/tutorProfile",
   authMiddleware,
   validateZodSchema(updateTutorProfileSchema),
-  tutorProfileController.updateTutorProfile
+  tutorProfileController.updateTutorProfile.bind(tutorProfileController)
 );
 
 export default router;
