515025de579cd0b1555637927849f161b28dfa70
KornelJanczak <korneljanczak10@gmail.com>
2025-01-02 22:48:35 +0100

    refactor: update auth router to use dependency injection for authController and bind methods

--- server/src/features/auth/auth.router.ts
+++ server/src/features/auth/auth.router.ts
@@ -1,31 +1,39 @@
 import { Router } from "express";
 import passport from "passport";
-import authController from "./auth.controller";
 import authMiddleware from "@shared/middleware/authMiddleware";
+import { IAuthController } from "./auth.interfaces";
+import { scope } from "./strategies/google-strategy";
+import authContainer from "./auth.container";
+import { config } from "@root/config";
 
+const authController = authContainer.resolve<IAuthController>("authController");
 const router = Router();
 
-const scope = [
-  "email",
-  "profile",
-  "openid",
-  "https://www.googleapis.com/auth/cloud-platform",
-];
-//@ts-ignore
-router.get("/auth/status", authController.authStatus);
-//@ts-ignore
-router.get("/auth/session", authMiddleware, authController.authSession);
-router.get("/auth/logout", authMiddleware, authController.logOut);
+router.get("/auth/status", authController.authStatus.bind(authController));
+
+router.get(
+  "/auth/session",
+  authMiddleware,
+  authController.authSession.bind(authController)
+);
+
+router.get(
+  "/auth/logout",
+  authMiddleware,
+  authController.logOut.bind(authController)
+);
+
 router.get(
   "/auth/google",
   passport.authenticate("google", {
     scope,
   })
 );
+
 router.get(
   "/auth/callback/google",
   passport.authenticate("google", {
-    successRedirect: process.env.CLIENT_URL,
+    successRedirect: config.CLIENT_URL,
   })
 );
 
