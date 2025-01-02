251d35d5544865fd25e96e0c5c87d2ce1c80f538
KornelJanczak <korneljanczak10@gmail.com>
2025-01-02 22:48:29 +0100

    refactor: implement logging in AuthController and update dependency injection

--- server/src/features/auth/auth.controller.ts
+++ server/src/features/auth/auth.controller.ts
@@ -1,23 +1,37 @@
 import { Request, Response, NextFunction } from "express";
 import AuthenticationError from "@shared/errors/authenticationError";
 import { User } from "@shared/services/db/schema";
-import { AuthControllerAbstract } from "./auth.interfaces";
+import {
+  IAuthController,
+  IAuthControllerDependencies,
+} from "./auth.interfaces";
 import HTTP_STATUS from "http-status-codes";
+import { Logger } from "winston";
 
-const fileName = "auth.controller";
+class AuthController implements IAuthController {
+  private readonly fileName = "auth.controller";
+  private readonly logger: Logger;
 
-class AuthController implements AuthControllerAbstract {
+  constructor({ logger }: IAuthControllerDependencies) {
+    this.logger = logger;
+  }
+
   public logOut(req: Request, res: Response, next: NextFunction) {
     req.logout((error) => {
       if (error) {
         next(
           new AuthenticationError({
-            fileName,
+            fileName: this.fileName,
             service: "logOut",
             ...error,
           })
         );
       } else {
+        this.logger.info({
+          service: "logOut",
+          message: `User has been logged out`,
+        });
+
         return res
           .status(HTTP_STATUS.OK)
           .json({ message: "Logged out successfully" });
@@ -27,12 +41,15 @@ class AuthController implements AuthControllerAbstract {
 
   public authStatus(req: Request, res: Response, next: NextFunction) {
     const currentUser: User = req.user as User;
-    console.log("AuthStatusController - User: ", currentUser.id);
+    this.logger.info({
+      service: "authStatus",
+      message: `User is authenticated ${currentUser.id}`,
+    });
 
     if (!currentUser) {
       next(
         new AuthenticationError({
-          fileName,
+          fileName: this.fileName,
           service: "authStatus",
           message: "User is not authenticated",
         })
@@ -46,5 +63,4 @@ class AuthController implements AuthControllerAbstract {
   }
 }
 
-const authController = new AuthController();
-export default authController;
+export default AuthController;
