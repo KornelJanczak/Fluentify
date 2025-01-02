0b0013691b2e0953149bcfbaacae1ee2c91bdbd8
KornelJanczak <korneljanczak10@gmail.com>
2025-01-02 22:48:48 +0100

    refactor: update chat interfaces to include userId and tutorProfileRepository for enhanced functionality

--- server/src/features/chat/chat.interfaces.ts
+++ server/src/features/chat/chat.interfaces.ts
@@ -1,8 +1,9 @@
 import { StreamTextResult, CoreTool, CoreMessage } from "ai";
-import { Request, Response, NextFunction } from "express";
+import { Request, Response } from "express";
 import * as googleCloud from "@google-cloud/text-to-speech";
 import { IChatRepository } from "@shared/repositories/chatRepository";
 import { IMessagesRepository } from "@shared/repositories/messagesRepository";
+import { ITutorProfileRepository } from "@shared/repositories/tutorProfileRepository";
 import { Logger } from "winston";
 
 export type ChatResult = StreamTextResult<Record<string, CoreTool<any, any>>>;
@@ -34,11 +35,19 @@ export interface IMainServiceConstructorProps {
 }
 
 export interface IChatStreamService {
-  startChatStream(
-    res: Response,
-    chatId: string,
-    messages: CoreMessage[]
-  ): Promise<void>;
+  startChatStream({
+    res,
+    chatId,
+    messages,
+    userId,
+  }: IChatRequest): Promise<void>;
+}
+
+export interface IChatRequest {
+  res: Response;
+  chatId: string;
+  messages: CoreMessage[];
+  userId: string;
 }
 
 export interface IChatStreamServiceDependencies {
@@ -52,12 +61,13 @@ export interface IChatStreamServiceDependencies {
 }
 
 export interface IAudioGeneratorService {
-  generateAudio(text: string): Promise<IAudioContent>;
+  generateAudio(text: string, userId: string): Promise<IAudioContent>;
 }
 
 export interface IAudioGeneratorServiceDependencies {
   userId: string;
   logger: Logger;
+  tutorProfileRepository: ITutorProfileRepository;
 }
 
 export interface IGenerateAudioRequest
