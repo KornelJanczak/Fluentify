ceea62d975e7b272c219610c1a20b17dc5926fdd
KornelJanczak <korneljanczak10@gmail.com>
2025-01-02 22:48:38 +0100

    feat: add Google authentication strategy with user serialization and deserialization

---
+++ server/src/features/auth/strategies/google-strategy.ts
@@ -0,0 +1,99 @@
+import passport from "passport";
+import { Strategy, type StrategyOptions } from "passport-google-oauth20";
+import { User } from "../../../shared/services/db/schema";
+import UserRepository from "@shared/repositories/userRepository";
+import { config } from "@root/config";
+import AuthenticationError from "../../../shared/errors/authenticationError";
+
+const userRepository = new UserRepository();
+const logger = config.createLogger("googleStrategy");
+
+export const scope = [
+  "profile",
+  "email",
+  "openid",
+  "https://www.googleapis.com/auth/cloud-platform",
+];
+
+const strategyOptions: StrategyOptions = {
+  clientID: config.GOOGLE_CLIENT_ID,
+  clientSecret: config.GOOGLE_CLIENT_SECRET,
+  callbackURL: config.GOOGLE_CALLBACK_URL,
+  passReqToCallback: false,
+  scope,
+};
+
+passport.serializeUser(({ id }: User, done) => {
+  logger.info({
+    message: `User has been serialized: ${id}`,
+    service: "serializeUser",
+  });
+  return done(null, id);
+});
+
+passport.deserializeUser(async (id: string, done) => {
+  try {
+    const currentUser = await userRepository.getById(id);
+
+    console.log(currentUser);
+
+    if (!currentUser) return done(null, false);
+
+    logger.info({
+      message: `User has been deserialized: ${currentUser.email}`,
+      service: "deserializeUser",
+    });
+
+    return done(null, currentUser);
+  } catch (err) {
+    return done(
+      new AuthenticationError({
+        message: err.message,
+        stack: err.stack,
+        service: "deserializeUser",
+      }),
+      null
+    );
+  }
+});
+
+export default passport.use(
+  new Strategy(strategyOptions, async (_, __, profile, done) => {
+    const account = profile._json;
+    let user: User;
+
+    try {
+      const existingUser: User = await userRepository.getByEmail(account.email);
+
+      if (existingUser) {
+        user = existingUser;
+      } else {
+        const newUser: User = {
+          id: account.sub,
+          email: account.email,
+          imagePath: account.picture,
+          role: "user",
+          subscriptionExpiryDate: "2024-11-17",
+          studingLanguageLevel: "B1",
+          nativeLanguage: "PL",
+        };
+
+        user = await userRepository.create(newUser);
+      }
+
+      logger.info({
+        message: `User ${user.email} has been authenticated`,
+        service: "googleStrategy",
+      });
+      return done(null, user);
+    } catch (err) {
+      return done(
+        new AuthenticationError({
+          message: err.message,
+          stack: err.stack,
+          service: "googleStrategy",
+        })
+      );
+    }
+  })
+);
