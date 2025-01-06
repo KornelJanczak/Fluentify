5d87f33d9262daca6ca5c05eb1be316f29edf540
KornelJanczak <korneljanczak10@gmail.com>
2025-01-06 23:16:05 +0100

    refactor: restructure TopicPromptBase class to improve topic handling and additional rules management

--- server/src/features/chat/chat.services/topicPrompt.service/topicPromptBase.ts
+++ server/src/features/chat/chat.services/topicPrompt.service/topicPromptBase.ts
@@ -1,42 +1,23 @@
 import { Logger } from "winston";
 
 abstract class TopicPromptBase {
-  protected abstract readonly topic: string;
-  protected abstract readonly logger: Logger;
-  protected readonly category: string;
-  protected topicsRules = [
-    {
-      category: "Chat about anything",
-      topics: [
-        {
-          topic: "Anything",
-          additonalRules: ` 
-            - You can talk about anything you want
-            - You can ask student about anything       
-            - If student is not talking you can ask him about his day
-            - You can ask student about his hobbies
-            - If student start some topic let him talk about it and ask questions about it    
-          `,
-        },
-        {
-          topic: "One of my interests",
-          additonalRules: "",
-        },
-        {
-          topic: "My goals & aspirations",
-          additonalRules: "",
-        },
-      ],
-    },
-  ];
+  protected abstract readonly category: string;
+  protected abstract readonly topics: {
+    topic: string;
+    additionalRules: string;
+  }[];
+  protected readonly topic: string;
+  protected readonly logger: Logger;
 
-  constructor(category: string) {
-    this.category = category;
+  constructor(topic: string, logger: Logger) {
+    this.topic = topic;
+    this.logger = logger;
   }
 
-  abstract generateTopicPrompt(): string;
-
-  protected abstract mergeBasePromptWithTopicPrompt(): string;
+  protected chooseAdditionalRules(): string {
+    return this.topics.find(({ topic }) => topic === this.topic)
+      .additionalRules;
+  }
 }
 
 export default TopicPromptBase;
