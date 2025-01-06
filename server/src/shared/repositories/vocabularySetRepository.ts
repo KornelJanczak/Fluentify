d3b2a8387299379fc08f6eee2112f827932b8a62
KornelJanczak <korneljanczak10@gmail.com>
2025-01-06 23:24:15 +0100

    feat: implement VocabularySetRepository for managing vocabulary sets

---
+++ server/src/shared/repositories/vocabularySetRepository.ts
@@ -0,0 +1,48 @@
+import { type VocabularySet, vocabularySets } from "@shared/services/db/schema";
+import { db } from "@shared/services/db";
+import DatabaseError from "@shared/errors/dbError";
+import { eq } from "drizzle-orm";
+
+interface IVocabularySetRepository {
+  create(newItem: VocabularySet): Promise<VocabularySet>;
+  getByUserId(userId: string): Promise<VocabularySet[]>;
+}
+
+class VocabularySetRepository implements IVocabularySetRepository {
+  private readonly fileName = "vocabularySetRepository";
+  async create(newItem: Omit<VocabularySet, "id">): Promise<VocabularySet> {
+    try {
+      const [createdItem] = await db
+        .insert(vocabularySets)
+        .values(newItem)
+        .returning();
+
+      return createdItem;
+    } catch (error) {
+      throw new DatabaseError({
+        fileName: this.fileName,
+        service: "create",
+        message: error.message,
+        stack: error.stack,
+      });
+    }
+  }
+
+  async getByUserId(userId: string): Promise<VocabularySet[]> {
+    try {
+      return await db
+        .select()
+        .from(vocabularySets)
+        .where(eq(vocabularySets.userId, userId));
+    } catch (error) {
+      throw new DatabaseError({
+        fileName: this.fileName,
+        service: "getByUserId",
+        message: error.message,
+        stack: error.stack,
+      });
+    }
+  }
+}
+
+export default VocabularySetRepository;
