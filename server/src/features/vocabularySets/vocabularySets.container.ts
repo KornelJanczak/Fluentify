0ef94d27cd8fda135f59922a6b440a643c69f1b6
KornelJanczak <korneljanczak10@gmail.com>
2025-01-07 23:32:19 +0100

    feat: add dependency injection container for VocabularySetsController and repository

---
+++ server/src/features/vocabularySets/vocabularySets.container.ts
@@ -0,0 +1,22 @@
+import { createContainer, asClass, InjectionMode, asFunction } from "awilix";
+import VocabularySetsController from "./vocabularySets.controller";
+import VocabularySetRepository from "@shared/repositories/vocabularySetRepository";
+import { config } from "@root/config";
+
+const container = createContainer({
+  injectionMode: InjectionMode.PROXY,
+});
+
+container.register({
+  vocabularySetsController: asClass(VocabularySetsController)
+    .singleton()
+    .scoped(),
+  vocabularySetRepository: asClass(VocabularySetRepository)
+    .singleton()
+    .scoped(),
+  logger: asFunction(() => config.createLogger("vocabularySetLogger"))
+    .singleton()
+    .scoped(),
+});
+
+export default container;
