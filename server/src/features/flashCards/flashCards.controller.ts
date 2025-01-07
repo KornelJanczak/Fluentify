80a1bdcc28baf5d3915dc2578877d08702d8844e
KornelJanczak <korneljanczak10@gmail.com>
2025-01-07 23:32:25 +0100

    feat: implement FlashCardsController with methods to create and retrieve flash cards

---
+++ server/src/features/flashCards/flashCards.controller.ts
@@ -0,0 +1,141 @@
+import {
+  IFlashCardsController,
+  IFlashCardsControllerDependencies,
+} from "./flashCards.interfaces";
+import { Request, Response, NextFunction } from "express";
+import HTTP_STATUS from "http-status-codes";
+import NotFoundError from "@shared/errors/notFoundError";
+import { Logger } from "winston";
+import { User } from "@shared/services/db/schema";
+import { IFlashCardRepository } from "@shared/repositories/flashCardRepository";
+
+class FlashCardsController implements IFlashCardsController {
+  private readonly flashCardRepository: IFlashCardRepository;
+  private readonly fileName = "FlashCardsController";
+  private readonly logger: Logger;
+
+  constructor({
+    flashCardRepository,
+    logger,
+  }: IFlashCardsControllerDependencies) {
+    this.flashCardRepository = flashCardRepository;
+    this.logger = logger;
+  }
+
+  async createFlashCard(req: Request, res: Response, next: NextFunction) {
+    this.logger.info({
+      message: "Creating flash card...",
+      fileName: this.fileName,
+      service: "createFlashCard",
+    });
+
+    const { body } = req;
+    const user: User = req.user as User;
+    const newItem = { ...body, userId: user.id };
+
+    const createdFlashCard = await this.flashCardRepository.create(newItem);
+
+    if (!createdFlashCard) {
+      next(
+        new NotFoundError({
+          message: "Flash card not created",
+          fileName: this.fileName,
+          service: "createFlashCard",
+        })
+      );
+    }
+
+    return res.status(HTTP_STATUS.OK).json(createdFlashCard);
+  }
+
+  async getAllFlashCardsByVocabularySetId(
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ) {
+    this.logger.info({
+      message: "Getting all flash cards by vocabulary set ID...",
+      fileName: this.fileName,
+      service: "getAllFlashCardsByVocabularySetId",
+    });
+
+    const { vocabularySetId } = req.params;
+
+    const flashCards =
+      await this.flashCardRepository.getFlashCardsByVocabularySetId(
+        vocabularySetId
+      );
+
+    if (!flashCards) {
+      next(
+        new NotFoundError({
+          message: "Flash cards not found for this vocabulary set",
+          fileName: this.fileName,
+          service: "getAllFlashCardsByVocabularySetId",
+        })
+      );
+    }
+
+    return res.status(HTTP_STATUS.OK).json(flashCards);
+  }
+
+  //   async updateFlashCard(req: Request, res: Response, next: NextFunction) {
+  //     this.logger.info({
+  //       message: "Updating flash card...",
+  //       fileName: this.fileName,
+  //       service: "updateFlashCard",
+  //     });
+
+  //     const { body } = req;
+  //     const { flashCardId } = req.params;
+  //     const user: User = req.user as User;
+
+  //     const updatedFlashCard = await this.flashCardRepository.update(
+  //       flashCardId,
+  //       body,
+  //       user.id
+  //     );
+
+  //     if (!updatedFlashCard) {
+  //       next(
+  //         new NotFoundError({
+  //           message: "Flash card not updated",
+  //           fileName: this.fileName,
+  //           service: "updateFlashCard",
+  //         })
+  //       );
+  //     }
+
+  //     return res.status(HTTP_STATUS.OK).json(updatedFlashCard);
+  //   }
+
+  //   async deleteFlashCard(req: Request, res: Response, next: NextFunction) {
+  //     this.logger.info({
+  //       message: "Deleting flash card...",
+  //       fileName: this.fileName,
+  //       service: "deleteFlashCard",
+  //     });
+
+  //     const { flashCardId } = req.params;
+  //     const user: User = req.user as User;
+
+  //     const deletedFlashCard = await this.flashCardRepository.delete(
+  //       flashCardId,
+  //       user.id
+  //     );
+
+  //     if (!deletedFlashCard) {
+  //       next(
+  //         new NotFoundError({
+  //           message: "Flash card not deleted",
+  //           fileName: this.fileName,
+  //           service: "deleteFlashCard",
+  //         })
+  //       );
+  //     }
+
+  //     return res.status(HTTP_STATUS.OK).json(deletedFlashCard);
+  //   }
+}
+
+export default FlashCardsController;
