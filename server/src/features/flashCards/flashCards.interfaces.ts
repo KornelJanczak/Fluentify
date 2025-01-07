f71050eb17a1cf6017d13e6265e3c98aa8040212
KornelJanczak <korneljanczak10@gmail.com>
2025-01-07 23:32:28 +0100

    feat: add IFlashCardsController interface and its dependencies

---
+++ server/src/features/flashCards/flashCards.interfaces.ts
@@ -0,0 +1,31 @@
+import { IFlashCardRepository } from "@shared/repositories/flashCardRepository";
+import { Request, Response, NextFunction } from "express";
+import { Logger } from "winston";
+
+export interface IFlashCardsController {
+  createFlashCard(
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Promise<Response>;
+  getAllFlashCardsByVocabularySetId(
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Promise<Response>;
+//   updateFlashCard(
+//     req: Request,
+//     res: Response,
+//     next: NextFunction
+//   ): Promise<Response>;
+//   deleteFlashCard(
+//     req: Request,
+//     res: Response,
+//     next: NextFunction
+//   ): Promise<Response>;
+}
+
+export interface IFlashCardsControllerDependencies {
+  flashCardRepository: IFlashCardRepository;
+  logger: Logger;
+}
