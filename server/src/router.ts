9378c08c5baeca136a97a5a1636c3c7d7fed919c
KornelJanczak <korneljanczak10@gmail.com>
2025-01-07 23:31:49 +0100

    feat: add vocabularySetRouter to the main application routes

--- server/src/router.ts
+++ server/src/router.ts
@@ -2,6 +2,7 @@ import { Application } from "express";
 import authRouter from "@auth/auth.router";
 import chatRouter from "@chat/chat.router";
 import tutorProfileRouter from "@tutorProfile/tutorProfile.router";
+import vovabularySetRouter from "@vocabularySets/vocabularySets.router";
 
 const BASE_PATH = "/api/v1";
 
@@ -10,6 +11,7 @@ export default (app: Application) => {
     app.use(BASE_PATH, authRouter);
     app.use(BASE_PATH, chatRouter);
     app.use(BASE_PATH, tutorProfileRouter);
+    app.use(BASE_PATH, vovabularySetRouter);
   };
   routes();
 };
