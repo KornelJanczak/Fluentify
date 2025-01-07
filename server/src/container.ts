eff32b11a2a991b0fe9e8d65a4f431ccca1b200c
KornelJanczak <korneljanczak10@gmail.com>
2025-01-07 23:31:46 +0100

    feat: integrate vocabularySetContainer into the application container

--- server/src/container.ts
+++ server/src/container.ts
@@ -1,27 +1,14 @@
-import { asValue } from "awilix";
 import { scopePerRequest } from "awilix-express";
 import { Application } from "express";
 import chatContainer from "@chat/chat.container";
 import authContainer from "@auth/auth.container";
-import { User } from "@shared/services/db/schema";
+import vocabularySetContainer from "@root/features/vocabularySets/vocabularySets.container";
 
 export default (app: Application) => {
   const loadContainer = () => {
     app.use(scopePerRequest(chatContainer));
     app.use(scopePerRequest(authContainer));
-    // app.use(scopePerRequest(container));
-    console.log("container", chatContainer.cradle);
-
-    // app.use((req, res, next) => {
-    //   const user: User = req.user as User;
-
-    //   //@ts-ignore
-    //   req.container.register({
-    //     userId: asValue(user.id), // from auth middleware... IMAGINATION!! :D
-    //   });
-    // });
-
-    console.log(chatContainer);
+    app.use(scopePerRequest(vocabularySetContainer));
   };
   loadContainer();
 };
