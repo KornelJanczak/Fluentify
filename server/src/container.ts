2a68323aac7a5d2d2026c9558b1f017918bed391
KornelJanczak <korneljanczak10@gmail.com>
2025-01-08 23:44:26 +0100

    feat: add flashCards container to dependency injection setup

--- server/src/container.ts
+++ server/src/container.ts
@@ -1,14 +1,16 @@
 import { scopePerRequest } from "awilix-express";
-import { Application } from "express";
+import { type Application } from "express";
 import chatContainer from "@chat/chat.container";
 import authContainer from "@auth/auth.container";
-import vocabularySetContainer from "@root/features/vocabularySets/vocabularySets.container";
+import vocabularySetContainer from "@vocabularySets/vocabularySets.container";
+import flashCardsContainer from "@flashCards/flashCards.container";
 
 export default (app: Application) => {
   const loadContainer = () => {
     app.use(scopePerRequest(chatContainer));
     app.use(scopePerRequest(authContainer));
     app.use(scopePerRequest(vocabularySetContainer));
+    app.use(scopePerRequest(flashCardsContainer));
   };
   loadContainer();
 };
