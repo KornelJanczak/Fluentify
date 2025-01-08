f4ec832db4c9ad78dba8fecb1e90144d5f39a539
KornelJanczak <korneljanczak10@gmail.com>
2025-01-08 23:44:49 +0100

    feat: add dependency injection container for FlashCardsController and FlashCardRepository

---
+++ server/src/features/flashCards/flashCards.container.ts
@@ -0,0 +1,18 @@
+import { createContainer, asClass, InjectionMode, asFunction } from "awilix";
+import FlashCardsController from "./flashCards.controller";
+import FlashCardRepository from "@shared/repositories/flashCardRepository";
+import { config } from "@root/config";
+
+const container = createContainer({
+  injectionMode: InjectionMode.PROXY,
+});
+
+container.register({
+  flashCardsController: asClass(FlashCardsController).singleton().scoped(),
+  flashCardRepository: asClass(FlashCardRepository).singleton().scoped(),
+  logger: asFunction(() => config.createLogger("flashCardLogger"))
+    .singleton()
+    .scoped(),
+});
+
+export default container;
