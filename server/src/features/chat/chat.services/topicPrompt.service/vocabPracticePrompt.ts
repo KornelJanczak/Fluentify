f2cce8eab96fad5d2cfe4bb64f66a5aaf740a86f
KornelJanczak <korneljanczak10@gmail.com>
2025-01-08 23:44:46 +0100

    feat: enhance VocabPracticePrompt to include flashCard repository dependency and improve flash card retrieval methods

--- server/src/features/chat/chat.services/topicPrompt.service/vocabPracticePrompt.ts
+++ server/src/features/chat/chat.services/topicPrompt.service/vocabPracticePrompt.ts
@@ -1,13 +1,59 @@
+import { IVocabPraticePromptDependencies } from "@chat/chat.interfaces";
 import TopicPromptBase from "./topicPromptBase";
 import { Logger } from "winston";
+import { IFlashCardRepository } from "@shared/repositories/flashCardRepository";
+import NotFoundError from "@shared/errors/notFoundError";
+import { FlashCard } from "@shared/services/db/schema";
 
 class VocabPracticePrompt extends TopicPromptBase {
+  private flashCardRepository: IFlashCardRepository;
   protected category = "Vocabulary practice";
   protected topic: string;
-  protected topics = [];
+  protected topics = [
+    {
+      topic: "Practice vocabulary word by word",
+      additionalRules: `
+      - You should 
+      - If student doesn't know the translation you should say it
+      - You should ask student to repeat the word after you
+      - You should ask student to use the word in a sentence
+    `,
+    },
+  ];
 
-  constructor(topic: string, logger: Logger) {
-    super(topic, logger);
+  constructor(dependencies: IVocabPraticePromptDependencies) {
+    super(dependencies);
+  }
+
+  private formatFlashCardsIntoStrings(flashCards: FlashCard[]): string[] {
+    return flashCards.map(
+      ({ definition, translation }) => `${definition} - ${translation}`
+    );
+  }
+
+  private async getFlashCardsByVocabularySetId(
+    vocabularySetId: string
+  ): Promise<FlashCard[]> {
+    const service = "getFlashCardsByVocabularySetId";
+
+    // this.logger.info({
+    //   message: "Getting flash cards by vocabulary set id",
+    //   service,
+    // });
+
+    const flashCards =
+      await this.flashCardRepository.getFlashCardsByVocabularySetId(
+        vocabularySetId
+      );
+
+    if (!flashCards) {
+      throw new NotFoundError({
+        service,
+        message: "Flash cards not found",
+      });
+    }
+
+    return flashCards;
   }
 }
 
