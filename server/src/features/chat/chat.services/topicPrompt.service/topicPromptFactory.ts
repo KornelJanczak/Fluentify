397c68f438291fdfaacaacf7d0900ce4aeae6ce2
KornelJanczak <korneljanczak10@gmail.com>
2025-01-11 20:21:57 +0100

    feat: refactor TopicPromptFactory to implement ITopicPromptFactory and simplify prompt creation

--- server/src/features/chat/chat.services/topicPrompt.service/topicPromptFactory.ts
+++ server/src/features/chat/chat.services/topicPrompt.service/topicPromptFactory.ts
@@ -2,26 +2,14 @@ import TopicPromptBase from "./topicPromptBase";
 import VocabPracticePrompt from "./vocabPracticePrompt";
 import AnyTopicPrompt from "./anyTopicPrompt";
 import { IFlashCardRepository } from "@shared/repositories/flashCardRepository";
+import { ITopicPromptFactory } from "@chat/chat.interfaces";
 
-class TopicPromptFactory {
-  private readonly flashCardRepository: IFlashCardRepository;
-  constructor({
-    flashCardRepository,
-  }: {
-    flashCardRepository: IFlashCardRepository;
-  }) {
-    this.flashCardRepository = flashCardRepository;
-  }
-
-  //   private readonly category: string;
-  //   private readonly topic: string;
-
-  createPrompt(category: string, topic: string): TopicPromptBase {
+class TopicPromptFactory implements ITopicPromptFactory {
+  createTopicPrompt(category: string, topic: string): TopicPromptBase {
     switch (category) {
       case "Vocabulary practice":
         return new VocabPracticePrompt({
           topic,
-          flashCardRepository: this.flashCardRepository,
         });
       case "Chat about anything":
         return new AnyTopicPrompt({ topic });
@@ -30,3 +18,5 @@ class TopicPromptFactory {
     }
   }
 }
+
+export default TopicPromptFactory;
