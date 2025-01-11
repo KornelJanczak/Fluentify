dd1d60eee8a8503da6f8a23898b182aa3777395e
KornelJanczak <korneljanczak10@gmail.com>
2025-01-11 20:21:45 +0100

    feat: add TopicPromptFactory and FlashCardRepository to chat container

--- server/src/features/chat/chat.container.ts
+++ server/src/features/chat/chat.container.ts
@@ -6,12 +6,14 @@ import {
   asValue,
 } from "awilix";
 import ChatController from "./chat.controller";
-import MessagesRepository from "@shared/repositories/messagesRepository";
-import ChatRepository from "@shared/repositories/chatRepository";
 import ChatStreamService from "./chat.services/chatStream.service";
 import AudioGeneratorService from "./chat.services/audioGenerator.service";
-import { config } from "@root/config";
+import TopicPromptFactory from "./chat.services/topicPrompt.service/topicPromptFactory";
+import ChatRepository from "@shared/repositories/chatRepository";
+import MessagesRepository from "@shared/repositories/messagesRepository";
 import TutorProfileRepository from "@shared/repositories/tutorProfileRepository";
+import FlashCardRepository from "@shared/repositories/flashCardRepository";
+import { config } from "@root/config";
 
 const container = createContainer({
   injectionMode: InjectionMode.PROXY,
@@ -21,13 +23,14 @@ container.register({
   chatController: asClass(ChatController).singleton().scoped(),
   chatStreamService: asClass(ChatStreamService).singleton().scoped(),
   audioGeneratorService: asClass(AudioGeneratorService).singleton().scoped(),
+  topicPromptFactory: asClass(TopicPromptFactory).singleton().scoped(),
   chatRepository: asClass(ChatRepository).singleton().scoped(),
   messagesRepository: asClass(MessagesRepository).singleton().scoped(),
   tutorProfileRepository: asClass(TutorProfileRepository).singleton().scoped(),
+  flashCardRepository: asClass(FlashCardRepository).singleton().scoped(),
   logger: asFunction(() => config.createLogger("chatService"))
     .singleton()
     .scoped(),
-  systemPrompt: asValue(""),
 });
 
 export default container;
