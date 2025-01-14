cbe63406168e9952e8684abb60bc38f37d4f53af
KornelJanczak <korneljanczak10@gmail.com>
2025-01-14 22:27:03 +0100

    refactor: remove TutorProfileRepository and add SystemPromptService and TutorPromptService to chat container

--- server/src/features/chat/chat.container.ts
+++ server/src/features/chat/chat.container.ts
@@ -1,19 +1,14 @@
-import {
-  createContainer,
-  asClass,
-  InjectionMode,
-  asFunction,
-  asValue,
-} from "awilix";
+import { createContainer, asClass, InjectionMode, asFunction } from "awilix";
 import ChatController from "./chat.controller";
 import ChatStreamService from "./chat.services/chatStream.service";
 import AudioGeneratorService from "./chat.services/audioGenerator.service";
 import TopicPromptFactory from "./chat.services/topicPrompt.service/topicPromptFactory";
 import ChatRepository from "@shared/repositories/chatRepository";
 import MessagesRepository from "@shared/repositories/messagesRepository";
-import TutorProfileRepository from "@shared/repositories/tutorProfileRepository";
 import FlashCardRepository from "@shared/repositories/flashCardRepository";
 import { config } from "@root/config";
+import SystemPromptService from "./chat.services/systemPrompt.service";
+import TutorPromptService from "./chat.services/tutorPrompt.service";
 
 const container = createContainer({
   injectionMode: InjectionMode.PROXY,
@@ -23,10 +18,11 @@ container.register({
   chatController: asClass(ChatController).singleton().scoped(),
   chatStreamService: asClass(ChatStreamService).singleton().scoped(),
   audioGeneratorService: asClass(AudioGeneratorService).singleton().scoped(),
+  systemPromptService: asClass(SystemPromptService).singleton().scoped(),
+  tutorPromptService: asClass(TutorPromptService).singleton().scoped(),
   topicPromptFactory: asClass(TopicPromptFactory).singleton().scoped(),
   chatRepository: asClass(ChatRepository).singleton().scoped(),
   messagesRepository: asClass(MessagesRepository).singleton().scoped(),
-  tutorProfileRepository: asClass(TutorProfileRepository).singleton().scoped(),
   flashCardRepository: asClass(FlashCardRepository).singleton().scoped(),
   logger: asFunction(() => config.createLogger("chatService"))
     .singleton()
