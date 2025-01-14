23873b8bfbcef40738406d4659d1f0bba1cad62b
KornelJanczak <korneljanczak10@gmail.com>
2025-01-14 22:25:50 +0100

    refactor: remove unused dotenv configuration from db service

--- server/src/shared/services/db/index.ts
+++ server/src/shared/services/db/index.ts
@@ -2,9 +2,6 @@ import { drizzle } from "drizzle-orm/neon-http";
 import { neon, neonConfig } from "@neondatabase/serverless";
 import type { NeonQueryFunction } from "@neondatabase/serverless";
 import { config } from "@root/config";
-// import { config } from "dotenv";
-
-// config({ path: ".env" });
 
 neonConfig.fetchConnectionCache = true;
 const sql: NeonQueryFunction<boolean, boolean> = neon(
