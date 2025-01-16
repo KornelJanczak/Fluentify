29880ebfddab4f8cdaa2862704d6740d3038c88f
KornelJanczak <korneljanczak10@gmail.com>
2025-01-16 22:30:46 +0100

    feat: add Navbar component to Home page

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,8 +1,11 @@
 // client/src/app/page.tsx
 
+import Navbar from "@/components/navbar";
+
 export default function Home() {
   return (
     <main>
+      <Navbar />
       <h1 className="mt-10 text-4xl font-bold tracking-tight text-white sm:text-6xl">
         Home Page
       </h1>
