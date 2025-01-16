d042ed4de8f64709ed22de01be1ddab0be3880e2
KornelJanczak <korneljanczak10@gmail.com>
2025-01-16 22:31:05 +0100

    feat: add ServerAPI class for handling API requests with session cookies

---
+++ client/src/common/api/server-api.ts
@@ -0,0 +1,40 @@
+import { cookies } from "next/headers";
+import { RestHelper } from "./rest-helper";
+
+class ServerAPI extends RestHelper {
+  constructor() {
+    super(process.env.NEXT_PUBLIC_API_URL);
+  }
+
+  protected async interceptOptions(options: RequestInit): Promise<RequestInit> {
+    const token = await this.getSessionCookies();
+
+    if (token) {
+      options.headers = {
+        ...options.headers,
+        credentials: "include",
+        Cookie: token,
+      };
+    }
+
+    return options;
+  }
+
+  public async getSessionCookies() {
+    const cookieStore = await cookies();
+    const sessionId = cookieStore.get("fluentify-server-session").value;
+    const sessionSignature = cookieStore.get(
+      "fluentify-server-session.sig"
+    ).value;
+
+    const sessionCookie =
+      "fluentify-server-session=" +
+      sessionId +
+      "; fluentify-server-session.sig=" +
+      sessionSignature;
+
+    return sessionCookie;
+  }
+}
+
+export const serverApi = new ServerAPI();
