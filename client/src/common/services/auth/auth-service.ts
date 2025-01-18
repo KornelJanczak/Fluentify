a96ae4c78e6b1553f1f01b6b864057a7aa89d3e0
KornelJanczak <korneljanczak10@gmail.com>
2025-01-18 23:00:56 +0100

    feat: implement AuthService for user authentication and session management

---
+++ client/src/common/services/auth/auth-service.ts
@@ -0,0 +1,59 @@
+import { HttpError } from "@/common/api/rest-helper";
+import { ServerAPI, serverApi } from "@/common/api/server-api";
+import { clientApi } from "@/common/api/client-api";
+
+export const getUserKey = ["users", "me"];
+
+class AuthService {
+  serverApi: ServerAPI;
+
+  constructor(serverApi: ServerAPI) {
+    this.serverApi = serverApi;
+  }
+
+  async getUser() {
+    try {
+      return (
+        await this.serverApi.get<UserResponse>("/auth/session", {
+          next: { tags: [getUserKey.join()] },
+        })
+      ).data;
+    } catch (error) {
+      if (!(error instanceof HttpError)) {
+        throw error;
+      }
+
+      if (error.status === 401) {
+        return null;
+      }
+
+      throw error;
+    }
+  }
+
+  async logOut() {
+    try {
+      await this.serverApi.get("/auth/logout");
+    } catch (error) {
+      if (!(error instanceof HttpError)) {
+        throw error;
+      }
+    }
+  }
+}
+
+export const authService = new AuthService(serverApi);
+
+export type User = {
+  id: string;
+  email: string;
+  name: string;
+  imagePath: string;
+  role: string;
+  subscriptionExpiryDate: string;
+  studyingLanguageLevel: string;
+  nativeLanguage: string;
+  tutorId: string;
+};
+
+export type UserResponse = User | null;
