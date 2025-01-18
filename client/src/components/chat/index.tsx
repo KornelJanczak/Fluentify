e3c1232f01f0144cf66f1481906f1e3374b498fb
KornelJanczak <korneljanczak10@gmail.com>
2025-01-18 23:01:13 +0100

    feat: refactor Chat component to improve props handling and integrate audio playback

--- client/src/components/chat/index.tsx
+++ client/src/components/chat/index.tsx
@@ -1,21 +1,23 @@
 "use client";
+
+import type { Message } from "ai";
 import { useChat } from "ai/react";
+import { MultimodalInput } from "./multimodal-input";
+import { Messages } from "./messages";
+import { useAudioPlayer } from "@/common/hooks/use-audio";
 import { useRef } from "react";
 
 interface ChatProps {
-  chatId: string;
+  id: string;
   sessionCookie: string;
-  initialMessages: any;
+  initialMessages: Array<Message>;
 }
 
-export default function Chat({
-  chatId,
-  sessionCookie,
-  initialMessages,
-}: ChatProps) {
+export function Chat({ id, initialMessages, sessionCookie }: ChatProps) {
   const audioRef = useRef<HTMLAudioElement>(null);
-  const { messages, input, handleInputChange, handleSubmit } = useChat({
-    id: chatId,
+
+  const { messages, handleSubmit, input, setInput, isLoading, stop } = useChat({
+    id,
     initialMessages,
     api: `${process.env.NEXT_PUBLIC_API_URL}/chat`,
     credentials: "include",
@@ -23,50 +25,36 @@ export default function Chat({
       chatCategory: "Vocabulary practice",
       chatTopic: "Practice vocabulary word by word",
       vocabularySetId: "e884f64a-9b44-48a5-b15d-f0dd23d75877",
-      chatId,
+      chatId: id,
     },
     headers: {
       Cookie: sessionCookie,
     },
-    onFinish(message, options) {
-      //@ts-ignore
-      const audio = JSON.parse(message.annotations.at(-1).data);
-
-      console.log(audio);
-
-      const audioArray = new Uint8Array(audio.data);
-
-      const audioBlob = new Blob([audioArray], { type: "audio/wav" });
-
-      const audioUrl = URL.createObjectURL(audioBlob);
-
-      if (audioRef !== null) {
-        audioRef.current.src = audioUrl;
-        audioRef.current.play();
-      }
+    onFinish(message) {
+      useAudioPlayer({ message, audioRef });
     },
   });
 
   return (
-    <div className="flex flex-col w-full max-w-md py-24 mx-auto stretch">
-      {messages.map((m) => (
-        <div key={m.id}>
-          <div className="whitespace-pre-wrap ">
-            {m.role === "user" ? "User: " : "AI: "}
-            {m.content}
-          </div>
-          <audio controls={true} ref={audioRef} />
-        </div>
-      ))}
-
-      <form onSubmit={handleSubmit}>
-        <input
-          className="text-black fixed bottom-0 w-full max-w-md p-2 mb-8 border border-gray-300 rounded shadow-xl"
-          value={input}
-          placeholder="Say something..."
-          onChange={handleInputChange}
+    <>
+      <div className="flex flex-col min-w-0 h-dvh bg-background">
+        <Messages
+          isLoading={isLoading}
+          messages={messages}
+          audioRef={audioRef}
         />
-      </form>
-    </div>
+
+        <form className="flex mx-auto px-4 bg-background pb-4 md:pb-6 gap-2 w-full md:max-w-3xl">
+          <MultimodalInput
+            chatId={id}
+            input={input}
+            setInput={setInput}
+            handleSubmit={handleSubmit}
+            isLoading={isLoading}
+            stop={stop}
+          />
+        </form>
+      </div>
+    </>
   );
 }
