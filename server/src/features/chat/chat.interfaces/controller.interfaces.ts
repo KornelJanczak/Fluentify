426d9adce03f92bb606f7af24844816644b04b09
KornelJanczak <korneljanczak10@gmail.com>
2025-01-20 19:18:00 +0100

    feat: add NextFunction parameter to IChatController methods for improved middleware support

--- server/src/features/chat/chat.interfaces/controller.interfaces.ts
+++ server/src/features/chat/chat.interfaces/controller.interfaces.ts
@@ -1,13 +1,13 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import { IChatRepository } from "@shared/repositories/chatRepository";
 import { IMessagesRepository } from "@shared/repositories/messagesRepository";
 import { IChatStreamService } from "./chatStream.service.interfaces";
 
 export interface IChatController {
-  startChat(req: Request, res: Response): void;
-  createChat(req: Request, res: Response): void;
-  getChatById(req: Request, res: Response): void;
-  getMessagesByChatId(req: Request, res: Response): void;
+  startChat(req: Request, res: Response, next: NextFunction): void;
+  createChat(req: Request, res: Response, next: NextFunction): void;
+  getChatById(req: Request, res: Response, next: NextFunction): void;
+  getMessagesByChatId(req: Request, res: Response, next: NextFunction): void;
 }
 
 export interface IChatControllerDependencies {
