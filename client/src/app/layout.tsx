ea39ce1c8423d59dfa0b65de7f3af26bec565939
KornelJanczak <korneljanczak10@gmail.com>
2025-01-20 19:19:35 +0100

    refactor: integrate AuthProvider into RootLayout for session management

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,6 +1,8 @@
 import { Inter } from "next/font/google";
 import { ThemeProvider } from "@/common/providers/theme-provider";
 import "./globals.css";
+import AuthProvider from "@/common/providers/auth-provider";
+import { serverApi } from "@/common/api/server-api";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -9,19 +11,23 @@ export default async function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
+  const session = await serverApi.getSessionCookies();
+
   return (
     <html lang="en" suppressHydrationWarning>
       <head />
 
       <body className={inter.className}>
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="dark"
-          enableSystem
-          disableTransitionOnChange
-        >
-          {children}
-        </ThemeProvider>
+        <AuthProvider session={session}>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="dark"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {children}
+          </ThemeProvider>
+        </AuthProvider>
       </body>
     </html>
   );
