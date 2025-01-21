95872fd0c80585cde64f01bb730e67afbf07847b
KornelJanczak <korneljanczak10@gmail.com>
2025-01-21 19:48:24 +0100

    refactor: simplify user dropdown menu options by removing unused session handling and logout logic

--- client/src/components/nav/nav-user/user-dropdown-menu-options.tsx
+++ client/src/components/nav/nav-user/user-dropdown-menu-options.tsx
@@ -1,20 +1,15 @@
 "use client";
 
-import { clientApi } from "@/common/api/client-api";
-import { useLogout } from "@/common/services/auth/useLogout";
 import {
   DropdownMenuGroup,
   DropdownMenuItem,
   DropdownMenuSeparator,
 } from "@/components/ui/dropdown-menu";
-import { useSession, signIn, signOut } from "next-auth/react";
 import { BadgeCheck, Bell, CreditCard, LogOut, Sparkles } from "lucide-react";
 
+
+
 const UserDropdownMenuOptions = () => {
-  const { data: session } = useSession();
-
-  console.log(session);
-
   return (
     <>
       <DropdownMenuGroup>
@@ -39,13 +34,7 @@ const UserDropdownMenuOptions = () => {
         </DropdownMenuItem>
       </DropdownMenuGroup>
       <DropdownMenuSeparator />
-      <DropdownMenuItem
-        onClick={async () => {
-          console.log("logout");
-
-          await clientApi.get("/auth/logout");
-        }}
-      >
+      <DropdownMenuItem>
         <LogOut />
         Log out
       </DropdownMenuItem>
