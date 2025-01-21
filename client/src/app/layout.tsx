ecf517112db3a7bbe8958c470d5c09d9aa6bc15a
KornelJanczak <korneljanczak10@gmail.com>
2025-01-21 19:48:21 +0100

    refactor: remove AuthProvider from RootLayout for simplification

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,8 +1,6 @@
 import { Inter } from "next/font/google";
 import { ThemeProvider } from "@/common/providers/theme-provider";
 import "./globals.css";
-import AuthProvider from "@/common/providers/auth-provider";
-import { serverApi } from "@/common/api/server-api";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,23 +9,18 @@ export default async function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
-  const session = await serverApi.getSessionCookies();
-
   return (
     <html lang="en" suppressHydrationWarning>
       <head />
-
       <body className={inter.className}>
-        <AuthProvider session={session}>
-          <ThemeProvider
-            attribute="class"
-            defaultTheme="dark"
-            enableSystem
-            disableTransitionOnChange
-          >
-            {children}
-          </ThemeProvider>
-        </AuthProvider>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
