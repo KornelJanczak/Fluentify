d443b04d07028506eb272399d628ae067fdbfda7
KornelJanczak <korneljanczak10@gmail.com>
2025-01-26 20:39:00 +0100

    refactor: remove unused ClientAPI class and related imports

--- client/src/common/api/client-api.ts
+++
@@ -1,9 +0,0 @@
-import { RestHelper } from "./rest-helper";
-
-class ClientAPI extends RestHelper {
-  constructor() {
-    super("http://localhost:5000/api/v1");
-  }
-}
-
-export const clientApi = new ClientAPI();
