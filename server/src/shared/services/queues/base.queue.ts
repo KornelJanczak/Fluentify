3ecb8faf4bdfdeb7f50ba84edadbaa1da6038f4e
KornelJanczak <korneljanczak10@gmail.com>
2025-01-28 22:52:11 +0100

    feat: enhance BaseQueue with job processing and completion handling

--- server/src/shared/services/queues/base.queue.ts
+++ server/src/shared/services/queues/base.queue.ts
@@ -1,27 +1,66 @@
-import { Queue, Job } from "bull";
+import Queue, { Job, ProcessCallbackFunction } from "bull";
 import { Logger } from "winston";
 import { ExpressAdapter } from "@bull-board/express";
 import { createBullBoard } from "@bull-board/api";
 import { BullAdapter } from "@bull-board/api/bullAdapter";
 import { config } from "@root/config";
+import { Chat, Message } from "@services/db/schema";
 
-export interface IBaseQueueDependencies {
-  queue: Queue;
-  logger: Logger;
-  serverAdapter: ExpressAdapter;
-}
-
+type IBaseJobData = Message[];
+let bullAdapters: BullAdapter[] = [];
+export let serverAdapter: ExpressAdapter;
 abstract class BaseQueue {
-  protected queue: Queue;
-  protected bullAdapters: BullAdapter[];
-  protected serverAdapter: ExpressAdapter;
+  protected queue: Queue.Queue;
+  protected queueName: string;
   protected logger: Logger;
+  // private serverAdapter: ExpressAdapter;
+  // private static bullAdapters: BullAdapter[] = [];
 
-  constructor({ queue, serverAdapter }: IBaseQueueDependencies) {
-    this.queue = queue;
-    this.bullAdapters.push(new BullAdapter(this.queue));
-    this.serverAdapter = serverAdapter;
-    this.serverAdapter.setBasePath(config.BULL_BASE_PATH);
+  constructor(queueName: string) {
+    // this.queue = new Queue(queueName, `${config.REDIS_HOST}`);
+    // BaseQueue.bullAdapters.push(new BullAdapter(this.queue));
+    // BaseQueue.bullAdapters = [...new Set(BaseQueue.bullAdapters)];
+    // this.serverAdapter = new ExpressAdapter();
+    // this.serverAdapter.setBasePath("queues");
+
+    this.queue = new Queue(queueName, `${config.REDIS_HOST}`);
+    bullAdapters.push(new BullAdapter(this.queue));
+    bullAdapters = [...new Set(bullAdapters)];
+    serverAdapter = new ExpressAdapter();
+    serverAdapter.setBasePath("/queues");
+
+    createBullBoard({
+      queues: bullAdapters,
+      serverAdapter: serverAdapter,
+    });
+
+    this.queue.on("completed", (job: Job) => {
+      job.remove();
+    });
+
+    this.queue.on("global:completed", (jobId: string) => {
+      this.logger.info(`Job ${jobId} completed`);
+    });
+
+    this.queue.on("global:stalled", (jobId: string) => {
+      this.logger.info(`Job ${jobId} is stalled`);
+    });
+  }
+
+  protected addJob(name: string, data: IBaseJobData): void {
+    this.queue.add(name, data, {
+      attempts: 3,
+      backoff: { type: "fixed", delay: 5000 },
+    });
+  }
+
+  protected processJob(
+    name: string,
+    concurrency: number,
+    callback: ProcessCallbackFunction<void>
+  ): void {
+   
+    this.queue.process(name, concurrency, callback);
   }
 }
 
