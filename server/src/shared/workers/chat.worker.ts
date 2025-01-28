ab372df6321074cad8e3192e22d2259ac159a78d
KornelJanczak <korneljanczak10@gmail.com>
2025-01-28 22:49:29 +0100

    feat: enhance ChatWorker with improved error logging and interface definition

--- server/src/shared/workers/chat.worker.ts
+++ server/src/shared/workers/chat.worker.ts
@@ -1,20 +1,33 @@
-import { IChatRepository } from "@shared/repositories/chatRepository";
-import { IMessagesRepository } from "@shared/repositories/messagesRepository";
+import { IMessagesRepository } from "@shared/repositories/messages.repository";
 import { DoneCallback, Job } from "bull";
+import { log } from "console";
 import { Logger } from "winston";
 
-class ChatWorker {
-  private logger: Logger;
+export interface IChatWorker {
+  saveChatMessages(jobQueue: Job, done: DoneCallback): Promise<void>;
+}
 
-  private messageRepository: IMessagesRepository;
+class ChatWorker {
+  private readonly fileName = "chat.worker";
+  private readonly logger: Logger;
+  private readonly messageRepository: IMessagesRepository;
 
   async saveChatMessages(jobQueue: Job, done: DoneCallback) {
     try {
+      console.log("jobQueue", "CHAT WORKER");
+
       await this.messageRepository.saveMessages(jobQueue.data);
       jobQueue.progress(100);
+      this.logger.info({ message: "Chat messages saved", data: jobQueue.data });
       done(null, jobQueue.data);
     } catch (error) {
-      // done(new);
+      this.logger.error({
+        fileName: this.fileName,
+        service: "saveChatMessages",
+        message: error.message,
+        stack: error.stack,
+      });
+      done(error as Error);
     }
   }
 }
