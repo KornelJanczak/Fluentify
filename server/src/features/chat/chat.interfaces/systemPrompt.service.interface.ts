6410e0a79479c2cb9daafcc4f175785fa78cd545
KornelJanczak <korneljanczak10@gmail.com>
2025-01-28 22:50:32 +0100

    fix: update import path for IFlashCardRepository to match file naming convention

--- server/src/features/chat/chat.interfaces/systemPrompt.service.interface.ts
+++ server/src/features/chat/chat.interfaces/systemPrompt.service.interface.ts
@@ -1,11 +1,9 @@
-import { IFlashCardRepository } from "@shared/repositories/flashCardRepository";
+import { IFlashCardRepository } from "@shared/repositories/flashCard.repository";
 import { ITopicPromptFactory } from "./topicPrompt.service.interfaces";
 import { ITutorPromptService } from "./tutorPrompt.service.interfaces";
 
 export interface ISystemPromptService {
-  getSystemPrompt(
-    args: IGetSystemPromptArgs
-  ): Promise<string>;
+  getSystemPrompt(args: IGetSystemPromptArgs): Promise<string>;
 }
 
 export interface ISystemPromptDependencies {
