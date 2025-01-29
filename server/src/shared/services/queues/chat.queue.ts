ba03122c32a15f7f52eae4fcda9f2f5594ef1021
KornelJanczak <korneljanczak10@gmail.com>
2025-01-29 20:23:43 +0100

    feat: add logger dependency to ChatQueue for improved logging capabilities

--- server/src/shared/services/queues/chat.queue.ts
+++ server/src/shared/services/queues/chat.queue.ts
@@ -1,17 +1,21 @@
 import { IChatWorker } from "@shared/workers/chat.worker";
 import BaseQueue from "./base.queue";
 import { Chat, Message } from "@services/db/schema";
+import { Logger } from "winston";
 
 export interface IChatQueueDependencies {
   chatWorker: IChatWorker;
+  logger: Logger;
 }
 
 class ChatQueue extends BaseQueue {
+  protected fileName: string = "chat.queue";
   private readonly chatWorker: IChatWorker;
 
-  constructor({ chatWorker }: IChatQueueDependencies) {
+  constructor({ chatWorker, logger }: IChatQueueDependencies) {
     super("chat");
     this.chatWorker = chatWorker;
+    this.logger = logger;
     this.processJob("saveChatMessages", 5, this.chatWorker.saveChatMessages);
   }
 
