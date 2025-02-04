9c59c5052e91a7f01c7e7cb37e161ffd7272076b
KornelJanczak <korneljanczak10@gmail.com>
2025-02-04 23:39:40 +0100

    feat: enhance Redis connection handling with connectionGuard method and improved error management

--- server/src/shared/services/redis/base.cache.ts
+++ server/src/shared/services/redis/base.cache.ts
@@ -1,3 +1,4 @@
+import RedisError from "@shared/errors/redis.error";
 import { createClient } from "redis";
 import { Logger } from "winston";
 
@@ -9,18 +10,29 @@ export interface IBaseCacheDependencies {
 }
 
 export abstract class BaseCache {
-  protected readonly fileName: string;
   public readonly client: RedisClient;
+  protected readonly fileName: string;
   protected readonly logger: Logger;
 
   constructor({ client, logger }: IBaseCacheDependencies) {
     this.client = client;
     this.logger = logger;
-
     this.cacheError();
   }
 
-  private cacheError(): void {
+  protected async connectionGuard(): Promise<void> {
+    try {
+      if (!this.client.isOpen) await this.client.connect();
+    } catch (error) {
+      throw new RedisError({
+        fileName: this.fileName,
+        message: error.message,
+        service: "connectionGuard",
+        stack: error.stack,
+      });
+    }
+  }
+  protected cacheError(): void {
     this.client.on("error", (error: unknown) => {
       this.logger.error(error);
     });
