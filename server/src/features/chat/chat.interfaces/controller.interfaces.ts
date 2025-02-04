eaae1f24cf714c73891fb2301a018a3952f3ccf5
KornelJanczak <korneljanczak10@gmail.com>
2025-02-04 23:39:53 +0100

    feat: update IChatController to use Promises and add deleteChatById method

--- server/src/features/chat/chat.interfaces/controller.interfaces.ts
+++ server/src/features/chat/chat.interfaces/controller.interfaces.ts
@@ -2,17 +2,44 @@ import { NextFunction, Request, Response } from "express";
 import { IChatRepository } from "@shared/repositories/chat.repository";
 import { IMessagesRepository } from "@shared/repositories/messages.repository";
 import { IChatStreamService } from "./chatStream.service.interfaces";
+import { IChatCache } from "@services/redis/chat.cache";
 
 export interface IChatController {
-  startChat(req: Request, res: Response, next: NextFunction): void;
-  createChat(req: Request, res: Response, next: NextFunction): void;
-  getChatById(req: Request, res: Response, next: NextFunction): void;
-  getChatsByUserId(req: Request, res: Response, next: NextFunction): void;
-  getMessagesByChatId(req: Request, res: Response, next: NextFunction): void;
+  startChat(
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Promise<Response | void>;
+  createChat(
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Promise<Response | void>;
+  getChatById(
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Promise<Response | void>;
+  getChatsByUserId(
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Promise<Response | void>;
+  getMessagesByChatId(
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Promise<Response | void>;
+  deleteChatById(
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Promise<Response | void>;
 }
 
 export interface IChatControllerDependencies {
   chatRepository: IChatRepository;
   messagesRepository: IMessagesRepository;
   chatStreamService: IChatStreamService;
+  chatCache: IChatCache;
 }
