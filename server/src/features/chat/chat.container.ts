849facf1bda7e8304d5f71191bc43cd6257977f3
KornelJanczak <korneljanczak10@gmail.com>
2025-02-04 23:40:04 +0100

    feat: add redis client to chat container and fix chatCache typo

--- server/src/features/chat/chat.container.ts
+++ server/src/features/chat/chat.container.ts
@@ -1,4 +1,10 @@
-import { createContainer, asClass, InjectionMode, asFunction } from "awilix";
+import {
+  createContainer,
+  asClass,
+  InjectionMode,
+  asFunction,
+  asValue,
+} from "awilix";
 import ChatController from "./chat.controller";
 import ChatStreamService from "./chat.services/chatStream.service";
 import AudioGeneratorService from "./chat.services/audioGenerator.service";
@@ -12,6 +18,7 @@ import TutorPromptService from "./chat.services/tutorPrompt.service";
 import ChatQueue from "@services/queues/chat.queue";
 import ChatWorker from "@shared/workers/chat.worker";
 import ChatCache from "@services/redis/chat.cache";
+import { client as redisClient } from "@services/redis/redis.client";
 
 const container = createContainer({
   injectionMode: InjectionMode.PROXY,
@@ -27,9 +34,10 @@ container.register({
   chatRepository: asClass(ChatRepository).singleton().scoped(),
   messagesRepository: asClass(MessagesRepository).singleton().scoped(),
   flashCardRepository: asClass(FlashCardRepository).singleton().scoped(),
-  chatCahe: asClass(ChatCache).singleton().scoped(),
+  chatCache: asClass(ChatCache).singleton().scoped(),
   chatQueue: asClass(ChatQueue).singleton().scoped(),
   chatWorker: asClass(ChatWorker).singleton().scoped(),
+  client: asValue(redisClient),
   logger: asFunction(() => config.createLogger("chatService"))
     .singleton()
     .scoped(),
