f8984ed3fcfeaa04334921e882cb26058d727abe
KornelJanczak <korneljanczak10@gmail.com>
2025-02-08 19:34:53 +0100

    feat: add credentials inclusion in API request options for enhanced security

--- client/src/common/services/api/client-api.ts
+++ client/src/common/services/api/client-api.ts
@@ -1,8 +1,19 @@
+"use client";
+
 import { RestHelper } from "./rest-helper";
 
 export class ClientAPI extends RestHelper {
   constructor() {
     super("http://localhost:5000/api/v1");
+  }
+
+  protected async interceptOptions(options: RequestInit): Promise<RequestInit> {
+    options = {
+      ...options,
+      credentials: "include",
+    };
+
+    return options;
   }
 }
 
