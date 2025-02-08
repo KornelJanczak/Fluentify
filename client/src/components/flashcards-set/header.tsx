173837841f5f50d7b57169a4cf55d88282639fbc
KornelJanczak <korneljanczak10@gmail.com>
2025-02-08 19:34:27 +0100

    feat: add FlashCardsSetHeader component for creating new flashcard sets

---
+++ client/src/components/flashcards-set/header.tsx
@@ -0,0 +1,9 @@
+import { Markdown } from "../markdown";
+
+export default function FlashCardsSetHeader() {
+  return (
+    <div>
+      <Markdown>Create new flashcards set</Markdown>
+    </div>
+  );
+}
