1e7074f1ef02b9e5f4f0750e5f2f04a324ab1409
KornelJanczak <korneljanczak10@gmail.com>
2025-02-08 19:34:56 +0100

    feat: create new flashcard set page with header component

---
+++ client/src/app/dashboard/flashcards/create-new/page.tsx
@@ -0,0 +1,9 @@
+import FlashCardsSetHeader from "@/components/flashcards-set/header";
+
+export default function CreateNewSetPage() {
+  return (
+    <div className="space-y-4 py-10 px-8">
+      <FlashCardsSetHeader />
+    </div>
+  );
+}
