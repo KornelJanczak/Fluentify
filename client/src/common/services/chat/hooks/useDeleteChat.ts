9f3387663fc5195a92ff33793a037339f12384be
KornelJanczak <korneljanczak10@gmail.com>
2025-02-08 19:34:46 +0100

    feat: enhance chat deletion with router refresh and improved error handling

--- client/src/common/services/chat/hooks/useDeleteChat.ts
+++ client/src/common/services/chat/hooks/useDeleteChat.ts
@@ -4,17 +4,26 @@ import { useMutation } from "@tanstack/react-query";
 import { ChatsResponse } from "../chat.service";
 import { clientApi } from "../../api/client-api";
 import { toast } from "sonner";
+import { revalidatePath } from "next/cache";
+import { useRouter } from "next/navigation";
 
 export const useDeleteChat = (chatId: string) => {
+  const router = useRouter();
+
   const mutation = useMutation({
     mutationFn: async () =>
-      (
-        await clientApi.delete<ChatsResponse>(`/chat/${chatId}`, {
-          credentials: "include",
-        })
-      ).data,
-    onSuccess: () => toast.success("Chat has been deleted successfully!"),
-    onError: () => toast.error("Failed to delete chat!"),
+      (await clientApi.delete<ChatsResponse>(`/chat/${chatId}`)).data,
+    onSuccess: (data) => {
+      console.log("data", data);
+
+      toast.success("Chat has been deleted successfully!");
+      router.refresh();
+    },
+    onError: (error) => {
+      console.log("error", error);
+
+      toast.error("Failed to delete chat!");
+    },
   });
 
   return mutation;
