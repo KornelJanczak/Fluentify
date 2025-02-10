b9e3d46d3cf109d10d77379e27ad654b9ce13a94
KornelJanczak <korneljanczak10@gmail.com>
2025-02-10 23:38:33 +0100

    feat: add flashCards field to createVocabularySetSchema for vocabulary set creation

--- server/src/features/vocabularySets/vocabularySets.schema.ts
+++ server/src/features/vocabularySets/vocabularySets.schema.ts
@@ -3,4 +3,10 @@ import z from "zod";
 export const createVocabularySetSchema = z.object({
   title: z.string(),
   description: z.string(),
+  flashCards: z.array(
+    z.object({
+      title: z.string(),
+      description: z.string(),
+    })
+  ),
 });
