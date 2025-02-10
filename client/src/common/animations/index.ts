46f4ecd141f925bbd0ef2f0716c1d2136abc056d
KornelJanczak <korneljanczak10@gmail.com>
2025-02-10 23:37:15 +0100

    feat: add fadeOut and scaleAndFadeAnimation variants for animations

---
+++ client/src/common/animations/index.ts
@@ -0,0 +1,21 @@
+"use client";
+
+import { type Variants } from "framer-motion";
+
+export const fadeOut: Variants = {
+  initial: { opacity: 0, y: -20, transition: { duration: 0.3 } },
+  animate: { opacity: 1, y: 0, transition: { duration: 0.3 } },
+  exit: { opacity: 0, y: 20, transition: { duration: 0.3 } },
+};
+
+export const scaleAndFadeAnimation: (isPresent: boolean) => Variants = (
+  isPresent: boolean
+) => ({
+  style: {
+    position: isPresent ? "static" : ("absolute" as any),
+  },
+  initial: { scale: 0, opacity: 0 },
+  animate: { scale: 1, opacity: 1 },
+  exit: { scale: 0, opacity: 0 },
+  transition: { type: "spring", stiffness: 900, damping: 40 },
+});
