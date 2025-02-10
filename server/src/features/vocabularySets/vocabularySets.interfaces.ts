4863edb2561d83cd8696340b41100ac73ac03735
KornelJanczak <korneljanczak10@gmail.com>
2025-02-10 23:38:31 +0100

    feat: define IVocabularySetsService and ICreateVocabularySetArgs interfaces

--- server/src/features/vocabularySets/vocabularySets.interfaces.ts
+++ server/src/features/vocabularySets/vocabularySets.interfaces.ts
@@ -1,6 +1,7 @@
 import { NextFunction, Request, Response } from "express";
 import { type Logger } from "winston";
-import { IVocabularySetRepository } from "@shared/repositories/vocabularySet.repository";
+import { VocabularySet, type FlashCard } from "@services/db/schema";
+import { type IVocabularySetRepository } from "@shared/repositories/vocabularySet.repository";
 
 export interface IVocabularySetsController {
   createVocabularySet(req: Request, res: Response, next: NextFunction): void;
@@ -13,5 +14,21 @@ export interface IVocabularySetsController {
 
 export interface IVocabularySetsControllerDependencies {
   vocabularySetRepository: IVocabularySetRepository;
+  vocabularySetsService: IVocabularySetsService;
   logger: Logger;
+}
+
+export interface IVocabularySetsService {
+  createVocabularySet(vocabularySet: ICreateVocabularySetArgs)
+}
+
+export interface IVocabularySetsServiceDependencies {}
+
+
+
+export interface ICreateVocabularySetArgs {
+  userId: string;
+  title: string;
+  description: string;
+  flashCards: Omit<FlashCard, "id">[];
 }
