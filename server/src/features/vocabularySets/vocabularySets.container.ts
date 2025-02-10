d7f2d907f2e28360988963349be62fe4d2e03109
KornelJanczak <korneljanczak10@gmail.com>
2025-02-10 23:38:20 +0100

    feat: add VocabularySetsService to manage vocabulary set operations

--- server/src/features/vocabularySets/vocabularySets.container.ts
+++ server/src/features/vocabularySets/vocabularySets.container.ts
@@ -2,6 +2,7 @@ import { createContainer, asClass, InjectionMode, asFunction } from "awilix";
 import VocabularySetsController from "./vocabularySets.controller";
 import VocabularySetRepository from "@shared/repositories/vocabularySet.repository";
 import { config } from "@root/config";
+import VocabularySetsService from "./vocabularySets.service";
 
 const container = createContainer({
   injectionMode: InjectionMode.PROXY,
@@ -14,6 +15,7 @@ container.register({
   vocabularySetRepository: asClass(VocabularySetRepository)
     .singleton()
     .scoped(),
+  vocabularySetsService: asClass(VocabularySetsService).singleton().scoped(),
   logger: asFunction(() => config.createLogger("vocabularySetLogger"))
     .singleton()
     .scoped(),
