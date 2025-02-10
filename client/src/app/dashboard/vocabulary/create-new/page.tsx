1ab31e55bd0a37e6c22f82fe9a88519ebd0d5424
KornelJanczak <korneljanczak10@gmail.com>
2025-02-10 23:37:11 +0100

    feat: add CreateNewSetPage component for creating new vocabulary sets

---
+++ client/src/app/dashboard/vocabulary/create-new/page.tsx
@@ -0,0 +1,6 @@
+import FlashCardsSet from "@/components/vocabulary-set/vocabulary-set";
+import FlashCardsSetCreateNew from "@/components/vocabulary-set/vocabulary-set-create-new";
+
+export default function CreateNewSetPage() {
+  return <FlashCardsSetCreateNew />;
+}
