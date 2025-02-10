2937e272c4a818f8236f731c882a25587003b0cb
KornelJanczak <korneljanczak10@gmail.com>
2025-02-10 23:37:05 +0100

    feat: remove CreateNewSetPage component

--- client/src/app/dashboard/flashcards/create-new/page.tsx
+++
@@ -1,6 +0,0 @@
-import FlashCardsSet from "@/components/flash-cards-set/flash-cards-set";
-import FlashCardsSetCreateNew from "@/components/flash-cards-set/flash-cards-set-create-new";
-
-export default function CreateNewSetPage() {
-  return <FlashCardsSetCreateNew />;
-}
