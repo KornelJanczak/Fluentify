a01662e1eb082390e01424a1774b158cb4f18143
KornelJanczak <korneljanczak10@gmail.com>
2025-02-10 23:38:23 +0100

    feat: integrate VocabularySetsService for creating vocabulary sets

--- server/src/features/vocabularySets/vocabularySets.controller.ts
+++ server/src/features/vocabularySets/vocabularySets.controller.ts
@@ -2,6 +2,7 @@ import { IVocabularySetRepository } from "@shared/repositories/vocabularySet.rep
 import {
   IVocabularySetsController,
   IVocabularySetsControllerDependencies,
+  IVocabularySetsService,
 } from "./vocabularySets.interfaces";
 import { NextFunction, Request, Response } from "express";
 import HTTP_STATUS from "http-status-codes";
@@ -11,19 +12,24 @@ import { User } from "@services/db/schema";
 
 class VocabularySetsController implements IVocabularySetsController {
   private readonly vocabularySetRepository: IVocabularySetRepository;
+  private readonly vocabularySetsService: IVocabularySetsService;
   private readonly fileName = "vocabularySet.controller";
   private readonly logger: Logger;
 
   constructor({
     vocabularySetRepository,
+    vocabularySetsService,
     logger,
   }: IVocabularySetsControllerDependencies) {
     this.vocabularySetRepository = vocabularySetRepository;
+    this.vocabularySetsService = vocabularySetsService;
     this.logger = logger;
   }
 
   async createVocabularySet(req: Request, res: Response, next: NextFunction) {
     const service = "createVocabularySet";
+    const user: User = req.user as User;
+    const { body } = req;
 
     this.logger.info({
       message: "Creating vocabulary set...",
@@ -31,32 +37,35 @@ class VocabularySetsController implements IVocabularySetsController {
       service,
     });
 
-    const { body } = req;
-
-    const user: User = req.user as User;
-    const newItem = { ...body, userId: user.id, createdAt: new Date() };
-
-    const createdVocabularySet = await this.vocabularySetRepository.create(
-      newItem
-    );
-
-    if (!createdVocabularySet) {
-      return next(
-        new NotFoundError({
-          message: "Vocabulary set not created",
-          fileName: this.fileName,
-          service: "createVocabularySet",
-        })
-      );
-    }
-
-    this.logger.info({
-      message: "Vocabulary set has been created!",
-      fileName: this.fileName,
-      service,
+    await this.vocabularySetsService.createVocabularySet({
+      userId: user.id,
+      description: body.description,
+      title: body.title,
+      flashCards: body.flashCards,
     });
 
-    return res.status(HTTP_STATUS.OK).json(createdVocabularySet);
+    // const createdVocabularySet =
+    //   await this.vocabularySetRepository.createNewVocabularySet(
+    //     newVocabularySet
+    //   );
+
+    // if (!createdVocabularySet) {
+    //   return next(
+    //     new NotFoundError({
+    //       message: "Vocabulary set not created",
+    //       fileName: this.fileName,
+    //       service: "createVocabularySet",
+    //     })
+    //   );
+    // }
+
+    // this.logger.info({
+    //   message: "Vocabulary set has been created!",
+    //   fileName: this.fileName,
+    //   service,
+    // });
+
+    // return res.status(HTTP_STATUS.OK).json(createdVocabularySet);
   }
 
   async getAllVocabularySetsByUserId(
