f6298ad86c0c1f7b77bc7719c29e3c3adfb6f543
KornelJanczak <korneljanczak10@gmail.com>
2025-02-10 23:38:00 +0100

    feat: remove FlashCardsSetHeader component from flash cards set

--- client/src/components/flash-cards-set/header.tsx
+++
@@ -1,9 +0,0 @@
-import { Markdown } from "../markdown";
-
-export default function FlashCardsSetHeader() {
-  return (
-    <div>
-      <Markdown>## Create new flashcards set</Markdown>
-    </div>
-  );
-}
