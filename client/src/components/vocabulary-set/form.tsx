f626d41281a11ba226fcc17d98a97c6b9d0ced16
KornelJanczak <korneljanczak10@gmail.com>
2025-02-10 23:38:12 +0100

    feat: add VocabularySetForm component for creating vocabulary sets

---
+++ client/src/components/vocabulary-set/form.tsx
@@ -0,0 +1,65 @@
+"use client";
+
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import {
+  FlashCardsSetFormValues,
+  useFlashCardsSetForm,
+} from "@/common/hooks/use-flash-cards-set-form";
+
+interface FlashCardsSetFormProps {
+  defaultValues?: FlashCardsSetFormValues;
+  onSubmit: (data: FlashCardsSetFormValues) => void;
+}
+
+export function VocabularySetForm({
+  onSubmit,
+  defaultValues,
+}: FlashCardsSetFormProps) {
+  const { form } = useFlashCardsSetForm({ defaultValues });
+
+  return (
+    <Form {...form}>
+      <form
+        id="flash-cards-set-form"
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="space-y-8 pt-8"
+      >
+        <FormField
+          control={form.control}
+          name="title"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Title</FormLabel>
+              <FormControl>
+                <Input placeholder="Set title" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="description"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Description</FormLabel>
+              <FormControl>
+                <Input placeholder="Add description..." {...field} />
+              </FormControl>
+
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+      </form>
+    </Form>
+  );
+}
