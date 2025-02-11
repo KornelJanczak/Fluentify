5496f5c12ef8ef4c9118da6256f1107afeb3b99d
KornelJanczak <korneljanczak10@gmail.com>
2025-02-11 23:31:56 +0100

    refactor: integrate vocabulary set creation logic into VocabularySetCreateNew component

--- client/src/components/vocabulary-set/vocabulary-set-create-new.tsx
+++ client/src/components/vocabulary-set/vocabulary-set-create-new.tsx
@@ -1,18 +1,32 @@
 "use client";
 
-import { useFlashCardsStore } from "@/common/hooks/use-flash-cards-set-create-new";
+import { useFlashCardsStore } from "@/common/hooks/use-flash-cards-store";
 import VocabularySet from "./vocabulary-set";
+import { useCreateVocabularySet } from "@/common/services/vocabulary-set/hooks/use-create-vocabulary-set";
 
 export default function VocabularySetCreateNew() {
   const { flashCards, addFlashCard, deleteFlashCard, flashCardsNumber } =
     useFlashCardsStore((state) => state);
 
+  const handleFormSubmit = (values) => {
+    const { mutate } = useCreateVocabularySet({
+      ...values,
+      flashCards: flashCards
+        .map(({ translation, definition }) =>
+          translation !== "" && definition !== ""
+            ? { translation, definition }
+            : null
+        )
+        .filter(Boolean),
+    });
+
+    mutate();
+  };
+
   return (
     <div className="space-y-4 py-10 px-8">
       <VocabularySet
-        onFormSubmit={() => {
-          console.log("Form submitted");
-        }}
+        onFormSubmit={handleFormSubmit}
         onDeleteFlashCard={deleteFlashCard}
         flashCards={flashCards}
         flashCardsNumber={flashCardsNumber}
