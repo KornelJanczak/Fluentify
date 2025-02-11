128fe95ac11eb40d5a038d47c60c3d8f2f6cd551
KornelJanczak <korneljanczak10@gmail.com>
2025-02-11 23:32:08 +0100

    refactor: enforce minimum length for title and description in vocabulary set schema

--- server/src/features/vocabularySets/vocabularySets.schema.ts
+++ server/src/features/vocabularySets/vocabularySets.schema.ts
@@ -1,12 +1,12 @@
 import z from "zod";
 
 export const createVocabularySetSchema = z.object({
-  title: z.string(),
-  description: z.string(),
+  title: z.string().min(2),
+  description: z.string().min(2),
   flashCards: z.array(
     z.object({
-      title: z.string(),
-      description: z.string(),
+      title: z.string().min(2),
+      description: z.string().min(2),
     })
   ),
 });
