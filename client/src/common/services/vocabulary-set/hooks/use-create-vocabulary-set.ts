9787224ab5059bb1a808dfe9ebd0fe156f2a35c9
KornelJanczak <korneljanczak10@gmail.com>
2025-02-13 23:07:15 +0100

    refactor: update useCreateVocabularySet hook to use fetch API and format request data

--- client/src/common/services/vocabulary-set/hooks/use-create-vocabulary-set.ts
+++ client/src/common/services/vocabulary-set/hooks/use-create-vocabulary-set.ts
@@ -2,30 +2,68 @@
 
 import { useMutation } from "@tanstack/react-query";
 import { clientApi } from "../../api/client-api";
-import {
-  type CreateVocabularySetRequest,
-  type CreateVocabularySetResponse,
-} from "../vocabulary-set.service";
+import { FlashCard } from "../../flash-card/flash-card.interfaces";
 import { toast } from "sonner";
 
-export const useCreateVocabularySet = (
-  vocabularySet: CreateVocabularySetRequest
-) => {
+export const useCreateVocabularySet = () => {
   const mutation = useMutation({
-    mutationFn: async () =>
-      (
-        await clientApi.post<CreateVocabularySetResponse>(
-          `/vocabulary-set`,
-          vocabularySet
-        )
-      ).data,
+    // mutationFn: async (vocabularySet: CreateVocabularySetRequest) =>
+    //   (
+    //     await clientApi.post<CreateVocabularySetResponse>(
+    //       `/vocabulary-set`,
+    //       formatCreateVocabularySetData(vocabularySet)
+    //     )
+    //   ).data,
+    mutationFn: async (vocabularySet: CreateVocabularySetRequest) => {
+      await fetch("http://localhost:5000/api/v1/vocabulary-set", {
+        method: "POST",
+        credentials: "include",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: formatCreateVocabularySetData(vocabularySet),
+      });
+    },
     onSuccess: () => {
       toast.success("Vocabulary set has been created successfully!");
     },
-    onError: () => {
+    onError: (error) => {
+      console.log("error", error);
+
       toast.error("Failed to create vocabulary set!");
     },
   });
 
   return mutation;
+};
+
+const formatCreateVocabularySetData = ({
+  title,
+  description,
+  flashCards,
+}: CreateVocabularySetRequest) => {
+  const obj = {
+    title,
+    description,
+    flashCards: flashCards
+      .map(({ translation, definition }) =>
+        translation !== "" && definition !== ""
+          ? { translation, definition }
+          : null
+      )
+      .filter(Boolean),
+  };
+  console.log("obj", obj);
+
+  return JSON.stringify(obj);
+};
+
+export type CreateVocabularySetResponse = {
+  vocabularySetId: string;
+};
+
+export type CreateVocabularySetRequest = {
+  title?: string;
+  description?: string;
+  flashCards: Omit<FlashCard, "id" | "vocabularySetId">[];
 };
