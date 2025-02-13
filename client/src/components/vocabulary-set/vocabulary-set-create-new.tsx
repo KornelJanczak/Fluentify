8121da68244bbcbeb8959723566fc8ad7c8944a9
KornelJanczak <korneljanczak10@gmail.com>
2025-02-13 23:07:31 +0100

    refactor: simplify handleFormSubmit in VocabularySetCreateNew component

--- client/src/components/vocabulary-set/vocabulary-set-create-new.tsx
+++ client/src/components/vocabulary-set/vocabulary-set-create-new.tsx
@@ -3,25 +3,18 @@
 import { useFlashCardsStore } from "@/common/hooks/use-flash-cards-store";
 import VocabularySet from "./vocabulary-set";
 import { useCreateVocabularySet } from "@/common/services/vocabulary-set/hooks/use-create-vocabulary-set";
+import { FlashCardsSetFormValues } from "@/common/hooks/use-flash-cards-set-form";
 
 export default function VocabularySetCreateNew() {
   const { flashCards, addFlashCard, deleteFlashCard, flashCardsNumber } =
     useFlashCardsStore((state) => state);
+  const { mutate } = useCreateVocabularySet();
 
-  const handleFormSubmit = (values) => {
-    const { mutate } = useCreateVocabularySet({
+  const handleFormSubmit = (values: FlashCardsSetFormValues) =>
+    mutate({
       ...values,
-      flashCards: flashCards
-        .map(({ translation, definition }) =>
-          translation !== "" && definition !== ""
-            ? { translation, definition }
-            : null
-        )
-        .filter(Boolean),
+      flashCards,
     });
-
-    mutate();
-  };
 
   return (
     <div className="space-y-4 py-10 px-8">
