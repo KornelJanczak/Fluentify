464be5246d30edeac44e8c708bb9e3f02304640d
KornelJanczak <korneljanczak10@gmail.com>
2025-02-14 22:29:29 +0100

    refactor: streamline database service by removing unused imports and optimizing connection handling

--- server/src/shared/services/db/index.ts
+++ server/src/shared/services/db/index.ts
@@ -1,15 +1,10 @@
-// import { drizzle } from "drizzle-orm/neon-http";
 import { drizzle } from "drizzle-orm/node-postgres";
-import { neon, neonConfig } from "@neondatabase/serverless";
-import type { NeonQueryFunction } from "@neondatabase/serverless";
-import { config } from "@root/config";
+import { neonConfig } from "@neondatabase/serverless";
 import { Pool } from "pg";
-import { pgTable, serial, text, varchar } from "drizzle-orm/pg-core";
 
 neonConfig.fetchConnectionCache = true;
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL!,
 });
-
 
 export const db = drizzle({ client: pool });
