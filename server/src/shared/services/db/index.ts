4651c9d457691f307d8b6e3dce0817cd5eedde87
KornelJanczak <korneljanczak10@gmail.com>
2025-02-15 21:28:03 +0100

    refactor: update database connection handling to use centralized config

--- server/src/shared/services/db/index.ts
+++ server/src/shared/services/db/index.ts
@@ -1,10 +1,16 @@
 import { drizzle } from "drizzle-orm/node-postgres";
 import { neonConfig } from "@neondatabase/serverless";
 import { Pool } from "pg";
+import { config } from "@root/config";
 
 neonConfig.fetchConnectionCache = true;
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL!,
+  // user: "postgres",
+  // host: "localhost",
+  // database: "fluentify",
+  // password: "123",
+  // port: 5432,
+  connectionString: config.DATABASE_URL,
 });
 
 export const db = drizzle({ client: pool });
