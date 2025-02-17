780e3b5e00cd4069ea0c143b0c3af72024d48d2a
KornelJanczak <korneljanczak10@gmail.com>
2025-02-17 16:14:20 +0100

    refactor: update DatabaseError status code to INTERNAL_SERVER_ERROR for better error handling

--- server/src/shared/errors/db.error.ts
+++ server/src/shared/errors/db.error.ts
@@ -3,7 +3,7 @@ import HTTP_STATUS from "http-status-codes";
 
 export default class DatabaseError extends ServerError {
   constructor({ fileName, service, message, stack }: ServerErrorArguments) {
-    super("DatabaseError", HTTP_STATUS.UNAUTHORIZED, {
+    super("DatabaseError", HTTP_STATUS.INTERNAL_SERVER_ERROR, {
       fileName,
       message,
       service,
