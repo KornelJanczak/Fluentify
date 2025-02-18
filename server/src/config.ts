48ca3dabc02b9ab728bd94798f50df970b6049f7
KornelJanczak <korneljanczak10@gmail.com>
2025-02-18 23:56:50 +0100

    refactor: restructure configuration class for improved readability and organization

--- server/src/config.ts
+++ server/src/config.ts
@@ -1,103 +1,93 @@
-import winston from "winston";
 import dotenv from "dotenv";
-import NotFoundError from "@shared/errors/notFound.error";
 import { toInteger } from "lodash";
+import { ServiceError } from "@shared/errors/service.error";
 
 dotenv.config();
 
 class Config {
-  public PORT: number | undefined;
-  public DATABASE_URL: string | undefined;
-  public NODE_ENV: string | undefined;
-  public SESSION_SECRET: string | undefined;
-  public CLIENT_URL: string | undefined;
-  public COOKIE_SESSION_NAME: string | undefined;
-  public LOG_LEVEL: string | undefined;
-  public OPENAI_API_KEY: string | undefined;
-  public GOOGLE_GENERATIVE_AI_API_KEY: string | undefined;
-  public GOOGLE_CLIENT_ID: string | undefined;
-  public GOOGLE_CLIENT_SECRET: string | undefined;
-  public GOOGLE_CALLBACK_URL: string | undefined;
-  public SECRET_KEY_ONE: string | undefined;
-  public SECRET_KEY_TWO: string | undefined;
-  public REDIS_HOST: string | undefined;
-  public REDIS_PORT: number | undefined;
-  public GOOGLE_API_KEY: string | undefined;
-  public BULL_BASE_PATH: string | undefined;
-  readonly loggerLevels = {
-    fatal: 0,
-    error: 1,
-    warn: 2,
-    info: 3,
-    debug: 4,
-    trace: 5,
+  public readonly PORT: number;
+  // public readonly NODE_ENV: string;
+  public readonly DATABASE_URL: string;
+  public readonly SESSION_SECRET: string;
+  public readonly CLIENT_URL: string;
+  public readonly LOG_LEVEL: string;
+  public readonly OPENAI_API_KEY: string;
+  public readonly BULL_BASE_PATH: string;
+
+  public readonly SECRETS: {
+    ONE: string;
+    TWO: string;
   };
-  private readonly fileName = "config";
+
+  public readonly REDIS: {
+    HOST: string;
+    PORT: number;
+  };
+
+  public readonly GOOGLE: {
+    CLIENT_ID: string;
+    CLIENT_SECRET: string;
+    CALLBACK_URL: string;
+    // API_KEY: string;
+    // GENERATIVE_AI_API_KEY: string;
+  };
+
+  readonly LOGGER_LEVELS = {
+    FATAL: 0,
+    ERROR: 1,
+    WARN: 2,
+    INFO: 3,
+    DEBUG: 4,
+    TRACE: 5,
+  };
 
   constructor() {
     this.PORT = toInteger(process.env.PORT) || 5000;
-    this.NODE_ENV = process.env.NODE_ENV || "";
+    // this.NODE_ENV = process.env.NODE_ENV || "";
     this.DATABASE_URL = process.env.DATABASE_URL || "";
     this.SESSION_SECRET = process.env.SESSION_SECRET || "";
     this.CLIENT_URL = process.env.CLIENT_URL || "";
-    this.COOKIE_SESSION_NAME = process.env.COOKIE_SESSION_NAME || "";
     this.LOG_LEVEL = process.env.LOG_LEVEL || "";
     this.OPENAI_API_KEY = process.env.OPENAI_API_KEY || "";
-    this.GOOGLE_GENERATIVE_AI_API_KEY =
-      process.env.GOOGLE_GENERATIVE_AI_API_KEY || "";
-    this.GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID || "";
-    this.GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET || "";
-    this.GOOGLE_CALLBACK_URL = process.env.GOOGLE_CALLBACK_URL || "";
-    this.SECRET_KEY_ONE = process.env.SECRET_KEY_ONE || "";
-    this.SECRET_KEY_TWO = process.env.SECRET_KEY_TWO || "";
-    this.REDIS_HOST = process.env.REDIS_HOST || "";
-    this.REDIS_PORT = toInteger(process.env.REDIS_PORT) || 6379;
-    this.GOOGLE_API_KEY = process.env.GOOGLE_API_KEY || "";
     this.BULL_BASE_PATH = process.env.BULL_BASE_PATH || "";
+
+    this.SECRETS = {
+      ONE: process.env.SECRET_KEY_ONE || "",
+      TWO: process.env.SECRET_KEY_TWO || "",
+    };
+
+    this.REDIS = {
+      HOST: process.env.REDIS_HOST || "",
+      PORT: toInteger(process.env.REDIS_PORT) || 6379,
+    };
+
+    this.GOOGLE = {
+      CLIENT_ID: process.env.GOOGLE_CLIENT_ID || "",
+      CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET || "",
+      CALLBACK_URL: process.env.GOOGLE_CALLBACK_URL || "",
+      // API_KEY: process.env.GOOGLE_API_KEY || "",
+      // GENERATIVE_AI_API_KEY: process.env.GOOGLE_GENERATIVE_AI_API_KEY || "",
+    };
+
+    this.validateConfig();
   }
 
-  public validateConfig(): void {
+  private validateConfig(): void {
     for (const [key, value] of Object.entries(this)) {
-      if (value === undefined) {
-        throw new NotFoundError({
-          fileName: this.fileName,
-          service: "validateConfig",
+      if (typeof value === "object") {
+        for (const [subKey, subValue] of Object.entries(value)) {
+          if (subValue === undefined || subValue === "") {
+            throw ServiceError.NotFound({
+              message: `Configuration ${key}.${subKey} is undefined.`,
+            });
+          }
+        }
+      } else if (value === undefined || value === "") {
+        throw ServiceError.NotFound({
           message: `Configuration ${key} is undefined.`,
         });
       }
     }
-  }
-
-  public createLogger(name?: string) {
-    return winston.createLogger({
-      levels: this.loggerLevels,
-      level: process.env.LOG_LEVEL || "info",
-      format: winston.format.combine(
-        winston.format.colorize(),
-        winston.format.timestamp({
-          format: "YYYY-MM-DD HH:mm:ss",
-        }),
-        winston.format.errors({ stack: true }),
-
-        winston.format.printf(
-          ({ timestamp, level, message, code, service, fileName, stack }) => {
-            const ifErrorCodeExist = code ? `(${code})` : "";
-            const isService = service ? `/${service}` : "";
-            const logLocation = fileName
-              ? `${fileName}${isService}`
-              : `${name}${isService}`;
-
-            const loggMessage = `${timestamp} ${level}${ifErrorCodeExist} [${logLocation}]: ${message}`;
-            if (stack) {
-              return `${loggMessage}\nStack: ${stack}`;
-            }
-
-            return loggMessage;
-          }
-        )
-      ),
-      transports: [new winston.transports.Console()],
-    });
   }
 }
 
