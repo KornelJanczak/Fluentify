413c263c086f91d64cb4712ddd4eb5e037136d7c
KornelJanczak <korneljanczak10@gmail.com>
2025-02-18 23:59:32 +0100

    refactor: update global error middleware to use HttpError and ServiceError for improved error handling

--- server/src/shared/middleware/error.middleware.ts
+++ server/src/shared/middleware/error.middleware.ts
@@ -1,10 +1,11 @@
 import { Request, Response, NextFunction } from "express";
 import ServerError from "../errors/server.error";
-import { config } from "@root/config";
-import RedisError from "@shared/errors/redis.error";
 import HTTP_STATUS from "http-status-codes";
+import { HttpError } from "@shared/errors/http.error";
+import { logger as errorLogger } from "@root/logger";
+import { ServiceError } from "@shared/errors/service.error";
 
-const logger = config.createLogger("errorMiddleware");
+const logger = errorLogger.createLogger("errorMiddleware");
 
 export const globalErrorMiddleware = (
   err: Error | ServerError,
@@ -21,26 +22,26 @@ export const globalErrorMiddleware = (
 
   let errorResponse: any = baseResponse;
 
-  if (err instanceof ServerError) {
+  if (err instanceof HttpError) {
     errorResponse = {
       code: err.code,
       name: err.name,
-      fileName: err.fileName,
-      service: err.service,
       message: JSON.stringify(err.message),
       stack: process.env.NODE_ENV === "production" ? null : err.stack,
     };
-  } else {
+
     logger.error({
       code: errorResponse.code,
       name: errorResponse.name,
       stack: errorResponse.stack,
       message: errorResponse.message,
     });
-  }
-
-  if (err instanceof RedisError) {
-    errorResponse = baseResponse;
+  } else if (err instanceof ServiceError) {
+    logger.error({
+      name: errorResponse.name,
+      stack: errorResponse.stack,
+      message: errorResponse.message,
+    });
   }
 
   return res.status(errorResponse.code).send(errorResponse);
