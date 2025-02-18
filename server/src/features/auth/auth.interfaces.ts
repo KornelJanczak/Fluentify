ea55c3a5eb848780773d28bcdfd6f6e40e2c510f
KornelJanczak <korneljanczak10@gmail.com>
2025-02-18 23:57:27 +0100

    refactor: remove unused authStatus method from IAuthController interface

--- server/src/features/auth/auth.interfaces.ts
+++ server/src/features/auth/auth.interfaces.ts
@@ -3,7 +3,6 @@ import { Logger } from "winston";
 
 export interface IAuthController {
   logOut(req: Request, res: Response, next: NextFunction): void;
-  authStatus(req: Request, res: Response, next: NextFunction): void;
   authSession(req: Request, res: Response, __: NextFunction): void;
 }
 
