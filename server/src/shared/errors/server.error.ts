75c16369adc72818d4b5cf7aaaac41e1cf43bf7f
KornelJanczak <korneljanczak10@gmail.com>
2025-02-18 23:59:18 +0100

    refactor: simplify ServerError class by removing unused properties

--- server/src/shared/errors/server.error.ts
+++ server/src/shared/errors/server.error.ts
@@ -1,28 +1,15 @@
 export type ServerErrorArguments = {
-  fileName?: string;
-  service: string;
   message: string;
   stack?: string;
 };
 
 abstract class ServerError extends Error {
-  public code: number;
-  public fileName?: string;
-  public service: string;
-
-  constructor(
-    name: string,
-    code: number,
-    { fileName, service, message, stack }: ServerErrorArguments
-  ) {
+  constructor(name: string, { message, stack }: ServerErrorArguments) {
     super(message);
     this.name = name;
-    this.code = code;
-    this.fileName = fileName;
-    this.service = service;
     this.stack = stack;
 
-    Error.captureStackTrace(this, ServerError);
+    // Error.captureStackTrace(this, ServerError);
   }
 }
 
