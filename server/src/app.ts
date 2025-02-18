52037d67df310b86f023f1641bcdd5bafb35335f
KornelJanczak <korneljanczak10@gmail.com>
2025-02-18 23:56:46 +0100

    refactor: enhance logging by simplifying error messages and removing unnecessary service labels

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,25 +1,21 @@
 import express, { Express } from "express";
-import { config } from "@root/config";
 import { FluentifyServer } from "@root/setupServer";
 import "@auth/strategies/google-strategy";
+import { logger as appLogger } from "@root/logger";
+
+const logger = appLogger.createLogger("app");
 
 class Application {
   public initialize(): void {
-    this.loadConfig();
     const app: Express = express();
     const server: FluentifyServer = new FluentifyServer(app);
     server.start();
     Application.handleExit();
   }
 
-  private loadConfig(): void {
-    config.validateConfig();
-  }
-
   private static handleExit(): void {
     process.on("uncaughtException", (error: Error) => {
       logger.error({
-        service: "uncaughtException",
         message: `There was an uncaught error: ${error.message}`,
         stack: error.stack,
       });
@@ -28,7 +24,6 @@ class Application {
 
     process.on("unhandledRejection", (reason: any) => {
       logger.error({
-        service: "unhandledRejection",
         message: `Unhandled rejection at promise: ${reason}`,
         stack: reason instanceof Error ? reason.stack : undefined,
       });
@@ -36,10 +31,7 @@ class Application {
     });
 
     process.on("SIGTERM", () => {
-      logger.error({
-        service: "SIGTERM",
-        message: "Caught SIGTERM",
-      });
+      logger.error("Caught SIGTERM");
       Application.shutDownProperly(2);
     });
 
@@ -62,22 +54,18 @@ class Application {
   private static shutDownProperly(exitCode: number): void {
     Promise.resolve()
       .then(() => {
-        logger.info({
-          service: "shutDownProperly",
-          message: "Shutdown complete",
-        });
+        logger.info("Shutdown complete");
         process.exit(exitCode);
       })
       .catch((error) => {
         logger.error({
-          service: "shutDownProperly",
           message: `Error during shutdown: ${error}`,
+          stack: error.stack,
         });
         process.exit(1);
       });
   }
 }
 
-const logger = config.createLogger("app");
 const application: Application = new Application();
 application.initialize();
