ab22106ade00738f62c94442c8e5b67b239eafda
KornelJanczak <korneljanczak10@gmail.com>
2025-02-18 23:57:52 +0100

    refactor: comment out unused imports and container registrations in flashCards.container.ts

--- server/src/features/flashCards/flashCards.container.ts
+++ server/src/features/flashCards/flashCards.container.ts
@@ -1,18 +1,18 @@
 import { createContainer, asClass, InjectionMode, asFunction } from "awilix";
-import FlashCardsController from "./flashCards.controller";
-import FlashCardRepository from "@shared/repositories/flashCard.repository";
-import { config } from "@root/config";
+// import FlashCardsController from "./flashCards.controller";
+// import FlashCardRepository from "@shared/repositories/flashCard.repository";
+// import { config } from "@root/config";
 
 const container = createContainer({
   injectionMode: InjectionMode.PROXY,
 });
 
-container.register({
-  flashCardsController: asClass(FlashCardsController).singleton().scoped(),
-  flashCardRepository: asClass(FlashCardRepository).singleton().scoped(),
-  logger: asFunction(() => config.createLogger("flashCardLogger"))
-    .singleton()
-    .scoped(),
-});
+// container.register({
+//   flashCardsController: asClass(FlashCardsController).singleton().scoped(),
+//   flashCardRepository: asClass(FlashCardRepository).singleton().scoped(),
+//   logger: asFunction(() => config.createLogger("flashCardLogger"))
+//     .singleton()
+//     .scoped(),
+// });
 
 export default container;
