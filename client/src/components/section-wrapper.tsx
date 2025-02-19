6749bd532afbee90fa6046bef1a336d750b842e4
KornelJanczak <korneljanczak10@gmail.com>
2025-02-19 23:40:22 +0100

    feat: add SectionWrapper component for consistent layout structure

---
+++ client/src/components/section-wrapper.tsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+
+export default function SectionWrapper({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <section className="flex flex-col justify-center items-center w-full space-y-4 py-10 px-8">
+      {children}
+    </section>
+  );
+}
