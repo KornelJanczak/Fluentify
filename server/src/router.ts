3a1c04b92104f7bb18388c3d45ed2a45599f9024
KornelJanczak <korneljanczak10@gmail.com>
2025-02-19 17:48:48 +0100

    refactor: simplify route registration in router.ts for improved readability

--- server/src/router.ts
+++ server/src/router.ts
@@ -8,12 +8,9 @@ import { serverAdapter } from "@services/queues/base.queue";
 const BASE_PATH = "/api/v1";
 
 export default (app: Application) => {
-  const routes = () => {
-    app.use("/queues", serverAdapter.getRouter());
-    app.use(BASE_PATH, authRouter);
-    app.use(BASE_PATH, chatRouter);
-    app.use(BASE_PATH, vovabularySetRouter);
-    app.use(BASE_PATH, flashCardsRouter);
-  };
-  routes();
+  app.use("/queues", serverAdapter.getRouter());
+  app.use(BASE_PATH, authRouter);
+  app.use(BASE_PATH, chatRouter);
+  app.use(BASE_PATH, vovabularySetRouter);
+  app.use(BASE_PATH, flashCardsRouter);
 };
