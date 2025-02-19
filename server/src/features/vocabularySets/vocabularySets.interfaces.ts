2f727e2dd95f1a9de3999cae9ce767a0db16bce8
KornelJanczak <korneljanczak10@gmail.com>
2025-02-19 23:41:07 +0100

    refactor: update IVocabularySetsService to return VocabularySetWithFlashCardsCount[]

--- server/src/features/vocabularySets/vocabularySets.interfaces.ts
+++ server/src/features/vocabularySets/vocabularySets.interfaces.ts
@@ -1,7 +1,10 @@
 import { NextFunction, Request, Response } from "express";
 import { type Logger } from "winston";
 import { VocabularySet, type FlashCard } from "@services/db/schema";
-import { IVocabularySetRepository } from "@shared/repositories/vocabularySet.repository";
+import {
+  IVocabularySetRepository,
+  VocabularySetWithFlashCardsCount,
+} from "@shared/repositories/vocabularySet.repository";
 
 export interface IVocabularySetsController {
   createVocabularySet(
@@ -23,7 +26,9 @@ export interface IVocabularySetsControllerDependencies {
 
 export interface IVocabularySetsService {
   createVocabularySet(vocabularySet: ICreateVocabularySetArgs): Promise<string>;
-  getAllVocabularySetsByUserId(userId: string): Promise<VocabularySet[]>;
+  getAllVocabularySetsByUserId(
+    userId: string
+  ): Promise<VocabularySetWithFlashCardsCount[]>;
 }
 
 export interface IVocabularySetsServiceDependencies {
