c5d2df7d413d057e81201bd115567a4792be5ac3
KornelJanczak <korneljanczak10@gmail.com>
2025-02-19 23:41:18 +0100

    refactor: update getAllByUserId to return VocabularySetWithFlashCardsCount[] and improve error handling

--- server/src/shared/repositories/vocabularySet.repository.ts
+++ server/src/shared/repositories/vocabularySet.repository.ts
@@ -6,15 +6,15 @@ import {
 } from "@services/db/schema";
 import { type FlashCard, type FlashCardWithoutIds } from "@services/db/schema";
 import { db } from "@shared/services/db";
-import { HttpError } from "@shared/errors/http.error";
-import { eq } from "drizzle-orm";
+import { ServiceError } from "@shared/errors/service.error";
+import { count, eq } from "drizzle-orm";
 
 export interface IVocabularySetRepository {
   createNewVocabularySet(
     newVocabularySet: VocabularySetWithoutId,
     flashCards: FlashCardWithoutIds[]
   ): Promise<string>;
-  getAllByUserId(userId: string): Promise<VocabularySet[]>;
+  getAllByUserId(userId: string): Promise<VocabularySetWithFlashCardsCount[]>;
 }
 
 class VocabularySetRepository implements IVocabularySetRepository {
@@ -41,21 +41,32 @@ class VocabularySetRepository implements IVocabularySetRepository {
         return vocabularySet.id;
       });
     } catch (error) {
-      throw HttpError.InternalServerError({
+      throw ServiceError.DatabaseError({
         message: error.message,
         stack: error.stack,
       });
     }
   }
 
-  public async getAllByUserId(userId: string): Promise<VocabularySet[]> {
+  public async getAllByUserId(
+    userId: string
+  ): Promise<VocabularySetWithFlashCardsCount[]> {
     try {
       return await db
-        .select()
+        .select({
+          id: vocabularySets.id,
+          title: vocabularySets.title,
+          description: vocabularySets.description,
+          createdAt: vocabularySets.createdAt,
+          userId: vocabularySets.userId,
+          flashCardsCount: count(flashCards.id),
+        })
         .from(vocabularySets)
-        .where(eq(vocabularySets.userId, userId));
+        .where(eq(vocabularySets.userId, userId))
+        .leftJoin(flashCards, eq(flashCards.vocabularySetId, vocabularySets.id))
+        .groupBy(vocabularySets.id);
     } catch (error) {
-      throw HttpError.InternalServerError({
+      throw ServiceError.DatabaseError({
         message: error.message,
         stack: error.stack,
       });
@@ -64,3 +75,9 @@ class VocabularySetRepository implements IVocabularySetRepository {
 }
 
 export default VocabularySetRepository;
+
+export type VocabularySetWithFlashCardsCount =
+  VocabularySet & {
+    flashCardsCount: number;
+  }
+
