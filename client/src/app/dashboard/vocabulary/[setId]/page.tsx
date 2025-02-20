2ede335db4c5d8b8dc0c3515e060638416851924
KornelJanczak <korneljanczak10@gmail.com>
2025-02-20 22:48:14 +0100

    feat: implement VocabularySet component to fetch and display vocabulary set details

--- client/src/app/dashboard/vocabulary/[setId]/page.tsx
+++ client/src/app/dashboard/vocabulary/[setId]/page.tsx
@@ -1,3 +1,13 @@
-export default function VocabularySet() {
-    
+import { vocabularySetService } from "@/common/services/vocabulary-set/vocabulary-set.service";
+import VocabularySetEdit from "@/components/vocabulary-set/vocabulary-set-edit";
+
+export default async function VocabularySet({
+  params,
+}: {
+  params: { setId: string };
+}) {
+  const vocabularySet = await vocabularySetService.getVocabularySetDetails(
+    params.setId
+  );
+  return <VocabularySetEdit vocabularySet={vocabularySet} />;
 }
