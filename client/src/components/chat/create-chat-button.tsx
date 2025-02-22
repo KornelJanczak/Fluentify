0bcec5cdc58144e1e05cfec0f73f051ccf63aab9
KornelJanczak <korneljanczak10@gmail.com>
2025-02-22 14:56:58 +0100

    refactor: remove session cookie from CreateChatButton and update redirect path

--- client/src/components/chat/create-chat-button.tsx
+++ client/src/components/chat/create-chat-button.tsx
@@ -1,20 +1,14 @@
 "use client";
+
 import { redirect } from "next/navigation";
 
-export default function CreateChatButton({
-  sessionCookie,
-}: {
-  sessionCookie: string;
-}) {
+export default function CreateChatButton() {
   const createChatHandler = async () => {
-    console.log("createChatHandler", sessionCookie);
-
     const response = await fetch("http://localhost:5000/api/v1/create-chat", {
       method: "POST",
       credentials: "include",
       headers: {
         "Content-Type": "application/json",
-        Cookie: sessionCookie,
       },
       body: JSON.stringify({
         title: "Chat with AI",
@@ -24,10 +18,10 @@ export default function CreateChatButton({
     if (!response.ok) {
       throw new Error("Failed to create chat");
     }
-    const chatId = await response.json();
-    console.log(chatId);
 
-    redirect(`/chat/${chatId}`);
+    const chatId = await response.json();
+
+    redirect(`/dashboard/chat/${chatId}`);
   };
 
   return (
