fb0e4bd44c179d8919c9062fd628a3a8a313741a
KornelJanczak <korneljanczak10@gmail.com>
2025-02-24 23:53:17 +0100

    feat: add custom hook for updating vocabulary sets with success and error notifications

---
+++ client/src/common/hooks/use-update-vocabulary-set.ts
@@ -0,0 +1,43 @@
+"use client";
+
+import { useMutation } from "@tanstack/react-query";
+import { clientApi } from "../api/client-api";
+import { FlashCard } from "../api/services/vocabulary-set.service";
+import { toast } from "sonner";
+
+export const useUpdateVocabularySet = () => {
+  const mutation = useMutation({
+    mutationFn: async ({ id, vocabularySet }: UpdateVocabularySetRequest) => {
+      console.log("id", id);
+      console.log("vocabularySet", vocabularySet);
+
+      (
+        await clientApi.put<UpdateVocabularySetResponse>(
+          `/vocabulary-set/${id}`,
+          vocabularySet
+        )
+      ).data;
+    },
+    onSuccess: () => {
+      toast.success("Vocabulary set has been saved successfully!");
+    },
+    onError: () => {
+      toast.error("Failed to save vocabulary set!");
+    },
+  });
+
+  return mutation;
+};
+
+export type UpdateVocabularySetResponse = {
+  vocabularySetId: string;
+};
+
+export type UpdateVocabularySetRequest = {
+  id: string;
+  vocabularySet: {
+    title?: string;
+    description?: string;
+    flashCards?: Omit<FlashCard, "vocabularySetId">[];
+  };
+};
