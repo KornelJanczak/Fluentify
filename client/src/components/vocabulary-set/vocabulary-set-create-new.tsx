42f46e6c52fb57c02b3420dfe27b2b0c70c14edb
KornelJanczak <korneljanczak10@gmail.com>
2025-02-24 23:54:07 +0100

    fix: refactor VocabularySetCreateNew component to simplify state management and update props

--- client/src/components/vocabulary-set/vocabulary-set-create-new.tsx
+++ client/src/components/vocabulary-set/vocabulary-set-create-new.tsx
@@ -2,17 +2,11 @@
 
 import { useFlashCardsStore } from "@/common/hooks/use-flash-cards-store";
 import VocabularySet from "./vocabulary-set";
-import { useCreateVocabularySet } from "@/common/services/vocabulary-set/hooks/use-create-vocabulary-set";
+import { useCreateVocabularySet } from "@/common/hooks/use-create-vocabulary-set";
 import { FlashCardsSetFormValues } from "@/common/hooks/use-flash-cards-set-form";
 
 export default function VocabularySetCreateNew() {
-  const {
-    flashCards,
-    addFlashCard,
-    deleteFlashCard,
-    flashCardsNumber,
-    resetState,
-  } = useFlashCardsStore((state) => state);
+  const { flashCards, resetState } = useFlashCardsStore((state) => state);
   const { mutate } = useCreateVocabularySet();
 
   const handleFormSubmit = (values: FlashCardsSetFormValues) => {
@@ -25,11 +19,10 @@ export default function VocabularySetCreateNew() {
 
   return (
     <VocabularySet
+      markdownContent="Create new vocabulary set"
+      buttonContent="Create"
       flashCards={flashCards}
-      flashCardsNumber={flashCardsNumber}
       onFormSubmit={handleFormSubmit}
-      onDeleteFlashCard={deleteFlashCard}
-      onAddFlashCard={addFlashCard}
     />
   );
 }
