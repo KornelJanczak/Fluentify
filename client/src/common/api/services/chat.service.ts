5e57116393239c40139e550313832bad304a95e8
KornelJanczak <korneljanczak10@gmail.com>
2025-02-24 23:53:05 +0100

    feat: add AuthService, ChatService, MessagesService, and VocabularySetService for enhanced API interactions

---
+++ client/src/common/api/services/chat.service.ts
@@ -0,0 +1,45 @@
+import { HttpError } from "@/common/api/rest-helper";
+import { ServerAPI, serverApi } from "@/common/api/server-api";
+
+interface IChatService {
+  getChatsHistory(): Promise<ChatsResponse>;
+}
+
+const chatKey = ["chat"];
+
+class ChatService implements IChatService {
+  public serverApi: ServerAPI;
+  private BASIC_PATH = "/chats";
+
+  constructor(serverApi: ServerAPI) {
+    this.serverApi = serverApi;
+  }
+
+  public async getChatsHistory() {
+    try {
+      return (
+        await this.serverApi.get<ChatsResponse>(this.BASIC_PATH, {
+          next: { tags: [chatKey.join()] },
+        })
+      ).data;
+    } catch (error) {
+      if (!(error instanceof HttpError)) {
+        throw error;
+      }
+
+      throw error;
+    }
+  }
+}
+
+export const chatService = new ChatService(serverApi);
+
+export type Chat = {
+  id: string;
+  title: string;
+  usedTokens: number;
+  startedAt: Date;
+  userId: string;
+};
+
+export type ChatsResponse = Chat[] | null;
---
+++ client/src/common/api/services/vocabulary-set.service.ts
@@ -0,0 +1,84 @@
+import { HttpError } from "@/common/api/rest-helper";
+import { ServerAPI, serverApi } from "@/common/api/server-api";
+
+interface IVocabularySetService {
+  getVocabularySets(): Promise<VocabularySetResponse>;
+  getVocabularySetDetails(id: string): Promise<VocabularySetDetailsResponse>;
+}
+
+const vocabularySetKey = ["vocabularySet"];
+
+class VocabularySetService implements IVocabularySetService {
+  public serverApi: ServerAPI;
+  private BASIC_PATH = "/vocabulary-set";
+
+  constructor(serverApi: ServerAPI) {
+    this.serverApi = serverApi;
+  }
+
+  public async getVocabularySets(): Promise<VocabularySetResponse> {
+    try {
+      return (
+        await this.serverApi.get<VocabularySetResponse>(this.BASIC_PATH, {
+          next: { tags: [vocabularySetKey.join()] },
+        })
+      ).data;
+    } catch (error) {
+      if (!(error instanceof HttpError)) {
+        throw error;
+      }
+
+      throw error;
+    }
+  }
+
+  public async getVocabularySetDetails(
+    id: string
+  ): Promise<VocabularySetDetailsResponse> {
+    try {
+      return (
+        await this.serverApi.get<VocabularySetDetailsResponse>(
+          `${this.BASIC_PATH}/${id}`,
+          {
+            next: { tags: [vocabularySetKey.join()] },
+          }
+        )
+      ).data;
+    } catch (error) {
+      if (!(error instanceof HttpError)) {
+        throw error;
+      }
+
+      throw error;
+    }
+  }
+}
+
+export const vocabularySetService = new VocabularySetService(serverApi);
+
+export type VocabularySet = {
+  id: string;
+  title: string;
+  description: string;
+  createdAt: Date;
+  userId: string;
+  flashCardsCount: number;
+};
+
+export type FlashCard = {
+  id: string;
+  definition: string;
+  translation: string;
+  vocabularySetId: string;
+};
+
+export type VocabularySetWithFlashCards = Omit<
+  VocabularySet,
+  "flashCardsCount"
+> & {
+  flashCards: Omit<FlashCard, "vocabularySetId">[];
+};
+
+export type VocabularySetResponse = VocabularySet[] | null;
+
+export type VocabularySetDetailsResponse = VocabularySetWithFlashCards | null;
