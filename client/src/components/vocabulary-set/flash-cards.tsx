db614105634483beef7efd142ef5be9aa13f8d9d
KornelJanczak <korneljanczak10@gmail.com>
2025-02-24 23:53:46 +0100

    fix: refactor FlashCards component to use updated FlashCard type and simplify props

--- client/src/components/vocabulary-set/flash-cards.tsx
+++ client/src/components/vocabulary-set/flash-cards.tsx
@@ -1,35 +1,27 @@
 "use client";
 
-import { FlashCard as FlashCardType } from "@/common/services/flash-card/flash-card.interfaces";
-import FlashCard from "./flash-card";
+import { type FlashCard } from "@/common/api/services/vocabulary-set.service";
+import FlashCardComponent from "./flash-card";
 import { AnimatePresence, motion, useIsPresent } from "framer-motion";
 import { scaleAndFadeAnimation } from "@/common/animations";
 
 export interface FlashCardsProps {
-  flashCards: Omit<FlashCardType, "vocabularySetId">[];
-  onDeleteFlashCard: (flashCardId: string) => void;
+  flashCards: Omit<FlashCard, "vocabularySetId">[];
 }
 
-export default function FlashCards({
-  flashCards,
-  onDeleteFlashCard,
-}: FlashCardsProps) {
+export default function FlashCards({ flashCards }: FlashCardsProps) {
   const isPresent = useIsPresent();
 
   return (
     <div className="flex flex-col space-y-4">
       <AnimatePresence>
-        {flashCards.map((flashCard: FlashCardType, index: number) => (
+        {flashCards.map((flashCard: FlashCard, index: number) => (
           <motion.div
             key={flashCard.id}
             layout
             {...scaleAndFadeAnimation(isPresent)}
           >
-            <FlashCard
-              flashCard={flashCard}
-              index={index + 1}
-              onDeleteFlashCard={onDeleteFlashCard}
-            />
+            <FlashCardComponent flashCard={flashCard} index={index + 1} />
           </motion.div>
         ))}
       </AnimatePresence>
