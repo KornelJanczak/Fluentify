2d4e956cb717356d658c7ccb7a26a33f3cbd07f4
KornelJanczak <korneljanczak10@gmail.com>
2025-02-24 23:53:57 +0100

    fix: refactor VocabularySet component to accept dynamic markdown and button content

--- client/src/components/vocabulary-set/vocabulary-set.tsx
+++ client/src/components/vocabulary-set/vocabulary-set.tsx
@@ -9,43 +9,35 @@ import { Button } from "../ui/button";
 import SectionWrapper from "../section-wrapper";
 
 export interface VocabularySet extends FlashCardsProps {
+  markdownContent: string;
+  buttonContent: string;
   defaultFormValues?: FlashCardsSetFormValues;
-  flashCardsNumber: number;
-  onFormSubmit: (values) => void;
-  onDeleteFlashCard: (flashCardId: string) => void;
-  onAddFlashCard: () => void;
+  onFormSubmit: (values: FlashCardsSetFormValues) => void;
 }
 
 export default function VocabularySet({
+  markdownContent,
+  buttonContent,
   defaultFormValues,
   flashCards,
-  flashCardsNumber,
   onFormSubmit,
-  onDeleteFlashCard,
-  onAddFlashCard,
 }: VocabularySet) {
   return (
     <SectionWrapper>
       <div className="flex flex-col space-y-6 w-full max-w-5xl">
-        <Markdown>## Create new vocabulary set</Markdown>
+        <Markdown>{`## ${markdownContent}`}</Markdown>
         <VocabularySetForm
           onSubmit={onFormSubmit}
           defaultValues={defaultFormValues}
         />
-        <FlashCards
-          flashCards={flashCards}
-          onDeleteFlashCard={onDeleteFlashCard}
-        />
-        <AddFlashCard
-          index={flashCardsNumber}
-          onAddFlashCard={onAddFlashCard}
-        />
+        <FlashCards flashCards={flashCards} />
+        <AddFlashCard />
         <Button
           type="submit"
           form="flash-cards-set-form"
           className="mt-4 self-end"
         >
-          Create
+          {buttonContent}
         </Button>
       </div>
     </SectionWrapper>
