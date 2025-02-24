c42614d8a355d423cdfeb4a36ea73cdffed2333e
KornelJanczak <korneljanczak10@gmail.com>
2025-02-24 23:54:29 +0100

    feat: add updateVocabularySetSchema for validating vocabulary set updates

--- server/src/features/vocabularySets/vocabularySets.schema.ts
+++ server/src/features/vocabularySets/vocabularySets.schema.ts
@@ -10,3 +10,15 @@ export const createVocabularySetSchema = z.object({
     })
   ),
 });
+
+export const updateVocabularySetSchema = z.object({
+  title: z.string().min(2),
+  description: z.string().min(2),
+  flashCards: z.array(
+    z.object({
+      id: z.string().min(2),
+      translation: z.string().min(2),
+      definition: z.string().min(2),
+    })
+  ),
+});
