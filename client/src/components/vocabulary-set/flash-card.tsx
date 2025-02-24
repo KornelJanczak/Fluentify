efd3d3a2486f89d61e50c9599ef20c4319735088
KornelJanczak <korneljanczak10@gmail.com>
2025-02-24 23:53:43 +0100

    fix: update FlashCard component to improve layout and input styling

--- client/src/components/vocabulary-set/flash-card.tsx
+++ client/src/components/vocabulary-set/flash-card.tsx
@@ -2,7 +2,7 @@
 
 import * as React from "react";
 import { Card, CardContent, CardHeader } from "../ui/card";
-import { type FlashCard } from "@/common/services/flash-card/flash-card.interfaces";
+import { type FlashCard } from "@/common/api/services/vocabulary-set.service";
 import { Separator } from "@/components/ui/separator";
 import { Markdown } from "../markdown";
 import { Input } from "../ui/input";
@@ -12,11 +12,10 @@ import { useFlashCardsStore } from "@/common/hooks/use-flash-cards-store";
 interface FlashCardProps extends React.HTMLAttributes<HTMLDivElement> {
   flashCard: Omit<FlashCard, "vocabularySetId">;
   index: number;
-  onDeleteFlashCard: (flashCardId: string) => void;
 }
 
 export default function FlashCard(props: FlashCardProps) {
-  const { flashCard, index, onDeleteFlashCard, ...restOfProps } = props;
+  const { flashCard, index, ...restOfProps } = props;
   const numberOfFlasCard = `${index}`;
   const {
     updateDefinitionOnChange,
@@ -29,25 +28,38 @@ export default function FlashCard(props: FlashCardProps) {
       <CardHeader className="px-0 pt-2">
         <div className="flex items-center justify-between">
           <Markdown className="px-4">{numberOfFlasCard}</Markdown>
-          <DeleteButton onClick={() => deleteFlashCard(flashCard.id)} />
+          <DeleteButton
+            className="bg-none bg-inherit px-3 py-1 mr-2 my-1 hover:bg-secondary rounded-full"
+            onClick={() => deleteFlashCard(flashCard.id)}
+          />
         </div>
         <Separator />
       </CardHeader>
-      <CardContent className="flex space-x-7">
-        <Input
-          onChange={(e) =>
-            updateDefinitionOnChange(flashCard.id, e.target.value)
-          }
-          defaultValue={flashCard.definition}
-          placeholder="Definition"
-        />
-        <Input
-          onChange={(e) =>
-            updateTranslationOnChange(flashCard.id, e.target.value)
-          }
-          defaultValue={flashCard.translation}
-          placeholder="Translation"
-        />
+      <CardContent className="space-y-10 sm:flex sm:space-x-14 sm:space-y-0">
+        <div className="w-full">
+          <Input
+            variant="secondary"
+            defaultValue={flashCard.definition}
+            onChange={(e) =>
+              updateDefinitionOnChange(flashCard.id, e.target.value)
+            }
+          />
+          <Markdown className="text-secondary-foreground text-sm pt-2.5">
+            Definition
+          </Markdown>
+        </div>
+        <div className="w-full">
+          <Input
+            variant="secondary"
+            defaultValue={flashCard.translation}
+            onChange={(e) =>
+              updateTranslationOnChange(flashCard.id, e.target.value)
+            }
+          />
+          <Markdown className="text-secondary-foreground text-sm pt-2.5">
+            Translation
+          </Markdown>
+        </div>
       </CardContent>
     </Card>
   );
