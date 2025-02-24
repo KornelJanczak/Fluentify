d71aa0d581054ccd70831461a951c9f76193dbd6
KornelJanczak <korneljanczak10@gmail.com>
2025-02-24 23:53:41 +0100

    fix: refactor AddFlashCard component to use global flash card store for state management

--- client/src/components/vocabulary-set/add-flash-card.tsx
+++ client/src/components/vocabulary-set/add-flash-card.tsx
@@ -4,25 +4,26 @@ import * as React from "react";
 import { Card, CardContent } from "../ui/card";
 import { Markdown } from "../markdown";
 import { Button } from "../ui/button";
+import { useFlashCardsStore } from "@/common/hooks/use-flash-cards-store";
 
-interface AddFlashCard extends React.HTMLAttributes<HTMLDivElement> {
-  index: number;
-  onAddFlashCard: () => void;
-}
+interface AddFlashCard extends React.HTMLAttributes<HTMLDivElement> {}
 
 export default function AddFlashCard(props: AddFlashCard) {
-  const { index, onAddFlashCard, ...restOfProps } = props;
-  const nextNumberOfFlashCard = `${index + 1}`;
+  const { addFlashCard, flashCardsNumber } = useFlashCardsStore(
+    (state) => state
+  );
+
+  const nextNumberOfFlashCard = `${flashCardsNumber + 1}`;
 
   return (
-    <Card className="" {...restOfProps}>
+    <Card className="" {...props}>
       <CardContent className="p-4">
         <div className="relative">
           <Markdown className="absolute  top-1/2  transform  -translate-y-1/2">
             {nextNumberOfFlashCard}
           </Markdown>
           <div className="flex justify-center items-center">
-            <Button variant="secondary" onClick={onAddFlashCard}>
+            <Button variant="secondary" onClick={addFlashCard}>
               ADD FLASHCARD
             </Button>
           </div>
