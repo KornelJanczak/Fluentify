a3682fdd3ea97b24f317984a1eb05be8a56bdb83
KornelJanczak <korneljanczak10@gmail.com>
2025-02-25 23:39:41 +0100

    feat: add pagination support to SetsPage component

--- client/src/app/dashboard/vocabulary/sets/page.tsx
+++ client/src/app/dashboard/vocabulary/sets/page.tsx
@@ -1,7 +1,14 @@
 import { vocabularySetService } from "@/common/api/services/vocabulary-set.service";
 import VocabularySets from "@/components/vocabulary-sets";
 
-export default async function SetsPage() {
-  const vocabularySets = await vocabularySetService.getVocabularySets();
+export default async function SetsPage({
+  searchParams,
+}: {
+  searchParams: {
+    page: string;
+  };
+}) {
+  const page = await searchParams.page;
+  const vocabularySets = await vocabularySetService.getVocabularySets(page);
   return <VocabularySets vocabularySets={vocabularySets} />;
 }
