00b5f6175a5e0c5d7548be7e9784829ea0d66265
KornelJanczak <korneljanczak10@gmail.com>
2025-02-25 23:40:01 +0100

    refactor: update initial state and add flashCardsNumber management in useFlashCardsStore

--- client/src/common/hooks/use-flash-cards-store.ts
+++ client/src/common/hooks/use-flash-cards-store.ts
@@ -17,66 +17,68 @@ interface FlashCardsStore {
 }
 
 const initialState = {
-  flashCards: [],
-  flashCardsNumber: 0,
+  flashCards: [
+    { id: uuidv4(), translation: "", definition: "" },
+    { id: uuidv4(), translation: "", definition: "" },
+  ],
+  flashCardsNumber: 2,
 };
 
 export const useFlashCardsStore = create<FlashCardsStore>()(
-  persist(
-    (set) => ({
-      ...initialState,
-      addFlashCards: (flashCards: FlashCardStore[]) =>
-        set(() => ({
-          flashCards: flashCards,
-        })),
-      addFlashCard: () =>
-        set((state) => ({
-          flashCards: [
-            ...state.flashCards,
-            { id: uuidv4(), translation: "", definition: "" },
-          ],
-          flashCardsNumber: state.flashCardsNumber + 1,
-        })),
-      deleteFlashCard: (flashCardId: string) =>
-        set((state) => ({
-          flashCards: state.flashCards.filter(
-            (flashCard: FlashCardStore) => flashCard.id !== flashCardId
-          ),
-          flashCardsNumber: state.flashCardsNumber - 1,
-        })),
-      updateDefinitionOnChange: (flashCardId: string, definition: string) =>
-        set((state) => ({
-          flashCards: state.flashCards.map((flashCard) =>
-            flashCard.id === flashCardId
-              ? {
-                  ...flashCard,
-                  definition: definition.trim().toLocaleLowerCase(),
-                }
-              : flashCard
-          ),
-          flashCardsNumber: state.flashCardsNumber,
-        })),
-      updateTranslationOnChange: (flashCardId: string, translation: string) =>
-        set((state) => ({
-          flashCards: state.flashCards.map((flashCard) =>
-            flashCard.id === flashCardId
-              ? {
-                  ...flashCard,
-                  translation: translation.trim().toLocaleLowerCase(),
-                }
-              : flashCard
-          ),
-          flashCardsNumber: state.flashCardsNumber,
-        })),
-      resetState: () => {
-        set(initialState);
-      },
-    }),
-    {
-      name: "flash-cards-storage",
-      storage: createJSONStorage(() => localStorage),
-    }
-  )
+  (set) => ({
+    ...initialState,
+    addFlashCards: (flashCards: FlashCardStore[]) =>
+      set(() => ({
+        flashCardsNumber: flashCards.length,
+        flashCards: flashCards,
+      })),
+    addFlashCard: () =>
+      set((state) => ({
+        flashCards: [
+          ...state.flashCards,
+          { id: uuidv4(), translation: "", definition: "" },
+        ],
+        flashCardsNumber: state.flashCardsNumber + 1,
+      })),
+    deleteFlashCard: (flashCardId: string) =>
+      set((state) => ({
+        flashCards: state.flashCards.filter(
+          (flashCard: FlashCardStore) => flashCard.id !== flashCardId
+        ),
+        flashCardsNumber: state.flashCardsNumber - 1,
+      })),
+    updateDefinitionOnChange: (flashCardId: string, definition: string) =>
+      set((state) => ({
+        flashCards: state.flashCards.map((flashCard) =>
+          flashCard.id === flashCardId
+            ? {
+                ...flashCard,
+                definition: definition.trim().toLocaleLowerCase(),
+              }
+            : flashCard
+        ),
+        flashCardsNumber: state.flashCardsNumber,
+      })),
+    updateTranslationOnChange: (flashCardId: string, translation: string) =>
+      set((state) => ({
+        flashCards: state.flashCards.map((flashCard) =>
+          flashCard.id === flashCardId
+            ? {
+                ...flashCard,
+                translation: translation.trim().toLocaleLowerCase(),
+              }
+            : flashCard
+        ),
+        flashCardsNumber: state.flashCardsNumber,
+      })),
+    resetState: () => {
+      set(initialState);
+    },
+  })
+  // {
+  //   name: "flash-cards-storage",
+  //   storage: createJSONStorage(() => localStorage),
+  // }
 );
 
 export type FlashCardStore = Omit<FlashCard, "vocabularySetId">;
