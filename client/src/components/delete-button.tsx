deb26a30c8e1949c9101c5137675ed5ea625e932
KornelJanczak <korneljanczak10@gmail.com>
2025-02-26 23:31:49 +0100

    feat: enhance DeleteButton styling with hover effect and rounded corners

--- client/src/components/delete-button.tsx
+++ client/src/components/delete-button.tsx
@@ -5,7 +5,10 @@ interface DeleteButtonProps extends ButtonProps {}
 
 export default function DeleteButton({ ...props }: DeleteButtonProps) {
   return (
-    <Button className="bg-none bg-inherit" {...props}>
+    <Button
+      className="bg-none bg-inherit hover:bg-secondary rounded-full"
+      {...props}
+    >
       <Trash2Icon size={25} />
     </Button>
   );
