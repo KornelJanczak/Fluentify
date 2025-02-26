3685fef9d6ef18121b9a8c41606333775b585b04
KornelJanczak <korneljanczak10@gmail.com>
2025-02-26 23:31:53 +0100

    feat: add custom hook for deleting vocabulary sets with success and error handling

---
+++ client/src/common/hooks/use-delete-vocabulary-set.ts
@@ -0,0 +1,25 @@
+"use client";
+
+import { useMutation } from "@tanstack/react-query";
+import { clientApi } from "../api/client-api";
+import { toast } from "sonner";
+import { useRouter } from "next/navigation";
+
+export const useDeleteVocabularySet = () => {
+  const router = useRouter();
+
+  const mutation = useMutation({
+    mutationFn: async (id: string) =>
+      (await clientApi.delete(`/vocabulary-set/${id}`)).data,
+
+    onSuccess: () => {
+      toast.success("Vocabulary set has been deleted successfully!");
+      router.push("/dashboard/vocabulary/sets");
+    },
+    onError: () => {
+      toast.error("Failed to delete vocabulary set!");
+    },
+  });
+
+  return mutation;
+};
