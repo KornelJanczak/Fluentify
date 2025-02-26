fea6a6734d9a3319db2ba626fc1a7a63c178e209
KornelJanczak <korneljanczak10@gmail.com>
2025-02-26 23:32:02 +0100

    feat: add VocabularySetAlertDialog component for confirming deletion of vocabulary sets

---
+++ client/src/components/vocabulary-set/vocabulary-set-alert-dialog.tsx
@@ -0,0 +1,50 @@
+"use client";
+
+import { useDeleteVocabularySet } from "@/common/hooks/use-delete-vocabulary-set";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+import { Button } from "@/components/ui/button";
+
+interface VocabularySetAlertDialogProps {
+  id: string;
+}
+
+export function VocabularySetAlertDialog({
+  id,
+}: VocabularySetAlertDialogProps) {
+  const { mutate } = useDeleteVocabularySet();
+
+  return (
+    <AlertDialog>
+      <AlertDialogTrigger asChild>
+        <Button variant="destructive" className="self-end">
+          Delete
+        </Button>
+      </AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+          <AlertDialogDescription>
+            This action cannot be undone. This will permanently delete
+            vocabulary set and its history.
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
+          <AlertDialogAction onClick={() => mutate(id)}>
+            Continue
+          </AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
+}
