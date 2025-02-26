da327336a484975c421dceb54fca74820fdfab10
KornelJanczak <korneljanczak10@gmail.com>
2025-02-26 23:32:05 +0100

    feat: enhance SectionWrapper component to accept additional class names and props

--- client/src/components/section-wrapper.tsx
+++ client/src/components/section-wrapper.tsx
@@ -1,12 +1,19 @@
 import * as React from "react";
+import { cn } from "@/lib/utils";
 
-export default function SectionWrapper({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+interface SectionWrapperProps extends React.HTMLAttributes<HTMLDivElement> {}
+
+export default function SectionWrapper(props: SectionWrapperProps) {
+  const { children, className, ...rest } = props;
+
   return (
-    <section className="flex flex-col justify-center items-center w-full space-y-4 py-10 px-8  ">
+    <section
+      className={cn(
+        "flex flex-col justify-center items-center w-full space-y-4 py-10 px-8",
+        className
+      )}
+      {...rest}
+    >
       {children}
     </section>
   );
