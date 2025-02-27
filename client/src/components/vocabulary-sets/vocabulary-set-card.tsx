eb98e36180555b86aa63266a5800d1e20f092e10
KornelJanczak <korneljanczak10@gmail.com>
2025-02-27 23:59:55 +0100

    feat: refactor VocabularySetCard to extend HTML attributes and remove Link component

--- client/src/components/vocabulary-sets/vocabulary-set-card.tsx
+++ client/src/components/vocabulary-sets/vocabulary-set-card.tsx
@@ -5,31 +5,33 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 import { type VocabularySet } from "@/common/api/services/vocabulary-set.service";
-import Link from "next/link";
+import React from "react";
 
-interface VocabularySetCardProps {
+interface VocabularySetCardProps extends React.HTMLAttributes<HTMLDivElement> {
   vocabularySet: VocabularySet;
 }
 
 export default function VocabularySetCard({
   vocabularySet,
+  ...props
 }: VocabularySetCardProps) {
-  const { id, title, description, flashCardsCount } = vocabularySet;
+  const { title, description, flashCardsCount } = vocabularySet;
 
   const termOrTerms = flashCardsCount === 1 ? "term" : "terms";
 
   return (
-    <Link href={`/dashboard/vocabulary/${id}`}>
-      <Card className="relative transition-all duration-300 hover:shadow-lg hover:shadow-primary/50">
-        <CardHeader className="relative">
-          <CardTitle>{title}</CardTitle>
-          <CardDescription>{description}</CardDescription>
-          <div className="absolute top-0 right-0 p-4">
-            {flashCardsCount} {termOrTerms}
-          </div>
-        </CardHeader>
-        <div className="absolute inset-0 border-b-4 border-transparent transition-all duration-300 hover:border-primary/95 hover:rounded" />
-      </Card>
-    </Link>
+    <Card
+      className="relative transition-all duration-300 hover:shadow-lg hover:shadow-primary/50"
+      {...props}
+    >
+      <CardHeader className="relative">
+        <CardTitle>{title}</CardTitle>
+        <CardDescription>{description}</CardDescription>
+        <div className="absolute top-0 right-0 p-4">
+          {flashCardsCount} {termOrTerms}
+        </div>
+      </CardHeader>
+      <div className="absolute inset-0 border-b-4 border-transparent transition-all duration-300 hover:border-primary/95 hover:rounded" />
+    </Card>
   );
 }
