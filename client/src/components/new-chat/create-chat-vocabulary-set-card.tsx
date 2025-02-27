e8f50d8cf674f4547a4e40e77bf37be645ff6311
KornelJanczak <korneljanczak10@gmail.com>
2025-02-27 23:59:46 +0100

    feat: add CreateChatVocabularySetCard component for initiating chats with vocabulary sets

---
+++ client/src/components/new-chat/create-chat-vocabulary-set-card.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import { useCreateChat } from "@/common/hooks/use-create-chat";
+import VocabularySetCard from "../vocabulary-sets/vocabulary-set-card";
+import type { VocabularySet } from "@/common/api/services/vocabulary-set.service";
+
+interface CreateChatVocabularySetCardProps {
+  vocabularySet: VocabularySet;
+  category: string;
+  topic: string;
+}
+
+export default function CreateChatVocabularySetCard({
+  vocabularySet,
+  category,
+  topic,
+}: CreateChatVocabularySetCardProps) {
+  const { id: vocabularySetId } = vocabularySet;
+  const { mutate } = useCreateChat();
+
+  return (
+    <VocabularySetCard
+      onClick={() => mutate({ category, topic, vocabularySetId })}
+      vocabularySet={vocabularySet}
+      className="cursor-pointer"
+    />
+  );
+}
