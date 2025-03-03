0a12b9b61caee7aa4d7a3d6f035b5c9333e3d5d1
KornelJanczak <korneljanczak10@gmail.com>
2025-03-03 23:05:38 +0100

    fix: refactor ChatStreamService to integrate audio uploading and streamline message saving

--- server/src/features/chat/chat.services/chatStream.service.ts
+++ server/src/features/chat/chat.services/chatStream.service.ts
@@ -8,51 +8,39 @@ import {
   type IOnFinishStream,
 } from "../chat.interfaces/chatStream.service.interfaces";
 import { IAudioGeneratorService } from "@chat/chat.interfaces/audioGenerator.service.interfaces";
+import { type Message } from "@shared/services/db/schema";
 import { type IChatRepository } from "@shared/repositories/chat.repository";
 import { type IMessagesRepository } from "@shared/repositories/messages.repository";
-import { Logger } from "winston";
 import { Chat } from "@services/db/schema";
 import { ISystemPromptService } from "@chat/chat.interfaces/systemPrompt.service.interface";
-import ChatQueue from "@services/queues/chat.queue";
-import { BaseCache } from "@services/redis/base.cache";
 import { ServiceError } from "@shared/errors/service.error";
+import { IAudioUploaderService } from "@chat/chat.interfaces/audioUploader.service.interfaces";
 
 class ChatStreamService implements IChatStreamService {
   private readonly audioGeneratorService: IAudioGeneratorService;
   private readonly systemPromptService: ISystemPromptService;
+  private readonly audioUploaderService: IAudioUploaderService;
   private readonly messagesRepository: IMessagesRepository;
   private readonly chatRepository: IChatRepository;
-  private readonly chatCache: BaseCache;
-  private readonly chatQueue: ChatQueue;
-  private readonly logger: Logger;
 
   constructor({
     audioGeneratorService,
     systemPromptService,
+    audioUploaderService,
     messagesRepository,
     chatRepository,
-    chatCache,
-    chatQueue,
-    logger,
   }: IChatStreamServiceDependencies) {
     this.audioGeneratorService = audioGeneratorService;
     this.systemPromptService = systemPromptService;
+    this.audioUploaderService = audioUploaderService;
     this.messagesRepository = messagesRepository;
     this.chatRepository = chatRepository;
-    this.chatCache = chatCache;
-    this.chatQueue = chatQueue;
-    this.logger = logger;
   }
 
   public async startChatStream(chatRequest: IChatRequest): Promise<void> {
-    // Check if chat exists
     const chat = await this.getChat(chatRequest.chatId);
-
-    // Extract last user message
     const lastUserMessage = this.extractLastUserMessage(chatRequest.messages);
-
-    // Save user message
-    await this.saveMessage(chat.id, lastUserMessage);
+    await this.saveMessage(chat.id, lastUserMessage, null);
 
     // Get system prompt to start the conversation
     const sytemPrompt = await this.systemPromptService.getSystemPrompt({
@@ -79,11 +67,9 @@ class ChatStreamService implements IChatStreamService {
   }
 
   private extractLastUserMessage(messages: CoreMessage[]): CoreMessage {
-    const lastUserMessage = messages
+    return messages
       .filter((message: CoreMessage) => message.role === "user")
       .at(-1);
-
-    return lastUserMessage;
   }
 
   private async streamChatToResponse(
@@ -98,21 +84,17 @@ class ChatStreamService implements IChatStreamService {
           model: customAi("gpt-3.5-turbo"),
           system: systemPrompt,
           messages,
-          onFinish: async ({ text, usage }) =>
+          onFinish: async ({ text, usage }) => {
             await this.onFinishStream({
               chatId,
               content: text,
               streamWriter,
               tutorId,
-              usedTokens: Number(usage),
-            }),
+              usedTokens: usage.totalTokens,
+            });
+          },
         });
-
-        // for await (const part of result.fullStream) {
-        //   const error = part.type.includes("error");
-
-        //   if (error)
-
+        
         return result.mergeIntoDataStream(streamWriter);
       },
     });
@@ -130,64 +112,26 @@ class ChatStreamService implements IChatStreamService {
       tutorId
     );
 
-    // Attache audio content to the stream
     streamWriter.writeMessageAnnotation({
       type: "audio",
       data: JSON.stringify(audioContent),
     });
 
-    // this.chatQueue.addChatJob("saveChatMessages", [
-    //   {
-    //     id: uuidv4(),
-    //     content: text,
-    //     createdAt: new Date(),
-    //     role: "assistant",
-    //     chatId: chatId,
-    //     usedTokens: 0,
-    //   },
-    // ]);
+    const messageId = await this.saveMessage(
+      chatId,
+      { content, role: "assistant" },
+      usedTokens
+    );
 
-    await this.saveAssistantMessage(chatId, content);
-    // await this.saveMessage(chatId, { content, role: "assistant" }, usedTokens);
-  }
-
-  private async saveUserMessage(
-    chatId: string,
-    message: CoreMessage
-  ): Promise<void> {
-    await this.messagesRepository.saveMessages([
-      {
-        id: uuidv4(),
-        ...message,
-        createdAt: new Date(),
-        chatId: chatId,
-        usedTokens: 0,
-      },
-    ]);
-  }
-
-  private async saveAssistantMessage(
-    chatId: string,
-    text: string
-  ): Promise<void> {
-    await this.messagesRepository.saveMessages([
-      {
-        id: uuidv4(),
-        content: text,
-        role: "assistant",
-        createdAt: new Date(),
-        chatId: chatId,
-        usedTokens: 0,
-      },
-    ]);
+    await this.audioUploaderService.uploadAudio(audioContent, messageId);
   }
 
   private async saveMessage(
     chatId: string,
     message: CoreMessage,
     usedTokens?: number
-  ): Promise<void> {
-    await this.messagesRepository.saveMessages([
+  ): Promise<string> {
+    return await this.messagesRepository.saveMessages([
       {
         id: uuidv4(),
         ...message,
