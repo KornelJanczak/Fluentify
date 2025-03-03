18f42bc78917992de88e8a8c21dc15876ea64aeb
KornelJanczak <korneljanczak10@gmail.com>
2025-03-03 23:04:58 +0100

    fix: enhance NewChat component layout and functionality with improved topic selection and navigation

--- client/src/components/new-chat/index.tsx
+++ client/src/components/new-chat/index.tsx
@@ -7,6 +7,8 @@ import SectionWrapper from "../section-wrapper";
 import { type ChatTopic, chatTopics } from "./chat-topics";
 import ChooseVocabularySetDialog from "./choose-vocabulary-set-dialog";
 import type { VocabularySet } from "@/common/api/services/vocabulary-set.service";
+import { Separator } from "../ui/separator";
+import { useRouter } from "next/navigation";
 
 interface NewChatProps {
   vocabularySets: VocabularySet[];
@@ -14,34 +16,58 @@ interface NewChatProps {
 
 export default function NewChat({ vocabularySets }: NewChatProps) {
   const [selectedTopic, setSelectedTopic] = useState<null | ChatTopic>(null);
-
-  const handleSelectTopic = (topic: ChatTopic) => {
-    setSelectedTopic(topic);
-  };
+  const router = useRouter();
 
   return (
-    <SectionWrapper className="h-screen">
-      <Markdown>## Choose an option</Markdown>
-      {selectedTopic === null &&
-        chatTopics.map((topic) => (
-          <Button key={topic.title} onClick={() => handleSelectTopic(topic)}>
-            {topic.title}
+    <SectionWrapper className="h-screen ">
+      <div className="flex flex-col items-center gap-2 w-full max-w-3xl">
+        <Markdown className="text-lg">## Choose an option</Markdown>
+        <div className="flex w-full flex-col space-y-4 pb-2 md:w-auto md:flex-row md:space-y-0 md:space-x-6">
+          {selectedTopic === null &&
+            chatTopics.map((topic) => (
+              <Button
+                key={topic.title}
+                size="lg"
+                onClick={() => setSelectedTopic(topic)}
+              >
+                {topic.title}
+              </Button>
+            ))}
+          {selectedTopic &&
+            selectedTopic.categories.map((topic) =>
+              topic.category === "vocabulary" ? (
+                <ChooseVocabularySetDialog
+                  key={topic.title}
+                  title={topic.title}
+                  topic={topic.topic}
+                  category={topic.category}
+                  vocabularySets={vocabularySets}
+                />
+              ) : (
+                <Button key={topic.title} size="lg">
+                  {topic.title}
+                </Button>
+              )
+            )}
+        </div>
+        <Markdown className="text-sm py-2">
+          You'll be chatting with John
+        </Markdown>
+        <Separator />
+        <div className="flex w-full justify-between">
+          {selectedTopic && (
+            <Button variant="link" onClick={() => setSelectedTopic(null)}>
+              Back
+            </Button>
+          )}
+          <Button
+            variant="link"
+            onClick={() => router.push("/dashboard/vocabulary/sets")}
+          >
+            View past chats
           </Button>
-        ))}
-      {selectedTopic != null &&
-        selectedTopic.categories.map((topic) =>
-          topic.category === "vocabulary" ? (
-            <ChooseVocabularySetDialog
-              key={topic.title}
-              title={topic.title}
-              topic={topic.topic}
-              category={topic.category}
-              vocabularySets={vocabularySets}
-            />
-          ) : (
-            <Button key={topic.title}>{topic.title}</Button>
-          )
-        )}
+        </div>
+      </div>
     </SectionWrapper>
   );
 }
