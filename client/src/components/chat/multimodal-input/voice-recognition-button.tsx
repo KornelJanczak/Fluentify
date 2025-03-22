1e4bf1f252229d292d5bfb685d419b3ec4a506ff
KornelJanczak <korneljanczak10@gmail.com>
2025-03-22 20:50:23 +0100

    feat: add VoiceRecognitionButton component for speech recognition functionality

---
+++ client/src/components/chat/multimodal-input/voice-recognition-button.tsx
@@ -0,0 +1,57 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { Mic, Pause } from "lucide-react";
+import React from "react";
+import { toast } from "sonner";
+import SpeechRecognition from "react-speech-recognition";
+
+interface VoiceRecognationButtonProps
+  extends React.HTMLAttributes<HTMLButtonElement> {
+  listening: boolean;
+  browserSupportsSpeechRecognition: boolean;
+}
+
+const VoiceRecognationButton = ({
+  browserSupportsSpeechRecognition,
+  listening,
+  ...props
+}: VoiceRecognationButtonProps) => {
+  if (!browserSupportsSpeechRecognition)
+    return toast.error(
+      "Browser does not support speech recognition. You won't be able send voice message! Please use a Google Chrome browser."
+    );
+
+  return (
+    <>
+      {listening && (
+        <Button
+          className="rounded-full p-1.5 h-fit border dark:border-zinc-600"
+          {...props}
+          onClick={(e) => {
+            e.preventDefault();
+            SpeechRecognition.stopListening();
+          }}
+        >
+          <Pause />
+        </Button>
+      )}
+      {!listening && (
+        <Button
+          className="rounded-full p-1.5 h-fit border dark:border-zinc-600"
+          {...props}
+          onClick={(e) => {
+            e.preventDefault();
+            SpeechRecognition.startListening({
+              continuous: true,
+              language: "en-US",
+            });
+          }}
+        >
+          <Mic />
+        </Button>
+      )}
+    </>
+  );
+};
+export default VoiceRecognationButton;
