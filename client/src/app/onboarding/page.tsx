baa952c67b2c58d9a17b91029428bb8bae5b00a2
KornelJanczak <korneljanczak10@gmail.com>
2025-03-26 23:54:27 +0100

    feat: add OnboardingPage component for user onboarding experience

---
+++ client/src/app/onboarding/page.tsx
@@ -0,0 +1,22 @@
+import LearningSettingsForm from "@/components/settings/learning-settings-form";
+import { Card } from "@/components/ui/card";
+import Markdown from "react-markdown";
+
+export default function OnboardingPage() {
+  return (
+    <section className="flex min-h-screen items-center justify-center bg-zinc-50 px-4 py-16 md:py-32 dark:bg-transparent">
+      <Card className="max-w-xl p-8">
+        <Markdown className="text-center">
+          ## 👋 Welcome! Please answer a few quick questions to get started.
+          (It's necessary to contine)
+        </Markdown>
+        <LearningSettingsForm
+          learningLanguage="english"
+          nativeLanguage="polish"
+          learningLanguageLevel="B2"
+          buttonContent="Continue"
+        />
+      </Card>
+    </section>
+  );
+}
