c8953a17a96ada0063789d6295257a95a2517914
KornelJanczak <korneljanczak10@gmail.com>
2025-03-26 23:54:42 +0100

    feat: implement SettingsService for managing user settings operations

---
+++ server/src/modules/settings/settings.service.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@nestjs/common';
+import { SettingsRepository } from 'src/shared/repositories/settings.repository';
+import { Settings } from 'src/shared/db/db.schema';
+import { ServiceError } from 'src/common/service-error';
+import { CreateSettingsDto, UpdateSettingsDto } from './settings.dto';
+
+@Injectable()
+export class SettingsService {
+  constructor(private settingsRepository: SettingsRepository) {}
+
+  public async create(createSettingsDto: CreateSettingsDto): Promise<Settings> {
+    const newSetting = await this.settingsRepository.create(createSettingsDto);
+
+    if (!newSetting)
+      throw ServiceError.NotFoundError('Failed to create settings');
+
+    return newSetting;
+  }
+
+  public async findById(id: string): Promise<Settings> {
+    const setting = await this.settingsRepository.findById(id);
+
+    if (!setting)
+      throw ServiceError.NotFoundError(`Settings with id ${id} not found`);
+
+    return setting;
+  }
+
+  public async update(
+    id: string,
+    updateSettingsDto: UpdateSettingsDto,
+  ): Promise<string> {
+    const updatedSettingId = await this.settingsRepository.update(
+      id,
+      updateSettingsDto,
+    );
+
+    if (!updatedSettingId)
+      throw ServiceError.NotFoundError('Failed to update settings');
+
+    return updatedSettingId;
+  }
+}
