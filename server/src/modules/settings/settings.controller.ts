4aeaa8bb24f9739a1511f84d5456f852db02fb8a
KornelJanczak <korneljanczak10@gmail.com>
2025-03-26 23:54:55 +0100

    feat: add SettingsController for managing user settings operations

---
+++ server/src/modules/settings/settings.controller.ts
@@ -0,0 +1,57 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Param,
+  Put,
+  Logger,
+  UseGuards,
+} from '@nestjs/common';
+import { SettingsService } from './settings.service';
+import { CreateSettingsDto, UpdateSettingsDto } from './settings.dto';
+import { Settings } from 'src/shared/db/db.schema';
+import { GoogleAuthGuard } from '../auth/strategies/google.guard';
+
+// @UseGuards(GoogleAuthGuard)
+@Controller('settings')
+export class SettingsController {
+  private readonly logger = new Logger(SettingsController.name);
+
+  constructor(private readonly settingsService: SettingsService) {}
+
+  @Post()
+  public async create(
+    @Body() createSettingsDto: CreateSettingsDto,
+  ): Promise<Settings> {
+    const newSetting = await this.settingsService.create(createSettingsDto);
+
+    this.logger.log(`Created new setting with ID: ${newSetting.id}`);
+
+    return newSetting;
+  }
+
+  @Get(':id')
+  public async findById(@Param('id') id: string): Promise<Settings> {
+    const setting = await this.settingsService.findById(id);
+
+    this.logger.log(`Fetched setting with ID: ${id}`);
+
+    return setting;
+  }
+
+  @Put(':id')
+  public async update(
+    @Param('id') id: string,
+    @Body() updateSettingsDto: UpdateSettingsDto,
+  ): Promise<string> {
+    const updatedSettingId = await this.settingsService.update(
+      id,
+      updateSettingsDto,
+    );
+
+    this.logger.log(`Updated setting with ID: ${updatedSettingId}`);
+
+    return updatedSettingId;
+  }
+}
