fef6d0a5d3188bc34131b1f272e9fa2e2a54db3a
KornelJanczak <korneljanczak10@gmail.com>
2025-03-27 23:58:11 +0100

    feat: enhance middleware to include chat settings retrieval and redirect for onboarding

--- client/src/middleware.ts
+++ client/src/middleware.ts
@@ -1,17 +1,23 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
+import { chatService } from "./common/api/services/chat.service";
 
-export function middleware(request: NextRequest) {
+export async function middleware(request: NextRequest) {
   const cookieStore = request.cookies;
   const sessionID = cookieStore.get("connect.sid");
 
-  if (!sessionID) return NextResponse.redirect(new URL("/auth/sign-in", request.url));
+  if (!sessionID)
+    return NextResponse.redirect(new URL("/auth/sign-in", request.url));
 
-  return NextResponse.next({
-    headers: {
-      Cookies: request.cookies.toString(),
-    },
-  });
+  const response = NextResponse.next();
+  response.headers.set("Cookies", request.cookies.toString());
+
+  const settings = await chatService.getChatSettingsByUserId();
+
+  if (!settings)
+    return NextResponse.redirect(new URL("/onboarding", request.url));
+
+  return response;
 }
 
 export const config = {
