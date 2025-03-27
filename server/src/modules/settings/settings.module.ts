986134365b66d317377f8fb91026d6602971123d
KornelJanczak <korneljanczak10@gmail.com>
2025-03-27 21:22:36 +0100

    feat: integrate DbModule and SettingsRepository into SettingsModule

--- server/src/modules/settings/settings.module.ts
+++ server/src/modules/settings/settings.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { SettingsController } from './settings.controller';
 import { SettingsService } from './settings.service';
+import { SettingsRepository } from 'src/shared/repositories/settings.repository';
+import { DbModule } from 'src/shared/db/db.module';
 
 @Module({
+  imports: [DbModule],
   controllers: [SettingsController],
-  providers: [SettingsService]
+  providers: [SettingsService, SettingsRepository],
 })
 export class SettingsModule {}
