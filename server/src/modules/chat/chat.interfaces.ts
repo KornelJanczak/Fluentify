e129ef9b4757a8da62e86172ab7592b16768b77e
KornelJanczak <korneljanczak10@gmail.com>
2025-03-27 23:58:28 +0100

    refactor: rename interfaces for clarity and enhance OnFinishStreamArgs and GetSystemPromptArgs

--- server/src/modules/chat/chat.interfaces.ts
+++ server/src/modules/chat/chat.interfaces.ts
@@ -2,15 +2,15 @@ import * as googleCloud from '@google-cloud/text-to-speech';
 import type { CoreMessage, DataStreamWriter } from 'ai';
 import { Response } from 'express';
 
-export interface IGenerateAudioRequest
+export interface GenerateAudioRequest
   extends googleCloud.protos.google.cloud.texttospeech.v1
     .ISynthesizeSpeechRequest {}
 
-export interface IAudioContent
+export interface AudioContent
   extends googleCloud.protos.google.cloud.texttospeech.v1
     .ISynthesizeSpeechResponse {}
 
-export interface IVoice
+export interface Voice
   extends googleCloud.protos.google.cloud.texttospeech.v1.IVoice {}
 
 export interface StartStreamRequest {
@@ -19,17 +19,27 @@ export interface StartStreamRequest {
   res: Response;
 }
 
+export interface StreamToResponseArgs {
+  tutorId: string;
+  learningLanguage: string;
+  systemPrompt: string;
+  startStreamRequest: StartStreamRequest;
+}
+
 export interface OnFinishStreamArgs {
-  userId: string;
   chatId: string;
   messageId: string;
+  tutorId: string;
+  learningLanguage: string;
   content: string;
   usedTokens: number;
   streamWriter: DataStreamWriter;
 }
 
 export interface GetSystemPromptArgs {
-  userId: string;
+  tutorId: string;
+  learningLanguage: string;
+  learningLanguageLevel: string;
   chatCategory: string;
   chatTopic: string;
   vocabularySetId?: string;
