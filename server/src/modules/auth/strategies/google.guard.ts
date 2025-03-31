c6d47438005ffe9378f44de7dddb13cc69f998e5
KornelJanczak <korneljanczak10@gmail.com>
2025-03-31 23:55:19 +0200

    refactor: add logging to GoogleAuthGuard canActivate method for better debugging

--- server/src/modules/auth/strategies/google.guard.ts
+++ server/src/modules/auth/strategies/google.guard.ts
@@ -11,12 +11,18 @@ export class GoogleAuthGuard extends AuthGuard('google') {
   }
 
   public async canActivate(context: ExecutionContext) {
+    console.log('GoogleAuthGuard canActivate called');
+
     const request = context.switchToHttp().getRequest<Request>();
+
+    console.log('request', request.isAuthenticated());
 
     if (request.isAuthenticated()) return true;
 
     const activate = (await super.canActivate(context)) as boolean;
     await super.logIn(request);
+
+    console.log('activate without google', activate);
 
     return activate;
   }
