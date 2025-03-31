1d705129175760fa5a8599d9d9271d37b5d5f628
KornelJanczak <korneljanczak10@gmail.com>
2025-03-31 23:55:15 +0200

    refactor: restructure SettingsPage to use tabs for Learning and Chat settings

--- client/src/app/dashboard/chat/settings/page.tsx
+++ client/src/app/dashboard/chat/settings/page.tsx
@@ -1,5 +1,38 @@
-import Settings from "@/components/settings";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { LearningSettingsForm } from "@/components/settings/learning-settings-form";
+import SectionWrapper from "@/components/section-wrapper";
+import { ChatSettingsForm } from "@/components/settings/chat-settings-form";
+import { settingsService } from "@/common/api/services/settings.service";
 
-export default function SettingsPage() {
-  return <Settings />;
+export default async function SettingsPage() {
+  const {
+    learningLanguage,
+    learningLanguageLevel,
+    nativeLanguage,
+    ...chatSettings
+  } = await settingsService.getChatSettingsByUserId();
+
+  return (
+    <SectionWrapper>
+      <Tabs defaultValue="learning" className="w-full max-w-6xl">
+        <TabsList>
+          <TabsTrigger value="learning">Learning Settings</TabsTrigger>
+          <TabsTrigger value="chat">Chat Settings</TabsTrigger>
+        </TabsList>
+        <TabsContent value="learning">
+          <LearningSettingsForm
+            learningLanguage={learningLanguage}
+            learningLanguageLevel={learningLanguageLevel}
+            nativeLanguage={nativeLanguage}
+          />
+        </TabsContent>
+        <TabsContent value="chat">
+          <ChatSettingsForm
+            learningLanguage={learningLanguage}
+            {...chatSettings}
+          />
+        </TabsContent>
+      </Tabs>
+    </SectionWrapper>
+  );
 }
